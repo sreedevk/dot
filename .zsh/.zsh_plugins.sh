d1a2e4ab6a7e26c02efda1a801a6bc357e9764e7
sreedev <sreedevpadmakumar@gmail.com>
2021-10-10 20:48:11 +0530

    fixed zsh plugins refactor

---
+++ .zsh/.zsh_plugins.sh
@@ -0,0 +1,4 @@
+source /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab/fzf-tab.plugin.zsh
+fpath+=( /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab )
