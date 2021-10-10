68e5f6f137e84f3846acaba94d8c9cb082d84bec
sreedev <sreedevpadmakumar@gmail.com>
2021-10-10 15:37:23 +0530

    simplified i3 config, removed duplicate i3 config, modified setup display script to use bash,removed polybar

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -1,3 +1,11 @@
-#!/usr/bin/zsh
-source /home/sreedev/.zsh_funcs
-setup_display
+#!/bin/bash
+
+connected_displays=( $(xrandr | grep -E '(^|\s)connected($|\s)' | awk '{print $1}') )
+port_display="${connected_displays[1]}"
+dock_display="${connected_displays[2]}"
+
+if [ ${#connected_displays[@]} -gt 1 ]; then
+  xrandr                                                                   \
+    --output "$port_display" --mode 1920x1080 --pos 1920x0 --rotate normal \
+    --output "$dock_display" --mode 1920x1080 --pos 0x0 --rotate normal
+fi
