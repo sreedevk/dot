442491bddefcdb87f744ed51d1f34aea7ed5f44a
sreedev <sreedevpadmakumar@gmail.com>
2021-10-11 18:54:50 +0530

    script changes

--- .scripts/alsa_automute_toggle.sh
+++ .scripts/alsa_automute_toggle.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/zsh
 
+# Script that toggles mute on Alsa. You can map this script to a keybinding on i3wm to mute & unmute audio
+
 export current_status="$(amixer -c 0 sget "Auto-Mute Mode" | grep Item0 | awk '{print $2}')"
 if [[ $current_status == "'Enabled'" ]]
 then
