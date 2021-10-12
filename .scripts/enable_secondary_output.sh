89d9e4c15bc1c451f038e778d513318746dad611
sreedev <sreedevpadmakumar@gmail.com>
2021-10-12 06:13:44 +0530

    alacritty changes + neovide configs added

--- .scripts/enable_secondary_output.sh
+++ .scripts/enable_secondary_output.sh
@@ -5,7 +5,7 @@ port_display="${connected_displays[1]}"
 dock_display="${connected_displays[2]}"
 
 if [ ${#connected_displays[@]} -gt 1 ]; then
-  xrandr                                                                   \
+  xrandr                                                                                          \
     --output "${connected_displays[0]}" --scale 2x2 --mode 1920x1080 --pos 3840x0 --rotate normal \
-    --output "${connected_displays[1]}" --scale 2x2 --mode 1920x1080 --pos 0x0 --rotate normal
+    --output "${connected_displays[1]}" --scale 2x2 --mode 1920x1080 --pos 0x0 --rotate normal --rate 120
 fi
