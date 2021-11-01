07a37369235e6c7568b3b5fc9ffeae19c7150c55
sreedev <sreedevpadmakumar@gmail.com>
2021-11-01 08:32:00 +0530

    added filetypes selection command for telescope

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -6,5 +6,6 @@ vim.cmd(
     command! WipeReg for i in range(34,122) | silent! call setreg(nr2char(i), []) | endfor
     command! -nargs=0 Sw w !sudo tee % > /dev/null
     command! -nargs=1 W3m !w3m <f-args>
+    command! Filetypes :Telescope filetypes
   ]]
 )
