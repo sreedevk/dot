145f513004a2389eb644031fc816839d1ac36ab5
sreedev <sreedevpadmakumar@gmail.com>
2021-11-01 06:06:04 +0530

    cleanup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,25 +1,15 @@
+# TITLE: Fish (Friendly Interactive Shell) Configuration
+# AUTHOR: Sreedev Kodichath
+
 if status is-interactive
-    # DISABLE FISH GREETINGS
     set fish_greeting
-
-    # TERMINAL
     set TERM "xterm-256color"
-
-    # SET BAT AS MAN PAGES CLIENT
     set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
-
-    # SET VI KEYBINDINGS
     fish_vi_key_bindings
 
-    # SET EMACS KEYBINDINGS (DEFAULT)
-    # fish_default_key_bindings
-
-    # SYNTAX HIGHLIGHTS
     set fish_color_autosuggestion '#7d7d7d'
     set fish_color_param brcyan
     set fish_color_normal brcyan
     set fish_color_command brcyan
     set fish_color_error '#ff6c6b'
-
-
 end
