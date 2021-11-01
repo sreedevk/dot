4a08269c4f944361c380f1fb0c9316fcca554a8f
sreedev <sreedevpadmakumar@gmail.com>
2021-11-01 21:11:59 +0530

    added function for smart tab & added shortcut for telescope

--- .config/nvim/lua/functions.lua
+++ .config/nvim/lua/functions.lua
@@ -7,4 +7,19 @@ function M.PresentationMode()
   vim.bo[0].cursorline     = false
 end
 
+function M.smart_tab()
+  if vim.fn.pumvisible() ~= 0 then
+    vim.api.nvim_eval([[feedkeys("\<c-n>", "n")]])
+    return
+  end
+
+  local col = vim.fn.col(".") - 1
+  if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+    vim.api.nvim_eval([[feedkeys("\<tab>", "n")]])
+    return
+  end
+
+  vim.fn["compe#complete"]();
+end
+
 return M
