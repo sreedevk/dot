ff75ba8d0287fc3eae1536448f0dd47969611359
sreedev <sreedevpadmakumar@gmail.com>
2021-11-11 08:47:24 +0530

    neovim plugins file reformatting

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,20 +1,21 @@
 vim.cmd('packadd packer.nvim')
 
 return require('packer').startup(function()
-  use { 'wbthomason/packer.nvim' }  -- plugin manager
-  use { 'mattn/emmet-vim' }         -- html emmets
-  use { 'scrooloose/nerdTree' }     -- project filetree
-  use { 'sjl/gundo.vim' }           -- undo tree
-  use { 'tpope/vim-surround' }      -- ysiw
-  use { 'hoob3rt/lualine.nvim' }    -- statusline
-  use { 'tpope/vim-fugitive' }      -- git
-  use { 'psliwka/vim-smoothie' }    -- smooth scrolling
+  use 'wbthomason/packer.nvim' -- plugin manager
+  use 'mattn/emmet-vim'        -- html emmets
+  use 'scrooloose/nerdTree'    -- project filetree
+  use 'sjl/gundo.vim'          -- undo tree
+  use 'tpope/vim-surround'     -- ysiw
+  use 'hoob3rt/lualine.nvim'   -- statusline
+  use 'tpope/vim-fugitive'     -- git
+  use 'psliwka/vim-smoothie'   -- smooth scrolling
 
   -- colorschemes
-  use { 'ayu-theme/ayu-vim' }
-  use { 'ghifarit53/tokyonight-vim' }
+  use 'ayu-theme/ayu-vim'
+  use 'ghifarit53/tokyonight-vim'
 
   -- Language Server + Intellisense
+  use 'neovim/nvim-lspconfig'
   use { 'neoclide/coc.nvim', branch = 'release' }
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
@@ -22,7 +23,11 @@ return require('packer').startup(function()
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
 
   -- EasyMotion
-  use { 'phaazon/hop.nvim', as = 'hop', config = function()
-    require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' } 
-  end }
+  use {
+    'phaazon/hop.nvim',
+    as = 'hop',
+    config = function()
+      require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+    end
+  }
 end)
