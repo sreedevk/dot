0538b681dc2e72e7e871024019a5528b41ee8891
sreedev <sreedevpadmakumar@gmail.com>
2021-11-12 01:49:56 +0530

    removed custom emacs configuration

--- stowed/.emacs-profiles.el
+++ stowed/.emacs-profiles.el
@@ -1,3 +1,2 @@
 (("doomemacs" . ((user-emacs-directory . "~/.emacs-distros/.doomemacs.d")))
- ("customacs" . ((user-emacs-directory . "~/.emacs-distros/.custom.d")))
  ("spacemacs" . ((user-emacs-directory . "~/.emacs-distros/.spacemacs.d"))))
