f5680e18f36fc28524efbf9386089c293c977013
sreedev <sreedevpadmakumar@gmail.com>
2021-11-15 12:02:31 +0530

    added lua functions for presentation mode + remove comments

--- stowed/.config/nvim/lua/commands.lua
+++ stowed/.config/nvim/lua/commands.lua
@@ -2,10 +2,11 @@
 vim.cmd(
   [[
     command! -complete=file -nargs=1 T tabedit <args>
-    command! W w
     command! WipeReg for i in range(34,122) | silent! call setreg(nr2char(i), []) | endfor
     command! -nargs=0 Sw w !sudo tee % > /dev/null
-    command! -nargs=1 W3m !w3m <f-args>
-    command! Filetypes :Telescope filetypes
+    command! Filetypes Telescope filetypes
+    command! W w
+    command! PresentationMode lua require('funk').presentation_mode()
+    command! RemoveComments lua require('funk').remove_comments()
   ]]
 )
---
+++ stowed/.config/nvim/lua/funk.lua
@@ -0,0 +1,15 @@
+local M = {}
+-- Switch to Presentation View
+function M.presentation_mode()
+  -- 0th buffer is the current buffer
+  vim.wo[0].number         = false
+  vim.wo[0].relativenumber = false
+  vim.wo[0].cursorline     = false
+end
+
+function M.remove_comments()
+  vim.cmd([[:g/\v^(#|\/\/)/d]])
+  vim.cmd([[:g/\v^\s*(#|\/\/)/d]])
+end
+
+return M
