2cd56f158828a3b9a53e4e350cd680f32d267c2b
sreedev <sreedevpadmakumar@gmail.com>
2021-11-15 12:16:56 +0530

    updated scripts - added cpyrt info

--- stowed/.local/bin/databing.sh
+++ stowed/.local/bin/databing.sh
@@ -16,13 +16,3 @@
 # IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-connected_displays=( $(xrandr | grep -E '(^|\s)connected($|\s)' | awk '{print $1}') )
-port_display="${connected_displays[1]}"
-dock_display="${connected_displays[2]}"
-
-if [ ${#connected_displays[@]} -gt 1 ]; then
-  xrandr                                                                                          \
-    --output "${connected_displays[0]}" --scale 2x2 --mode 1920x1080 --pos 3840x0 --rotate normal \
-    --output "${connected_displays[1]}" --scale 2x2 --mode 1920x1080 --pos 0x0 --rotate normal --rate 120
-fi
