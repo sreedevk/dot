7e49417bd8a47a7942ef27c3176a22670421726a
sreedev <sreedevpadmakumar@gmail.com>
2021-11-19 04:02:48 +0530

    updated tmux status with custom bash script

---
+++ stowed/.local/bin/tmux_status_right.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# TIME
+LOCAL_TIME=$(date '+%d/%m/%Y %H:%M %p %Z')
+OFFSHORE_TIME=$(TZ=America/New_York date '+%d/%m/%Y %I:%M %p %Z') 
+
+# WIRELESS
+WIRELESS_INTERFACE=$(cat /proc/net/wireless | tail -n 1 | awk '{print $1}' | tr -d :)
+IP_ADDR="$(ip -f inet addr show $WIRELESS_INTERFACE | awk '/inet / {print $2}')"
+
+# BATTERY
+BATTERY_PERCENTAGE=$(upower -e | grep BAT0 | xargs -Ix upower -i x | grep percentage | awk '{print $2}')
+BATTERY_STATUS=$(upower -e | grep BAT0 | xargs -Ix upower -i x | grep state | awk '{print $2}')
+BATTERY_INFO="$BATTERY_PERCENTAGE $BATTERY_STATUS"
+
+echo "$WIRELESS_INTERFACE: $IP_ADDR | BAT: $BATTERY_INFO | $OFFSHORE_TIME | $LOCAL_TIME"
+
+#MEMORY
