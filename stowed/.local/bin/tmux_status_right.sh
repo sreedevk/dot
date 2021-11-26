bb62c5f5eb4be94e5e262cdf81940d7c3e6ba662
sreedev <sreedevpadmakumar@gmail.com>
2021-11-26 22:23:39 +0530

    added GPU info to tmux status

--- stowed/.local/bin/tmux_status_right.sh
+++ stowed/.local/bin/tmux_status_right.sh
@@ -13,4 +13,11 @@ BATTERY_PERCENTAGE=$(upower -e | grep BAT0 | xargs -Ix upower -i x | grep percen
 BATTERY_STATUS=$(upower -e | grep BAT0 | xargs -Ix upower -i x | grep state | awk '{print $2}')
 BATTERY_INFO="$BATTERY_PERCENTAGE $BATTERY_STATUS"
 
-echo "$WIRELESS_INTERFACE: $IP_ADDR | BAT: $BATTERY_INFO | $OFFSHORE_TIME | $LOCAL_TIME"
+# GPU
+GPU_INFO=$(nvidia-smi -q)
+PRODUCT_NAME="$(awk '/Product Name/ {print $4FS$5}' <<< $GPU_INFO)"
+MEMORY_USAGE="$(awk '/Used/ {print $3;exit}' <<< $GPU_INFO)"
+TOTAL_MEMORY="$(awk '/Total/ {print $3FS$4;exit}' <<< $GPU_INFO)"
+GPU_TEMP="$(awk '/GPU Current Temp/ {print $5"°"$6;exit}' <<< $GPU_INFO)"
+
+echo "$PRODUCT_NAME: $GPU_TEMP $MEMORY_USAGE/$TOTAL_MEMORY | $WIRELESS_INTERFACE: $IP_ADDR | BAT: $BATTERY_INFO | $OFFSHORE_TIME | $LOCAL_TIME"
