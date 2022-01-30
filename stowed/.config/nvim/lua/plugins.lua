ebd2f3ff86751cf485a0f06b0e932a5bf7477996
sreedev <sreedevpadmakumar@gmail.com>
2022-01-30 17:14:29 +0530

    updated anaconda path + neovim configs

--- stowed/.config/nvim/lua/plugins.lua
+++ stowed/.config/nvim/lua/plugins.lua
@@ -11,6 +11,9 @@ return require('packer').startup(function()
   use 'tpope/vim-surround'     -- ysiw
   use 'folke/tokyonight.nvim'  -- colorscheme
 
+  -- Note Taking
+  use 'dhruvasagar/vim-dotoo'
+
   -- Language Server + Intellisense
   use { 'sheerun/vim-polyglot' }
   use { 'neoclide/coc.nvim', branch = 'release' }
