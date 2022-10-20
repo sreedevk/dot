13d04115b1cb840338d68e8c12b19dfac580cb27
sreedev <sreedevpadmakumar@gmail.com>
2022-10-21 04:47:56 +0530

    awesome wm updates

--- stowed/.config/nvim/lua/plugins.lua
+++ stowed/.config/nvim/lua/plugins.lua
@@ -32,7 +32,6 @@ return require('packer').startup(function(use)
 
   use {
     'kyazdani42/nvim-tree.lua',
-    requires = { 'kyazdani42/nvim-web-devicons' }, -- optional, for file icons
     tag = 'nightly', -- optional, updated every week. (see issue #1193)
   }
 
