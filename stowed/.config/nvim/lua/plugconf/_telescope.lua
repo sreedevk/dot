4ae200e4dd0fdb83910e81f558cf62937b1ae290
sreedev <sreedevpadmakumar@gmail.com>
2022-10-23 20:44:23 -0400

    gitignore fix

---
+++ stowed/.config/nvim/lua/plugconf/_telescope.lua
@@ -0,0 +1,12 @@
+require('telescope').setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-k>"] = require('telescope.actions').move_selection_previous,
+        ["<C-j>"] = require('telescope.actions').move_selection_next,
+        ["<esc>"] = require('telescope.actions').close,
+      }
+    }
+  },
+  pickers = {},
+}
---
+++ stowed/.config/nvim/lua/utils/init.lua
@@ -0,0 +1,20 @@
+local M = {}
+-- Switch to Presentation View
+function M.presentation_mode()
+  -- 0th buffer is the current buffer
+  vim.wo[0].number         = false
+  vim.wo[0].relativenumber = false
+  vim.wo[0].cursorline     = false
+end
+
+function M.remove_comments()
+  vim.cmd([[:g/\v^(#|\/\/)/d]])
+  vim.cmd([[:g/\v^\s*(#|\/\/)/d]])
+end
+
+function M.neovim_config()
+  vim.cmd([[:cd ~/.config/nvim]])
+  vim.cmd([[:e .]])
+end
+
+return M
