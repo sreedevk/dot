4e000f71de87d126170775d0c56c85dc53b47e35
sreedev <sreedevpadmakumar@gmail.com>
2022-10-23 21:53:13 -0400

    replaced coc with native lsp client

---
+++ stowed/.config/nvim/lua/plugconf/lsp/mason.lua
@@ -0,0 +1,39 @@
+local mason_ok, mason = pcall(require, "mason")
+if not mason_ok then
+  return
+end
+
+local mason_lspconfig_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not mason_lspconfig_ok then
+  return
+end
+
+mason_lspconfig.setup({
+  ensure_installed = {
+    "tsserver",
+    "html",
+    "cssls",
+    "tailwindcss",
+    "sumneko_lua",
+    "rust_analyzer",
+    "solargraph",
+    "clojure_lsp",
+    "elixirls",
+    "erlangls",
+    "gopls",
+    "hls",
+    "julials",
+    "texlab",
+    "marksman",
+    "ocamllsp",
+    "pyright",
+    "sqlls",
+    "svelte",
+    "rescriptls",
+    "graphql",
+    "dockerls",
+    "denols",
+    "clangd",
+    "asm_lsp"
+  }
+})
--- stowed/.config/nvim/lua/plugins.lua
+++ stowed/.config/nvim/lua/plugins.lua
@@ -18,13 +18,16 @@ return require('packer').startup(function(use)
       require("mason").setup()
     end
   }
+  use 'williamboman/mason-lspconfig.nvim'
 
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
+  use 'L3MON4D3/LuaSnip'
   use 'saadparwaiz1/cmp_luasnip'
   use 'hrsh7th/cmp-nvim-lsp'
+  use { "glepnir/lspsaga.nvim", branch = "main" }
 
   -- lsp config (end)
 
