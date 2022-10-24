6f8bbc9e42be162e1021d71b0c31b791d89579ea
sreedev <sreedevpadmakumar@gmail.com>
2022-10-23 21:59:19 -0400

    neovim: autocmd for autoloading plugins on saving plugins.lua

--- stowed/.config/nvim/lua/plugins.lua
+++ stowed/.config/nvim/lua/plugins.lua
@@ -1,4 +1,20 @@
-vim.cmd('packadd packer.nvim')
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+local packer_bootstrap = ensure_packer() -- true if packer was just installed
+
+-- import packer safely
+local status, packer = pcall(require, "packer")
+if not status then
+	return
+end
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim' -- plugin manager
