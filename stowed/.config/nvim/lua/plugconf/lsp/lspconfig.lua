2d4062a4861e66f315fdd5e47c131bc09002bb7d
sreedev <sreedevpadmakumar@gmail.com>
2022-11-13 23:56:32 -0500

    lspconfig keybinds changed

--- stowed/.config/nvim/lua/plugconf/lsp/lspconfig.lua
+++ stowed/.config/nvim/lua/plugconf/lsp/lspconfig.lua
@@ -22,7 +22,6 @@ local on_attach = function(client, buff)
   keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
   keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
   keymap.set("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
-  keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
   keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to previous diagnostic in buffer
   keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
   keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
