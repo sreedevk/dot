e0435d81218f6bab9bef379696c68780e91eb0c2
sreedev <sreedevpadmakumar@gmail.com>
2023-01-26 02:23:11 -0500

    treesitter nvim updates

--- stowed/.config/awesome/theme/base.lua
+++ stowed/.config/awesome/theme/base.lua
@@ -25,7 +25,7 @@ theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
 theme.useless_gap   = dpi(0)
-theme.border_width  = dpi(1)
+theme.border_width  = dpi(0)
 theme.border_normal = "#000000"
 theme.border_focus  = "#535d6c"
 theme.border_marked = "#91231c"
@@ -96,7 +96,7 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = os.getenv("HOME") .. "/.config/awesome/wallpapers/archlinux.jpg"
+theme.wallpaper = os.getenv("HOME") .. "/.config/awesome/wallpapers/arch-jp.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
