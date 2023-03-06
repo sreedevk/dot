480da8adb2b852318c4f972e0e2158dc723e1bcd
sreedev <sreedevpadmakumar@gmail.com>
2023-03-06 10:28:53 -0500

    picom fixes + neovim updates

--- stowed/.config/nvim/lua/plugconf/_lualine.lua
+++ stowed/.config/nvim/lua/plugconf/_lualine.lua
@@ -5,29 +5,30 @@ end
 
 lualine.setup {
   options = {
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' }
+    theme = 'auto'
   },
   sections = {
-    lualine_a = {
-      { 'mode', separator = { left = ' ' }, right_padding = 1 },
-    },
-    lualine_b = { 'filename', 'branch' },
-    lualine_c = { 'fileformat' },
-    lualine_x = {},
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff' },
+    lualine_c = { 'buffers' },
+    lualine_x = { 'tabs' },
     lualine_y = { 'filetype', 'progress' },
     lualine_z = {
-      { 'location', separator = { right = ' ' }, left_padding = 1 },
+      { 'diagnostics',
+        sources = { 'nvim_diagnostic', 'nvim_lsp' },
+        sections = { 'error', 'warn', 'info', 'hint' },
+        diagnostics_color = {
+          -- Same values as the general color option can be used here.
+          error = 'DiagnosticError', -- Changes diagnostics' error color.
+          warn  = 'DiagnosticWarn', -- Changes diagnostics' warn color.
+          info  = 'DiagnosticInfo', -- Changes diagnostics' info color.
+          hint  = 'DiagnosticHint', -- Changes diagnostics' hint color.
+        },
+        symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' },
+        colored = true, -- Displays diagnostics status in color if set to true.
+        update_in_insert = false, -- Update diagnostics in insert mode.
+        always_visible = false, -- Show diagnostics even if there are none.
+      }
     },
   },
-  inactive_sections = {
-    lualine_a = { 'filename' },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'location' },
-  },
-  tabline = {},
-  extensions = {},
 }
