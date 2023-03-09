d4c39196e430719998d67230af53234225eaa54e
sreedev <sreedevpadmakumar@gmail.com>
2023-03-09 12:49:20 -0500

    neovim improvements

--- stowed/.config/nvim/lua/plugins/telescope.lua
+++ stowed/.config/nvim/lua/plugins/telescope.lua
@@ -2,20 +2,37 @@ return {
   'nvim-telescope/telescope.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-telescope/telescope-project.nvim'
+    'nvim-telescope/telescope-project.nvim',
+    'nvim-telescope/telescope-file-browser.nvim',
   },
   cmd = "Telescope",
   config = function()
-    require('telescope').setup {
+    local telescope = require("telescope")
+    telescope.setup {
+      extensions = {
+        file_browser = {
+          theme = 'ivy',
+          hijack_netrw = false,
+          mappings = {
+                ["i"] = {
+                  ["<C-k>"] = require('telescope.actions').move_selection_previous,
+                  ["<C-j>"] = require('telescope.actions').move_selection_next,
+                  ["<esc>"] = require('telescope.actions').close,
+            }
+          }
+        }
+      },
       defaults = {
         mappings = {
           i = {
-            ["<C-k>"] = require('telescope.actions').move_selection_previous,
-            ["<C-j>"] = require('telescope.actions').move_selection_next,
-            ["<esc>"] = require('telescope.actions').close,
+                ["<C-k>"] = require('telescope.actions').move_selection_previous,
+                ["<C-j>"] = require('telescope.actions').move_selection_next,
+                ["<esc>"] = require('telescope.actions').close,
           }
         }
       },
     }
+
+    telescope.load_extension "file_browser"
   end
 }
