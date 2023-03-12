88961784f395283248c77e96f57fd7bcc820324a
sreedev <sreedevpadmakumar@gmail.com>
2023-03-12 18:32:58 -0400

    neovim updates

--- stowed/.config/nvim/lua/plugins/init.lua
+++ stowed/.config/nvim/lua/plugins/init.lua
@@ -11,6 +11,22 @@ return {
     cmd = "UndotreeToggle"
   },
   {
+    "cuducos/yaml.nvim",
+    ft = { "yaml" },
+    cmd = {
+      "YAMLView",
+      "YAMLYank",
+      "YAMLYankKey",
+      "YAMLYankValue",
+      "YAMLQuickfix",
+      "YAMLTelescope"
+    },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim"
+    },
+  },
+  {
     'dhruvasagar/vim-table-mode',
     cmd = "TableModeToggle"
   },
