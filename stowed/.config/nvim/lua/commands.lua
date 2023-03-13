b39d14b601d31fad5e48fdac875f85bac822a425
sreedev <sreedevpadmakumar@gmail.com>
2023-03-13 02:43:01 -0400

    neovim improvements

--- stowed/.config/nvim/lua/commands.lua
+++ stowed/.config/nvim/lua/commands.lua
@@ -6,7 +6,7 @@ api.nvim_create_user_command('Wq', 'wq', {})
 api.nvim_create_user_command('WQ', 'wq', {})
 api.nvim_create_user_command('Q', 'q', {})
 api.nvim_create_user_command('Filetypes', 'Telescope filetypes', {})
-api.nvim_create_user_command('Resource', 'source ~/.config/nvim/init.lua', {})
+api.nvim_create_user_command('ToggleVenn', ":lua require('helpers').toggle_venn()", {})
 api.nvim_create_user_command(
   'WipeReg',
   'for i in range(34,122) | silent! call setreg(nr2char(i), []) | endfor',
