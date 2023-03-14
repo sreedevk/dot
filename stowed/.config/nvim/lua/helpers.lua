a3753e73dcfc854ab8639ec1e183ba00cc3ff2df
sreedev <sreedevpadmakumar@gmail.com>
2023-03-14 13:30:55 -0400

    neovim added functions to run code async + sync in term

--- stowed/.config/nvim/lua/helpers.lua
+++ stowed/.config/nvim/lua/helpers.lua
@@ -1,7 +1,7 @@
 local api = vim.api
 
-local Helpers = {}
-function Helpers.create_augroups(definitions)
+local M = {}
+function M.create_augroups(definitions)
   for group_name, group_definitions in pairs(definitions) do
     local group = api.nvim_create_augroup(group_name, { clear = true })
     for _, definition in ipairs(group_definitions) do
@@ -17,30 +17,10 @@ function Helpers.create_augroups(definitions)
   end
 end
 
--- venn.nvim: enable or disable keymappings
-function Helpers.toggle_venn()
-    local venn_enabled = vim.inspect(vim.b.venn_enabled)
-    if venn_enabled == "nil" then
-        vim.b.venn_enabled = true
-        vim.cmd[[setlocal ve=all]]
-        -- draw a line on HJKL keystokes
-        vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", {noremap = true})
-        vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", {noremap = true})
-        vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", {noremap = true})
-        vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", {noremap = true})
-        -- draw a box by pressing "f" with visual selection
-        vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", {noremap = true})
-    else
-        vim.cmd[[setlocal ve=]]
-        vim.cmd[[mapclear <buffer>]]
-        vim.b.venn_enabled = nil
-    end
-end
-
-function Helpers.map(mode, lhs, rhs, opts)
+function M.map(mode, lhs, rhs, opts)
   local options = { noremap = true }
   if opts then options = vim.tbl_extend('force', options, opts) end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-return Helpers
+return M
