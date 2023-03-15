fc70858f4814baf137f2eb1d0afedc6e5fd8a5fc
sreedev <sreedevpadmakumar@gmail.com>
2023-03-14 20:47:14 -0400

    doom emacs fixes

--- stowed/.config/doom/packages.el
+++ stowed/.config/doom/packages.el
@@ -1,9 +1,4 @@
 ;;; package.el -*- lexical-binding: t; -*-
 
-
-(package! straight :pin "3eca39d")
 (package! emmet-mode)
 (package! cider)
-(package! jet :recipe (:host github :repo "ericdallo/jet.el"))
-(package! company-org-roam
-   :recipe (:host github :repo "jethrokuan/company-org-roam"))
