add0b30148065fb6e29f37c4e39ddd72048d1b23
sreedev <sreedevpadmakumar@gmail.com>
2023-03-24 15:34:34 -0400

    neovim improvements

--- stowed/.config/nvim/init.lua
+++ stowed/.config/nvim/init.lua
@@ -10,20 +10,7 @@
 --      ░                           ░
 -- AUTHOR: SREEDEV KODICHATH
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-
-vim.opt.rtp:prepend(lazypath)
-
+require('lazypm')
 require('opts')
 require('lazy').setup('plugins')
 require('commands')
