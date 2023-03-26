12742eb1a4ac7fdd2dd4b3a06b1b36c950e8584b
sreedev <sreedevpadmakumar@gmail.com>
2023-03-26 17:42:50 -0400

    keybinding organization neovim

--- stowed/.config/nvim/lua/keybind.lua
+++ stowed/.config/nvim/lua/keybind.lua
@@ -9,82 +9,70 @@ map('n', '<Leader>gpu', "<cmd>Git push<CR>")
 map('n', '<Leader>gpl', "<cmd>Git pull<CR>")
 
 -- lazy
-map('n', '<Leader>lz', "<cmd>Lazy<CR>")
+map('n', '<Leader>l', "<cmd>Lazy<CR>")
 
 -- convenience
 map('n', '<Leader>w', "<cmd>w<CR>")
 map('n', '<Leader>q', "<cmd>q<CR>")
 map('n', '<Leader>h', "<cmd>noh<CR>")
+map('t', 'jj', "<C-\\><C-n>")
+map('n', 'n', 'nzzzv')
+map('n', 'N', 'Nzzzv')
+map('n', '<C-d>', "<C-d>zz")
+map('n', '<C-u>', "<C-u>zz")
+map('v', '<M-j>', ":m '>+1<CR>gv=gv")
+map('v', '<M-k>', ":m '<-2<CR>gv=gv")
+map('n', '<Leader>x', "<cmd>! chmod +x %<CR>")
 
 -- Neotree
 map('n', '<C-n>', "<cmd>Neotree reveal toggle<CR>")
+
+-- File/Dir Nav
 map('n', '<Leader>cd', "<cmd>lua vim.cmd('cd ' .. vim.fn.expand('%:p:h'))<CR>")
-
--- terminal
-map('n', '<Leader>tr', "<cmd>ToggleTerm<CR>")
-map('n', '<Leader>tf', "<cmd>ToggleTerm direction=float<CR>")
-
--- telescope
 map('n', '<C-p>', "<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files <CR>")
 map('n', '<Leader>fb', "<cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>")
-map('n', '<Leader>bl', "<cmd>Telescope buffers<CR>")
-map('n', '<C-s>', "<cmd>Telescope<CR>")
 map('n', '<Leader>rg', "<cmd>Telescope live_grep<CR>")
+
+-- telescope
+map('n', '<C-s>', "<cmd>Telescope<CR>")
 map('n', '<Leader>gr', "<cmd>Telescope grep_string<CR>")
 
--- aerial
+-- LSP/TS
 map('n', '<Leader>cv', "<cmd>Lspsaga outline<CR>")
 
--- disable arrows
+-- chadmode: no arrow keys
 map('', '<up>', "<nop>")
 map('', '<down>', "<nop>")
 map('', '<left>', "<nop>")
 map('', '<right>', "<nop>")
 
--- tab nav
+-- Tabs
 map('n', '<C-t>', "<cmd>tabnew<CR>")
 
--- zen mode
-map('n', '<Leader>z', '<cmd>ZenMode<CR>')
-
--- undo tree
-map('n', '<Leader>u', "<cmd>UndotreeToggle<CR>")
-
--- easy escape
-map('t', 'jj', "<C-\\><C-n>")
-
--- search nav
-map('n', 'n', 'nzzzv')
-map('n', 'N', 'Nzzzv')
-
--- buffer nav
-map('n', '<C-d>', "<C-d>zz")
-map('n', '<C-u>', "<C-u>zz")
+-- buffers
+map('n', '<Leader>bl', "<cmd>Telescope buffers<CR>")
 map('n', '<Leader>bd', "<cmd>bd<CR>")
-
--- buffer switch
 map('n', '<Leader>bb', '<cmd>bnext<CR>')
 map('n', '<Leader>bB', '<cmd>bprev<CR>')
 
--- notes
+-- toggle
 map('n', '<Leader>tm', "<cmd>TableModeToggle<CR>")
-map('n', '<leader>ve', "<cmd>ToggleVenn<CR>")
+map('n', '<leader>tv', "<cmd>ToggleVenn<CR>")
+map('n', '<leader>ts', "<cmd>set spell!<CR>")
+map('n', '<Leader>tu', "<cmd>UndotreeToggle<CR>")
+map('n', '<Leader>tr', "<cmd>ToggleTerm<CR>")
+map('n', '<Leader>tf', "<cmd>ToggleTerm direction=float<CR>")
+map('n', '<Leader>tz', '<cmd>ZenMode<CR>')
 
--- Move
-map('v', '<M-j>', ":m '>+1<CR>gv=gv")
-map('v', '<M-k>', ":m '<-2<CR>gv=gv")
+-- alt toggle
+map('n', '<Leader>z', '<cmd>ZenMode<CR>')
+map('n', '<Leader>u', "<cmd>UndotreeToggle<CR>")
 
--- Utils
-map('n', '<Leader>x', "<cmd>! chmod +x %<CR>")
-
--- spell check
-map('n', '<Leader>sk', '<cmd>set spell!<CR>')
-
--- run project commands / repl
+-- Run Command
 map('n', '<Leader>rn', '<cmd>CmdRun<CR>')
 map('v', '<Leader>rs', ":'>ToggleTermSendVisualLine<CR>")
 
--- Debugging
+-- debugging
 map('n', '<F5>', ':lua require("dap").continue()<CR>')
 map('n', '<F10>', ':lua require("dap").step_over()<CR>')
 map('n', '<F11>', ':lua require("dap").step_into()<CR>')
---
+++ stowed/.config/nvim/lua/plugins/whichkey.lua
@@ -0,0 +1,60 @@
+return {
+  "folke/which-key.nvim",
+  config = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+
+    local wk = require('which-key')
+    local mappings = {
+      ["<leader>"] = {
+        g = {
+          name = "+git",
+          i = { "<cmd>Git<CR>", "git/status" },
+          s = { "<cmd>Neotree git_status<CR>", "git/status(neotree)" },
+          l = {
+            name = "+git/log",
+            g = { "<cmd>Git log --oneline --decorate --graph<CR>", "git/log(graph)" },
+            o = { "<cmd>Git log<CR>", "git/log" }
+          },
+          p = {
+            name = "+pull/push",
+            u = { "<cmd>Git push<CR>", "git/push" },
+            l = { "<cmd>Git pull<CR>", "git/pull" }
+          }
+        },
+        t = {
+          name = "+toggle",
+          m = { "<cmd>TableModeToggle<CR>", "toggle/table"},
+          f = { "<cmd>ToggleTerm direction=float<CR>", "toggle/terminal(f)" },
+          r = { "<cmd>ToggleTerm<CR>", "toggle/terminal" },
+          v = { "<cmd>ToggleVenn<CR>", "toggle/venn" },
+          s = { "<cmd>set spell!<CR>", "toggle/spellcheck" },
+          u = { "<cmd>UndotreeToggle<CR>", "toggle/undotree" },
+          z = { "<cmd>ZenMode<CR>", "toggle/zenmode" }
+        },
+        r = {
+          name = "+cmd",
+          n = { "<cmd>CmdRun<CR>", "cmd/run" },
+          s = { ":'>ToggleTermSendVisualLine<CR>", "cmd/send" }
+        },
+        b = {
+          name = "+buffer",
+          b = { "<cmd>bnext<CR>", "buffer/next" },
+          B = { "<cmd>bprev<CR>", "buffer/prev" },
+          l = { "<cmd>Telescope buffers<CR>", "buffer/list" },
+          d = { "<cmd>bd<CR>", "buffer/delete" }
+        }
+      }
+    }
+    wk.register(mappings)
+    wk.setup({
+      key_labels = {
+        ["<space>"] = "SPC",
+        ["<cr>"] = "RET",
+        ["<tab>"] = "TAB"
+      },
+      ignore_missing = true,
+      show_help = false,
+    })
+  end
+}
