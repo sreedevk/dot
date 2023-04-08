6c7d820bdc8a3f476a4c345827dbb9bf542224ca
sreedev <sreedevpadmakumar@gmail.com>
2023-04-08 15:21:36 -0400

    added dependency install script for arch

---
+++ scripts/arch_install_deps.sh
@@ -0,0 +1,5 @@
+sudo pacman -S --noconfirm \
+  playerctl git zsh curl neovim bat stow exa dust tmux lf xplr fzf direnv \
+  zoxide kitty alacritty emacs i3-wm gnu-netcat dunst \
+  maim rofi polybar ttf-iosevka-nerd ttf-jetbrains-mono ttf-fira-code ttf-fira-mono \
+  ttf-firacode-nerd ttf-font-awesome brightnessctl picom dmenu
