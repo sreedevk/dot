929917344542990ad45d8557466919ffa675df48
sreedev <sreedevpadmakumar@gmail.com>
2023-04-09 03:31:21 -0400

    zsh autosuggestion plugins

--- stowed/.zsh/plugins.sh
+++ stowed/.zsh/plugins.sh
@@ -2,3 +2,5 @@ source /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-
 fpath+=( /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
 source /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab/fzf-tab.plugin.zsh
 fpath+=( /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab )
+source /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
