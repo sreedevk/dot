b2d6eb47916cf4195c6d102ccd45a65ad56795f5
sreedev <sreedevpadmakumar@gmail.com>
2023-04-11 08:58:12 -0400

    polybar redesign + neovim lsp winbar fixes

--- stowed/.config/nvim/lua/plugins/lspconf.lua
+++ stowed/.config/nvim/lua/plugins/lspconf.lua
@@ -23,6 +23,16 @@ return {
       },
       config = function()
         require("lspsaga").setup({
+          symbol_in_winbar = {
+            enable = true,
+            separator = "  ",
+            ignore_patterns={},
+            hide_keyword = true,
+            show_file = true,
+            folder_level = 2,
+            respect_root = false,
+            color_mode = true,
+          },
           lightbulb = {
             enable = false,
             enable_in_insert = false,
