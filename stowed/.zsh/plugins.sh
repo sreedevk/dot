189dcf8131ab70e614f877cd8cdddd556d2a1d14
sreedev <sreedevpadmakumar@gmail.com>
2023-04-15 04:28:57 -0400

    zsh syntax highlighting improvements + git config updates

--- stowed/.zsh/plugins.sh
+++ stowed/.zsh/plugins.sh
@@ -1,5 +1,3 @@
-source /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-fpath+=( /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
 source /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab/fzf-tab.plugin.zsh
 fpath+=( /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab )
 source /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
@@ -8,3 +6,5 @@ source /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-
 fpath+=( /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mroth-SLASH-evalcache )
 source /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode/zsh-vi-mode.plugin.zsh
 fpath+=( /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode )
+source /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting )
