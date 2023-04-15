1ab47056f4d4840d662a060e24c62798e5467135
sreedev <sreedevpadmakumar@gmail.com>
2023-04-14 21:58:35 -0400

    moved showcmdloc to lualine and added macro recording info to lualine

--- stowed/.config/nvim/lua/plugins/lualine.lua
+++ stowed/.config/nvim/lua/plugins/lualine.lua
@@ -2,20 +2,37 @@ return {
   'hoob3rt/lualine.nvim',
   lazy = true,
   event = { "BufReadPost", "BufAdd", "BufNewFile" },
-  config = function ()
-    require('lualine').setup {
+  config = function()
+    local lualine = require('lualine')
+
+    local function macro_rec_stats()
+      local recording_register = vim.fn.reg_recording()
+      if recording_register == "" then
+        return ""
+      else
+        return "Recording @ " .. recording_register
+      end
+    end
+
+    lualine.setup {
       options = {
         component_separators = { left = '', right = '' },
         section_separators = { left = ' ', right = '' },
       },
       sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch'  },
+        lualine_b = { 'branch' },
         lualine_c = { 'buffers' },
-        lualine_x = { 'fileformat', 'tabs' },
+        lualine_x = {
+          { 'macro-rec', fmt = macro_rec_stats },
+          '%S',
+          'fileformat',
+          'tabs'
+        },
         lualine_y = { 'filetype', 'progress' },
         lualine_z = {
-          { 'diagnostics',
+          {
+            'diagnostics',
             sources = { 'nvim_diagnostic', 'nvim_lsp' },
             sections = { 'error', 'warn', 'info', 'hint' },
             diagnostics_color = {
@@ -32,5 +49,25 @@ return {
         },
       },
     }
+
+
+    vim.api.nvim_create_autocmd("RecordingEnter", {
+      callback = function()
+        lualine.refresh({
+          place = { "statusline" },
+        })
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("RecordingLeave", {
+      callback = function()
+        local timer = vim.loop.new_timer()
+        timer:start(50, 0, vim.schedule_wrap(
+          function()
+            lualine.refresh({ place = { "statusline" } })
+          end)
+        )
+      end,
+    })
   end
 }
