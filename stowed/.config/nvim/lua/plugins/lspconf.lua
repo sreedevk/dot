d26296ea93b5ceb1a921d202801f71891b877f31
sreedev <sreedevpadmakumar@gmail.com>
2023-04-16 02:26:38 -0400

    neovim startup improvements: only 16 plugins loaded on startup

--- stowed/.config/nvim/lua/plugins/lspconf.lua
+++ stowed/.config/nvim/lua/plugins/lspconf.lua
@@ -1,6 +1,7 @@
 return {
   'VonHeikemen/lsp-zero.nvim',
   branch = "v2.x",
+  lazy = true,
   event = { "BufReadPost", "BufAdd", "BufNewFile" },
   dependencies = {
     'neovim/nvim-lspconfig',
--- stowed/.config/nvim/lua/plugins/treesitter.lua
+++ stowed/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  lazy = true,
+  event = "BufReadPost",
   dependencies = {
     'windwp/nvim-ts-autotag'
   },
