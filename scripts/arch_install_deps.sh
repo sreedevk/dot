08d0b7ddb50e249486275d10a21428d5404cf983
sreedev <sreedevpadmakumar@gmail.com>
2023-04-25 12:12:38 -0400

    updated arch linux deps

--- scripts/arch_install_deps.sh
+++ scripts/arch_install_deps.sh
@@ -2,4 +2,6 @@ sudo pacman -S --noconfirm \
   playerctl git zsh curl neovim bat stow exa dust tmux lf xplr fzf direnv \
   zoxide kitty alacritty emacs i3-wm gnu-netcat dunst \
   maim rofi polybar ttf-iosevka-nerd ttf-jetbrains-mono ttf-fira-code ttf-fira-mono \
-  ttf-firacode-nerd ttf-font-awesome brightnessctl picom dmenu
+  ttf-firacode-nerd ttf-font-awesome brightnessctl picom dmenu odt2txt chafa lynx \
+  mediainfo pdftoppm atool odt2txt irssi
+
