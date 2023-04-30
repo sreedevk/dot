b56f7a3aad982edf7ae5a800c81019f697d773b2
sreedev <sreedevpadmakumar@gmail.com>
2023-04-30 04:48:22 -0400

    i3 media keys update

--- scripts/arch_install_deps.sh
+++ scripts/arch_install_deps.sh
@@ -3,5 +3,5 @@ sudo pacman -S --noconfirm \
   zoxide kitty alacritty emacs i3-wm gnu-netcat dunst \
   maim rofi polybar ttf-iosevka-nerd ttf-jetbrains-mono ttf-fira-code ttf-fira-mono \
   ttf-firacode-nerd ttf-font-awesome brightnessctl picom dmenu odt2txt chafa lynx \
-  mediainfo pdftoppm atool odt2txt irssi
+  mediainfo pdftoppm atool odt2txt irssi playerctl
 
