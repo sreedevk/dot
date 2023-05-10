1afd5e63a9b175c8c5e2278a716530d426df56a8
sreedev <sreedevpadmakumar@gmail.com>
2023-05-09 20:03:38 -0400

    added volume dwmblock

--- stowed/.zsh/plugins.sh
+++ stowed/.zsh/plugins.sh
@@ -1,5 +1,4 @@
-source /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab/fzf-tab.plugin.zsh
-fpath+=( /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab )
+
 source /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 fpath+=( /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
 source /home/sreedev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mroth-SLASH-evalcache/evalcache.plugin.zsh
