ba48a3a8af779f2fb6cc7f73e0384e4c8cf10bb6
sreedev <sreedevpadmakumar@gmail.com>
2023-05-27 18:48:48 -0400

    neovim updates

--- stowed/.config/nvim/lua/plugins/catppuccin.lua
+++ stowed/.config/nvim/lua/plugins/catppuccin.lua
@@ -8,7 +8,7 @@ return {
         light = "latte",
         dark = "mocha",
       },
-      transparent_background = false,
+      transparent_background = true,
       show_end_of_buffer = false, -- show the '~' characters after the end of buffers
       term_colors = false,
       dim_inactive = {
