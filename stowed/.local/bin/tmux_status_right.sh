a403eec92bfb38857e315570b177ae9bea00200c
sreedev <sreedevpadmakumar@gmail.com>
2023-06-06 17:44:12 -0400

    conflict fixes

--- stowed/.local/bin/tmux_status_right.sh
+++ stowed/.local/bin/tmux_status_right.sh
@@ -5,16 +5,10 @@ LOCAL_TIME=$(date '+%d/%m/%Y %I:%M:%S %p %Z')
 OFFSHORE_TIME=$(TZ=Asia/Kolkata date '+%d/%m/%Y %I:%M:%S %p %Z') 
 
 # WIRELESS
-WIRELESS_INTERFACE=$(cat /proc/net/wireless | tail -n 1 | awk '{print $1}' | tr -d :)
-IP_ADDR="$(ip -f inet addr show $WIRELESS_INTERFACE | awk '/inet / {print $2}')"
-
-# BATTERY
-BATTERY_PERCENTAGE=$(upower -e | grep BAT0 | xargs -Ix upower -i x | grep percentage | awk '{print $2}')
-BATTERY_STATUS=$(upower -e | grep BAT0 | xargs -Ix upower -i x | grep state | awk '{print $2}')
-BATTERY_INFO="$BATTERY_PERCENTAGE $BATTERY_STATUS"
+WIRELESS_INTERFACE=$(ip link show | grep -E '^[0-9]' | awk '{print $2}' | tr -d : | head -n 2 | tail -n 1)
+IP_ADDR="$(ip -f inet addr show $WIRELESS_INTERFACE | grep inet | awk '{print $2}' | cut -f1 -d"/")"
 
 # CPU
-CPU_TEMPERATURES="$(sensors -u | grep input | grep temp | awk '{print $2}')"
-AVERAGE_CPU_TEMPERATURE="$(echo $CPU_TEMPERATURES | jq -s 'add / length | round')"
+CPU_TEMPERATURE="$(vcgencmd measure_temp | awk '{split($0,a,"="); print a[2]}')"
 
-echo "CPU TEMP: $AVERAGE_CPU_TEMPERATUREºC | $WIRELESS_INTERFACE: $IP_ADDR | BAT: $BATTERY_INFO | $OFFSHORE_TIME | $LOCAL_TIME"
+echo "CPU TEMP: $CPU_TEMPERATUREºC | $WIRELESS_INTERFACE: $IP_ADDR | $OFFSHORE_TIME | $LOCAL_TIME"
