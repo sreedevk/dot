305d9c0946094fe92594fcf33df33715e59770a7
sreedev <sreedevpadmakumar@gmail.com>
2023-11-22 01:30:10 -0500

    removed flash.nvim + added emacs copilot

--- stowed/.config/doom/config.el
+++ stowed/.config/doom/config.el
@@ -111,3 +111,11 @@
                  (org-remove-inline-images)
                  (org-present-show-cursor)
                  (org-present-read-write)))))
+
+;; accept completion from copilot and fallback to company
+(use-package! copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("C-e" . 'copilot-accept-completion)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-<tab>" . 'copilot-accept-completion-by-word)))
