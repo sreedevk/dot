2ce43379e3213718e4acf10f0502aaccfb3f178c
sreedev <sreedevpadmakumar@gmail.com>
2024-01-07 22:48:46 -0500

    neovim: cleaned up unused plugins

--- scripts/arch_install_deps.sh
+++
@@ -1,9 +0,0 @@
-sudo pacman -S --noconfirm \
-  playerctl git zsh curl neovim bat stow exa dust tmux lf xplr fzf direnv \
-  zoxide kitty alacritty emacs i3-wm gnu-netcat dunst \
-  maim rofi polybar ttf-iosevka-nerd ttf-jetbrains-mono ttf-fira-code ttf-fira-mono \
-  ttf-firacode-nerd ttf-font-awesome brightnessctl picom dmenu odt2txt chafa lynx \
-  mediainfo pdftoppm atool odt2txt irssi playerctl
-
-
-sudo aura -A autotiling
