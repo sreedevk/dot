4dc7cd01a7a79514fdec3db4e3f7d3d77b6399d5
sreedev <sreedevpadmakumar@gmail.com>
2024-01-27 13:31:25 -0500

    fzf preview improvements & fixes

--- stowed/.zsh/plugin-opts.sh
+++ stowed/.zsh/plugin-opts.sh
@@ -7,16 +7,18 @@ export FZF_TAB_GROUP_COLORS=(
     $'\033[38;5;100m' $'\033[38;5;98m' $'\033[91m' $'\033[38;5;80m' $'\033[92m' \
     $'\033[38;5;214m' $'\033[38;5;165m' $'\033[38;5;124m' $'\033[38;5;120m'
 )
-export FZF_DEFAULT_OPTS="
- --multi --no-height --extended --ansi
- --color=fg:#e0def4,hl:#6e6a86
- --color=fg+:#908caa,hl+:#908caa
- --color=info:#9ccfd8,prompt:#f6c177,pointer:#c4a7e7
- --color=marker:#ebbcba,spinner:#eb6f92,header:#ebbcba"
 
 zstyle ':fzf-tab:*' group-colors $FZF_TAB_GROUP_COLORS
 zstyle ':completion:*:git-checkout:*' sort false
+zstyle ':fzf-tab:complete:git-(add|diff|restore):*' fzf-preview 'git diff $word | delta'
 zstyle ':completion:*:descriptions' format '[%d]'
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
 zstyle ':fzf-tab:*' switch-group ',' '.'
+zstyle ':fzf-tab:complete:systemctl-*:*' fzf-preview 'SYSTEMD_COLORS=1 systemctl status $word'
+zstyle ':fzf-tab:complete:ls:*' fzf-preview 'bat --color=always ${(Q)realpath} 2> /dev/null'
+zstyle ':fzf-tab:complete:git-show:*' fzf-preview \
+	'case "$group" in
+	"commit tag") git show --color=always $word ;;
+	*) git show --color=always $word | delta ;;
+	esac'
