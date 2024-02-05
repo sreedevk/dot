3a750cd4c2281dc068b55b2a81421175db2de23c
sreedev <sreedevpadmakumar@gmail.com>
2024-02-04 20:52:42 -0500

    fixed issues in neovide rendering

--- stowed/.config/nvim/lua/neovide.lua
+++ stowed/.config/nvim/lua/neovide.lua
@@ -1,18 +1,19 @@
-if vim.g.neovide == true then
-  vim.g.neovide_transparency           = 0.75
-  vim.g.neovide_floating_blur_amount_x = 2.0
-  vim.g.neovide_floating_blur_amount_y = 2.0
-  vim.g.neovide_profiler               = false
-  vim.g.neovide_cursor_antialiasing    = true
-  -- vim.o.guifont                        = "VT323:h14"
-  vim.o.guifont                        = "Iosevka NF:h14"
-  vim.g.neovide_fullscreen             = false
-  vim.g.neovide_padding_top            = 0
-  vim.g.neovide_padding_bottom         = 0
-  vim.g.neovide_padding_right          = 0
-  vim.g.neovide_padding_left           = 0
-  vim.g.neovide_hide_mouse_when_typing = true
-  vim.g.neovide_scale_factor           = 1.0
+if vim.g.neovide then
+  vim.g.neovide_transparency             = 0.75
+  vim.g.neovide_floating_blur_amount_x   = 2.0
+  vim.g.neovide_floating_blur_amount_y   = 2.0
+  vim.g.neovide_profiler                 = false
+  vim.g.neovide_cursor_antialiasing      = true
+  vim.o.guifont                          = "Iosevka NF:h14"
+  vim.g.neovide_fullscreen               = false
+  vim.g.neovide_padding_top              = 0
+  vim.g.neovide_padding_bottom           = 0
+  vim.g.neovide_padding_right            = 0
+  vim.g.neovide_padding_left             = 0
+  vim.g.neovide_hide_mouse_when_typing   = true
+  vim.g.neovide_scale_factor             = 1.0
+  vim.g.neovide_scroll_animation_length  = 0.3
+  vim.g.neovide_refresh_rate             = 90
 
   local map                            = require('helpers').map
 
