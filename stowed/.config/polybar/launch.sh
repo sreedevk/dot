7fe3f7b86de44c2eb96017a2ced55a895cf8dc23
sreedev <sreedevpadmakumar@gmail.com>
2024-02-27 14:02:10 -0500

    monitor specific polybar font added

--- stowed/.config/polybar/launch.sh
+++ stowed/.config/polybar/launch.sh
@@ -3,8 +3,12 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar -q main -c ~/.config/polybar/config.ini & disown
+    if [[ "$m" == "DP-1-3" ]]; then
+      FONT="Iosevka NF:style=Bold:pixelsize=18;2" HEIGHT=52 MONITOR=$m polybar -q main -c ~/.config/polybar/config.ini & disown
+    else
+      FONT="Iosevka NF:style=Bold:pixelsize=12;2" MONITOR=$m polybar -q main -c ~/.config/polybar/config.ini & disown
+    fi
   done
 else
-  polybar -q main -c ~/.config/polybar/config.ini & disown
+  FONT="Iosevka NF:style=Bold:pixelsize=12;2" polybar -q main -c ~/.config/polybar/config.ini & disown
 fi
