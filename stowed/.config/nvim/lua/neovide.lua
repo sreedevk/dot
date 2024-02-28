af67e774593b073100897ef1bc604e44c7882903
sreedev <sreedevpadmakumar@gmail.com>
2024-02-27 19:23:35 -0500

    neovim transparency enabled
    (cherry picked from commit 2b2c1d4c6deef8cd2517f44223e1437a0c472a54)

--- stowed/.config/nvim/lua/neovide.lua
+++ stowed/.config/nvim/lua/neovide.lua
@@ -2,7 +2,7 @@ if vim.g.neovide then
   vim.g.neovide_transparency             = 0.5
   vim.g.neovide_floating_blur_amount_x   = 0.0
   vim.g.neovide_floating_blur_amount_y   = 0.0
-  vim.g.neovide_window_blurred           = false
+  vim.g.neovide_window_blurred           = true
   vim.g.neovide_profiler                 = false
   vim.g.neovide_cursor_antialiasing      = true
   vim.o.guifont                          = "Iosevka NF:h14"
