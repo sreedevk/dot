5e31ce34f5e07e11ac291e407669beb08b15c79b
sreedev <sreedevpadmakumar@gmail.com>
2024-02-27 19:23:35 -0500

    neovim transparency enabled
    (cherry picked from commit a700fef1c9c1c43949b7a8f6a7c1a7ff86de95d0)

--- stowed/.config/nvim/lua/plugins/nightfox.lua
+++ stowed/.config/nvim/lua/plugins/nightfox.lua
@@ -5,7 +5,7 @@ return {
       options = {
         compile_path = vim.fn.stdpath("cache") .. "/nightfox",
         compile_file_suffix = "_compiled",
-        transparent = false,
+        transparent = true,
         terminal_colors = true,
         dim_inactive = false,
         module_default = true,
