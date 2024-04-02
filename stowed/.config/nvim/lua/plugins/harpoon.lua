4b235569b4d380818a60aa928666db67fc10733a
sreedev <sreedevpadmakumar@gmail.com>
2024-04-02 19:42:26 -0400

    harpoon2 depricated config fixed

--- stowed/.config/nvim/lua/plugins/harpoon.lua
+++ stowed/.config/nvim/lua/plugins/harpoon.lua
@@ -4,9 +4,10 @@ return {
   dependencies = { { "nvim-lua/plenary.nvim" } },
   config = function()
     local harpoon = require("harpoon")
+
     harpoon.setup()
 
-    vim.keymap.set("n", "<Leader>fa", function() harpoon:list():append() end)
+    vim.keymap.set("n", "<Leader>fa", function() harpoon:list():add() end)
     vim.keymap.set("n", "<Leader>fl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
     vim.keymap.set("n", "<M-o>", function() harpoon:list():prev() end)
     vim.keymap.set("n", "<M-i>", function() harpoon:list():next() end)
