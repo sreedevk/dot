739edf9a52a369bdd2472e794cb9315aca0b0fa1
sreedev <sreedevpadmakumar@gmail.com>
2024-04-13 17:47:58 +0000

    Nix server

---
+++ nixos/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "NixOS System Configuration Management Flake for Multiple Hosts";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs } @ inputs:
+    let
+      system = "x86_64-linux";
+      mkSystem = pkgs: system: hostname:
+        pkgs.lib.nixosSystem {
+          system = system;
+          modules = [
+            (import ./hosts/${hostname}/configuration.nix)
+          ];
+          specialArgs = { inherit inputs; };
+        };
+    in
+    {
+      nixosConfigurations = {
+        nullptrderef1 = mkSystem inputs.nixpkgs system "nullptrderef1";
+      };
+    };
+}
---
+++ stowed/.zsh/plugins.sh
@@ -0,0 +1,8 @@
+source /home/admin/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab/fzf-tab.plugin.zsh
+fpath+=( /home/admin/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab )
+source /home/admin/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/admin/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/admin/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode/zsh-vi-mode.plugin.zsh
+fpath+=( /home/admin/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode )
+source /home/admin/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( /home/admin/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting )
