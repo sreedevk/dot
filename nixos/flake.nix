c94b735fecf2454d3ecf2a48b2a82531fcec0480
sreedev <sreedevpadmakumar@gmail.com>
2024-04-13 15:18:48 -0400

    testing secrets

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -8,13 +8,14 @@
   outputs = { self, nixpkgs } @ inputs:
     let
       system = "x86_64-linux";
+      secrets = builtins.fromJSON (builtins.readFile "${self}/secrets/secrets.json");
       mkSystem = pkgs: system: hostname:
         pkgs.lib.nixosSystem {
           system = system;
           modules = [
             (import ./hosts/${hostname}/configuration.nix)
           ];
-          specialArgs = { inherit inputs; };
+          specialArgs = { inherit inputs secrets; };
         };
     in
     {
