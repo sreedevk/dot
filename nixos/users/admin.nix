b080f7abae3b4658eb0c8d80287539fc479bd288
sreedev <sreedevpadmakumar@gmail.com>
2024-04-13 22:31:14 -0400

    removed nixos program configs

--- nixos/users/admin.nix
+++ nixos/users/admin.nix
@@ -1,58 +1,56 @@
 { configs, pkgs, ... }:
 {
-  home.username = "admin";
-  home.homeDirectory = "/home/admin";
-  home.stateVersion = "23.11";
 
-  home.packages = with pkgs; [
-    amfora
-    antibody
-    bat
-    btop
-    cava
-    delta
-    du-dust
-    duf
-    emacs
-    eza
-    fastfetch
-    fd
-    fzf
-    gh
-    git
-    git-crypt
-    glab
-    glow
-    gping
-    hexyl
-    hledger
-    htop
-    hyperfine
-    ipcalc
-    irssi
-    jaq
-    jq
-    lf
-    ncdu
-    neovim
-    newsboat
-    procs
-    ruby
-    stow
-    tailspin
-    taskwarrior
-    taskwarrior-tui
-    tmux
-    tokei
-    xh
-    xxd
-    yt-dlp
-    zoxide
-  ];
+  home = {
+    username = "admin";
+    homeDirectory = "/home/admin";
+    stateVersion = "23.11";
+    packages = with pkgs; [
+      amfora
+      antibody
+      bat
+      btop
+      cava
+      delta
+      du-dust
+      duf
+      emacs
+      eza
+      fastfetch
+      fd
+      fzf
+      gh
+      git
+      git-crypt
+      glab
+      glow
+      gping
+      hexyl
+      hledger
+      htop
+      hyperfine
+      ipcalc
+      irssi
+      jaq
+      jq
+      lf
+      ncdu
+      neovim
+      newsboat
+      procs
+      ruby
+      stow
+      tailspin
+      taskwarrior
+      taskwarrior-tui
+      tmux
+      tokei
+      xh
+      xxd
+      yt-dlp
+      zoxide
+    ];
+  };
 
   programs.home-manager.enable = true;
-
-  imports = [
-    ../programs
-  ];
 }
