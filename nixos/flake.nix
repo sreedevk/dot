b7fa6b002b0764b10251df3160845a9e4d80a055
sreedev <sreedevpadmakumar@gmail.com>
2024-04-13 20:06:11 -0400

    nix: home-manager added

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,24 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs } @ inputs:
+  outputs = { self, nixpkgs, home-manager } @ inputs:
     let
       system = "x86_64-linux";
       secrets = builtins.fromJSON (builtins.readFile "${self}/secrets/secrets.json");
-      mkSystem = pkgs: system: hostname:
+      mkSystem = pkgs: system: hostname: user:
         pkgs.lib.nixosSystem {
           system = system;
           modules = [
             (import ./hosts/${hostname}/configuration.nix)
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users."${user}" = (import ./hosts/${hostname}/home.nix);
+            }
           ];
           specialArgs = { inherit inputs secrets; };
         };
     in
     {
       nixosConfigurations = {
-        nullptrderef1 = mkSystem inputs.nixpkgs system "nullptrderef1";
+        nullptrderef1 = mkSystem inputs.nixpkgs system "nullptrderef1" "admin";
       };
     };
 }
