61c3397140084b530160d8594c5b288cdb63082c
sreedev <sreedevpadmakumar@gmail.com>
2024-04-24 15:56:44 -0400

    added firefly app

--- nixos/hosts/nullptrderef1/containers/default.nix
+++ nixos/hosts/nullptrderef1/containers/default.nix
@@ -608,5 +608,49 @@
         ports = [ "9000:9000" ];
         volumes = [ "${applicationConfigDir}/thelounge:/var/opt/thelounge" ];
       };
+
+      "firefly-db" = {
+        autoStart = true;
+        image = "mariadb:lts";
+        ports = [ "3306:3306" ];
+        extraOptions = [ "--add-host=nullptrderef1:${lanAddress}" ];
+        volumes = [
+          "${applicationConfigDir}/firefly/db:/var/lib/mysql"
+        ];
+        environment = {
+          MYSQL_RANDOM_ROOT_PASSWORD = "yes";
+          MYSQL_USER = "firefly";
+          MYSQL_PASSWORD = secrets.firefly.db.password;
+          MYSQL_DATABASE = "firefly";
+        };
+      };
+
+      "firefly-app" = {
+        autoStart = true;
+        image = "fireflyiii/core:latest";
+        extraOptions = [ "--add-host=nullptrderef1:${lanAddress}" ];
+        dependsOn = [ "firefly-db" ];
+        ports = [ "6003:8080" ];
+        volumes = [
+          "${applicationConfigDir}/firefly/uploads/:/var/www/html/storage/upload"
+        ];
+        environment = {
+          APP_ENV = "production";
+          SITE_OWNER = secrets.firefly.app.site_owner;
+          APP_KEY = secrets.firefly.app.secret;
+          TZ = timeZone;
+          PUID = adminUID;
+          PGID = adminGID;
+          DB_CONNECTION = "mysql";
+          DB_HOST = "nullptrderef1";
+          DB_PORT = "3306";
+          DB_DATABASE = "firefly";
+          DB_USERNAME = "firefly";
+          DB_PASSWORD = secrets.firefly.db.password;
+          MYSQL_USE_SSL = "false";
+          MYSQL_SSL_VERIFY_SERVER_CERT = "false";
+          APP_URL = "http://nullptrderef1";
+        };
+      };
     };
 }
