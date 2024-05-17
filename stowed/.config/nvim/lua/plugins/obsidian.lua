a3001dab72df37cff524b359755aa7f0f45f1c39
sreedev <sreedevpadmakumar@gmail.com>
2024-05-17 17:04:18 -0400

    nvim: obsidian improvements

--- stowed/.config/nvim/lua/plugins/obsidian.lua
+++ stowed/.config/nvim/lua/plugins/obsidian.lua
@@ -1,44 +1,13 @@
 return {
   "epwalsh/obsidian.nvim",
   version = "*",
-  lazy = true,
-  event = {
-    "BufReadPre " .. vim.fn.expand "~" .. "/Data/notebook/obsidian/notebook/**.md",
-    "BufNewFile " .. vim.fn.expand "~" .. "/Data/notebook/obsidian/notebook/**.md",
-  },
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
   opts = {
-    workspaces = {
-      {
-        name = "notebook",
-        path = "~/Data/notebook/obsidian/notebook/",
-      },
-    },
     completion = {
       nvim_cmp = true,
       min_chars = 2,
-
-      -- Where to put new notes created from completion. Valid options are
-      --  * "current_dir" - put new notes in same directory as the current buffer.
-      --  * "notes_subdir" - put new notes in the default notes subdirectory.
-      new_notes_location = "current_dir",
-
-      -- Control how wiki links are completed with these (mutually exclusive) options:
-      --
-      -- 1. Whether to add the note ID during completion.
-      -- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
-      -- Mutually exclusive with 'prepend_note_path' and 'use_path_only'.
-      prepend_note_id = false,
-      -- 2. Whether to add the note path during completion.
-      -- E.g. "[[Foo" completes to "[[notes/foo|Foo]]" assuming "notes/foo.md" is the path of the note.
-      -- Mutually exclusive with 'prepend_note_id' and 'use_path_only'.
-      prepend_note_path = true,
-      -- 3. Whether to only use paths during completion.
-      -- E.g. "[[Foo" completes to "[[notes/foo]]" assuming "notes/foo.md" is the path of the note.
-      -- Mutually exclusive with 'prepend_note_id' and 'prepend_note_path'.
-      use_path_only = false,
     },
     mappings = {
       -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
@@ -55,7 +24,34 @@ return {
         end,
         opts = { buffer = true },
       },
+      -- Smart action depending on context, either follow link or toggle checkbox.
+      ["<cr>"] = {
+        action = function()
+          return require("obsidian").util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      }
     },
-    finder = "telescope.nvim"
+    workspaces = {
+      {
+        name = "work",
+        path = "~/Data/Notebook/work"
+      },
+      {
+        name = "thinkbox",
+        path = "~/Data/Notebook/thinkbox"
+      },
+      {
+        name = "other",
+        path = "~/Data/Notebook/other"
+      }
+    },
+    picker = {
+      name = "telescope.nvim",
+      mappings = {
+        new = "<C-x>",
+        insert_link = "<C-l>"
+      },
+    }
   },
 }
