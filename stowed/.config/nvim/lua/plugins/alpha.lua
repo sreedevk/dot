c0d38217cd71503ee4b80887516da48fddbb9c0f
sreedev <sreedevpadmakumar@gmail.com>
2024-05-27 13:11:23 -0400

    removed obsidian references

--- stowed/.config/nvim/lua/plugins/alpha.lua
+++ stowed/.config/nvim/lua/plugins/alpha.lua
@@ -70,7 +70,6 @@ return {
       type = "group",
       val = {
         button("e", "New File", ":enew<cr>"),
-        button("n", "Notes", ":ObsidianQuickSwitch<cr>"),
         button("z", "Zoxide", ":Zi<CR>"),
         button("l", "Last Session", "<cmd>lua require('persistence').load({last=true})<cr>"),
         button("f", "Find Files", ":Telescope find_files<CR>"),
