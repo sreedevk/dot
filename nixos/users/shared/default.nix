d33361d0c00ff412397e3cd3190fcecfbd0aaa5d
sreedev <sreedevpadmakumar@gmail.com>
2024-06-09 11:50:38 -0400

    moved ssh to shared config

--- nixos/users/shared/default.nix
+++ nixos/users/shared/default.nix
@@ -1,5 +1,27 @@
 { pkgs, secrets, ... }:
 {
+  programs.ssh = {
+    enable = true;
+    extraConfig = ''
+      Host sree.dev
+        HostName sree.dev
+        User deploy
+        IdentityFile ~/.ssh/devtechnica
+
+      Host github.com
+        HostName github.com
+        User git
+        IdentityFile ~/.ssh/devtechnica
+        IdentitiesOnly yes
+
+      Host gitlab.com
+        HostName gitlab.com
+        User git
+        IdentityFile ~/.ssh/devtechnica
+        IdentitiesOnly yes
+    '';
+  };
+
   home.file = {
     ".taskrc" = {
       enable = true;
