afbf9783ee87880ed4f99088c25819657e1cd8f5
sreedev <sreedevpadmakumar@gmail.com>
2024-06-10 09:32:18 -0400

    moved autotiling service to specific user

--- nixos/users/shared/systemd.nix
+++ nixos/users/shared/systemd.nix
@@ -3,21 +3,6 @@
   systemd.user = {
     enable = true;
     services = {
-      autotiling = {
-        Unit = {
-          Description = "xorg based window manager autotiling service";
-          Documentation = "https://github.com/nwg-piotr/autotiling";
-        };
-        Service = {
-          Type = "simple";
-          ExecStart = "${pkgs.autotiling}/bin/autotiling";
-          Restart = "on-failure";
-          RestartSec = 3;
-        };
-        Install = {
-          WantedBy = [ "default.target" ];
-        };
-      };
       emacs = {
         Unit = {
           Description = "Emacs text editor";
