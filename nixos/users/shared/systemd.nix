8b62013f9596aa93c011b18081d1de025269a7bf
sreedev <sreedevpadmakumar@gmail.com>
2024-06-10 11:38:28 -0400

    taskwarrior client sync service enabled

--- nixos/users/shared/systemd.nix
+++ nixos/users/shared/systemd.nix
@@ -16,6 +16,30 @@
           Restart = "on-failure";
         };
       };
+
+      taskwarrior-sync = {
+        Unit = {
+          Description = "Taskwarrior3 Taskchampion Sync Job";
+          Documentation = "info:task man:task(1) https://taskwarrior.org/docs/";
+        };
+        Service = {
+          Type = "simple";
+          ExecStart = "${pkgs.taskwarrior3}/bin/task sync";
+          Restart = "on-failure";
+        };
+      };
+    };
+
+    timers = {
+      taskwarrior-sync-timers = {
+        Unit = {
+          Description = "Timer for Taskwarrior3 (TaskChampion) Sync Service";
+        };
+        Timer = {
+          OnBootSec = "5min";
+          OnUnitActiveSec = "30min";
+        };
+      };
     };
   };
 }
