31b0762f1e2b8cebbe9f7b0438deed0d47b5e974
sreedev <sreedevpadmakumar@gmail.com>
2024-06-09 20:47:33 -0400

    zsh performance improvements

--- stowed/.zsh/autoloads.sh
+++ stowed/.zsh/autoloads.sh
@@ -21,26 +21,7 @@ if command -v zoxide &> /dev/null; then
   eval "$(zoxide init zsh)"
 fi
 
-# LUAROCKS AUTOLOAD
-if command -v luarocks &> /dev/null; then
-  eval "$(luarocks path --bin)"
-fi
-
-# HASKELL AUTOLOADS
-if [ -f "$HOME/.ghcup/env" ]; then
-   source $HOME/.ghcup/env
-fi
-
 # RUST AUTOLOADS
 if [ -f "$HOME/.cargo/env" ]; then
   . $HOME/.cargo/env
-fi
-
-# GOLANG AUTOLOADS
-if command -v go &> /dev/null; then
-  eval "$(go env)"
-fi
-
-if command -v glow &> /dev/null; then
-  eval "$(glow completion zsh)"
 fi
