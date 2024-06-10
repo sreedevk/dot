b5fc2e1ea9207d3f5a6f8efb9ee9ffa4ae914aae
sreedev <sreedevpadmakumar@gmail.com>
2024-06-10 10:31:40 -0400

    home-manager: moved awscli to nix

--- nixos/users/sreedev.nix
+++ nixos/users/sreedev.nix
@@ -9,6 +9,7 @@
       amfora
       autorandr
       autotiling
+      awscli2
       bat
       broot
       btop
@@ -42,6 +43,22 @@
       };
       Install = {
         WantedBy = [ "default.target" ];
+      };
+    };
+  };
+
+  programs.awscli = {
+    enable = true;
+    settings = {
+      "default" = {
+        region = secrets.aws.config.region;
+        output = "json";
+      };
+    };
+    credentials = {
+      "default" = {
+        "aws_access_key_id" = secrets.aws.credentials.aws_access_key_id;
+        "aws_secret_access_key" = secrets.aws.credentials.aws_secret_access_key;
       };
     };
   };
