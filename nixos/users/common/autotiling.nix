dcbbbae08168686efbddd1631444478530f0f38e
sreedev <sreedevpadmakumar@gmail.com>
2024-06-14 20:26:12 +0000

    hm: advanced modularization of hm

---
+++ nixos/users/common/autotiling.nix
@@ -0,0 +1,19 @@
+{ pkgs, secrets, lib, inputs, system, ... }: {
+  systemd.user.services = {
+    autotiling = {
+      Unit = {
+        Description = "xorg based window manager autotiling service";
+        Documentation = "https://github.com/nwg-piotr/autotiling";
+      };
+      Service = {
+        Type = "simple";
+        ExecStart = "${pkgs.autotiling}/bin/autotiling";
+        Restart = "always";
+        RestartSec = 3;
+        StartLimitInterval = 0;
+        StartLimitBurst = 0;
+      };
+      Install = { WantedBy = [ "default.target" ]; };
+    };
+  };
+}
