5320dd208fa2ad30f81e342bc33154e6c704aafd
sreedev <sreedevpadmakumar@gmail.com>
2024-06-15 01:34:24 +0000

    tmux plugin config changes

--- nixos/users/common/packages/tmux.nix
+++ nixos/users/common/packages/tmux.nix
@@ -1,9 +1,21 @@
 { secrets, pkgs, ... }: {
   home.packages = with pkgs; [
-    tmux
-    tmuxPlugins.extrakto
-    tmuxPlugins.jump
-    tmuxPlugins.tmux-thumbs
-    tmuxPlugins.yank
   ];
+
+  programs.tmux = {
+    enable = true;
+    shell = "${pkgs.zsh}/bin/zsh";
+    terminal = "tmux-256color";
+    historyLimit = 100000;
+    plugins = with pkgs; [
+      tmuxPlugins.extrakto
+      tmuxPlugins.jump
+      tmuxPlugins.yank
+      tmuxPlugins.tmux-thumbs
+    ];
+    extraConfig = ''
+      set -g @thumbs-key U
+      set -g @thumbs-reverse enabled
+    '';
+  };
 }
