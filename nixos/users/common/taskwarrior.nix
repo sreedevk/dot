1084c0c63be8838aaa8c3734b5d81945442d2553
sreedev <sreedevpadmakumar@gmail.com>
2024-06-16 00:33:58 +0000

    hm: split systemd services into app specific modules

--- nixos/users/common/taskwarrior.nix
+++ nixos/users/common/taskwarrior.nix
@@ -27,4 +27,33 @@
       recursive = false;
     };
   };
+
+  systemd.user =
+    {
+      services = {
+        taskwarrior-sync = {
+          Unit = {
+            Description = "Taskwarrior3 Taskchampion Sync Job";
+            Documentation = "info:task man:task(1) https://taskwarrior.org/docs/";
+          };
+          Service = {
+            Type = "simple";
+            ExecStart = "${pkgs.taskwarrior3}/bin/task sync";
+            Restart = "always";
+          };
+        };
+      };
+
+      timers = {
+        taskwarrior-sync-timers = {
+          Unit = {
+            Description = "Timer for Taskwarrior3 (TaskChampion) Sync Service";
+          };
+          Timer = {
+            OnBootSec = "5min";
+            OnUnitActiveSec = "30min";
+          };
+        };
+      };
+    };
 }
