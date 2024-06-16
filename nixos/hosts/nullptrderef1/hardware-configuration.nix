00c5256ad32bc45645ce2e07a0ccd09482ab4aaa
sreedev <sreedevpadmakumar@gmail.com>
2024-06-16 17:12:42 -0400

    zfs experiments

--- nixos/hosts/nullptrderef1/hardware-configuration.nix
+++ nixos/hosts/nullptrderef1/hardware-configuration.nix
@@ -3,11 +3,14 @@
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
+  boot.supportedFilesystems = [ "zfs" ];
   boot.initrd.availableKernelModules =
     [ "xhci_pci" "ahci" "nvme" "usbhid" "uas" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+
+  boot.zfs.forceImportRoot = false;
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/66773c39-ea86-4b86-ae8b-31a4e56bf46b";
@@ -29,14 +32,22 @@
     fsType = "ext4";
   };
 
+  fileSystems."/mnt/media" =
+    { device = "datapool/media";
+      fsType = "zfs";
+    };
+
   boot.initrd.luks.devices."enc_data_drive".device =
     "/dev/disk/by-uuid/2123e639-909b-44ac-ac4a-6c896746d33f";
+
+  boot.zfs.extraPools = [ "datapool" ];
 
   swapDevices = [ ];
 
   networking.useDHCP = lib.mkDefault false;
   networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
   networking.interfaces.wlan0.useDHCP = lib.mkDefault true;
+  networking.hostId = "0ec79991";
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode =
