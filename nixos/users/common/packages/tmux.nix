c874de23e5e77e47ef865386b4f57facceddd388
sreedev <sreedevpadmakumar@gmail.com>
2024-06-18 21:41:53 +0000

    removed tmux from stylix and moved tmux config to tmux nix packages

--- nixos/users/common/packages/tmux.nix
+++ nixos/users/common/packages/tmux.nix
@@ -3,6 +3,7 @@
     enable = true;
     shell = "${pkgs.zsh}/bin/zsh";
     terminal = "tmux-256color";
+    extraConfig = builtins.readFile ../../../../stowed/.tmux.conf;
     historyLimit = 100000;
     plugins = with pkgs; [
       tmuxPlugins.extrakto
--- nixos/users/common/stylix.nix
+++ nixos/users/common/stylix.nix
@@ -50,7 +50,7 @@
       nushell.enable = true;
       rofi.enable = false;
       sxiv.enable = false;
-      tmux.enable = true;
+      tmux.enable = false;
       xresources.enable = true;
       zathura.enable = false;
       zellij.enable = true;
