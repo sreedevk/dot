6b3c467a0990a0e0c23cc0a49c432ecdd730d17c
sreedev <sreedevpadmakumar@gmail.com>
2024-06-19 22:40:27 +0000

    ensure re-encryption on error in run_with_creds.sh

--- scripts/run_with_creds.sh
+++ scripts/run_with_creds.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env zsh
 
-set -xe
+set -e
 
 if [[ -z $1 ]]; then
   echo "no inputs provided"
   return 1
 fi
 
-echo "running gc & decrypting secrets..."
+cleanup() {
+  echo "[RWC] re-encrypting before exit on error..."
+  git crypt lock
+}
+
+trap 'cleanup' EXIT
+
+echo "[RWC] running gc & decrypting secrets..."
 nix-collect-garbage &>/dev/null
 git crypt unlock
 
 eval $@
 
-echo "encrypting secrets & running gc..."
+echo "[RWC] encrypting secrets & running gc..."
 git crypt lock
 nix-collect-garbage &>/dev/null
