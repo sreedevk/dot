c3c82b6f423b038620c3a398dcc4e34c81ec8473
sreedev <sreedevpadmakumar@gmail.com>
2024-06-19 22:20:37 +0000

    server hardware config reorg

--- nixos/hosts/nullptrderef1/hardware-configuration.nix
+++ nixos/hosts/nullptrderef1/hardware-configuration.nix
@@ -3,24 +3,30 @@
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.supportedFilesystems = [ "zfs" ];
-  boot.initrd.availableKernelModules =
-    [ "xhci_pci" "ahci" "nvme" "usbhid" "uas" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    supportedFilesystems = [ "zfs" ];
+    availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "uas" "usb_storage" "sd_mod" ];
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
+    zfs.forceImportRoot = false;
+    initrd.luks.devices = {
+      "enc_data_drive" = {
+        device = "/dev/disk/by-uuid/2123e639-909b-44ac-ac4a-6c896746d33f";
+      };
+    };
+  };
 
-  boot.zfs.forceImportRoot = false;
 
   # NOTE: CORE OS PARTITIONS
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/66773c39-ea86-4b86-ae8b-31a4e56bf46b";
-    fsType = "ext4";
-  };
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/0B7B-5F96";
     fsType = "vfat";
+  };
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/66773c39-ea86-4b86-ae8b-31a4e56bf46b";
+    fsType = "ext4";
   };
 
   # NOTE: DATA PARTITIONS
@@ -33,9 +39,6 @@
     device = "/dev/disk/by-uuid/af7977f8-c99e-4b48-8a25-dc716233bfd5";
     fsType = "ext4";
   };
-
-  boot.initrd.luks.devices."enc_data_drive".device =
-    "/dev/disk/by-uuid/2123e639-909b-44ac-ac4a-6c896746d33f";
 
   # NOTE: USE THE CONFIG BELOW TO AUTO IMPORT 
   # ZPOOLS + ZFS DATAPOINTS AFTER CREATING
