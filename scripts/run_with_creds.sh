433a0011099cc513a764a0af88cac8503bb35151
sreedev <sreedevpadmakumar@gmail.com>
2024-06-20 01:49:11 +0000

    run_with_creds.sh simplifieid

--- scripts/run_with_creds.sh
+++ scripts/run_with_creds.sh
@@ -7,23 +7,9 @@ if [[ -z $1 ]]; then
   return 1
 fi
 
-git_lock_secret() {
-  encstat="$(git crypt status nixos/secrets)"
-  if [[ "$encstat" == *'not encrypted'* ]]; then
-    git crypt lock
-  fi
-}
-
-git_unlock_secret() {
-  encstat="$(git crypt status nixos/secrets)"
-  if [[ "$encstat" != *'not encrypted'* ]]; then
-    git crypt unlock
-  fi
-}
-
 cleanup() {
   echo "[RWC] re-encrypting before exit on error..."
-  git_lock_secret
+  git crypt lock || true
 }
 
 trap 'cleanup' EXIT
@@ -31,10 +17,10 @@ trap 'cleanup' SIGINT
 
 echo "[RWC] running gc & decrypting secrets..."
 nix-collect-garbage &>/dev/null
-git_unlock_secret
+git crypt unlock || true
 
 eval $@
 
 echo "[RWC] encrypting secrets & running gc..."
-git_lock_secret
+git crypt lock || true
 nix-collect-garbage &>/dev/null
