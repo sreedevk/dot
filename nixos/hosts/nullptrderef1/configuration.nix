00ec29fc8a0de2159dc2789cea4e4f416a0d61c0
sreedev <sreedevpadmakumar@gmail.com>
2024-06-22 19:35:31 -0400

    added prometheus

--- nixos/hosts/nullptrderef1/configuration.nix
+++ nixos/hosts/nullptrderef1/configuration.nix
@@ -257,6 +257,26 @@ in
         pools = [ "dpool0" ];
       };
     };
+
+    prometheus = {
+      enable = true;
+      port = 9001;
+      exporters = {
+        node = {
+          enable = true;
+          enabledCollectors = [ "systemd" ];
+          port = 9002;
+        };
+      };
+      scrapeConfigs = [
+        {
+          job_name = "chrysalis";
+          static_configs = [{
+            targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
+          }];
+        }
+      ];
+    };
   };
 
   security.sudo.wheelNeedsPassword = false;
