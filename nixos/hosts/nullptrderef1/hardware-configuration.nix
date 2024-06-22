b2bfb692105bf966d30bb8afc12f23c5d2ea0d0c
sreedev <sreedevpadmakumar@gmail.com>
2024-06-22 16:55:43 +0000

    zfs mountpoints data moved out of hwconfig

--- nixos/hosts/nullptrderef1/hardware-configuration.nix
+++ nixos/hosts/nullptrderef1/hardware-configuration.nix
@@ -28,42 +28,26 @@
     }
   ];
 
+  fileSystems =
+    let
+      zfs_mountpoints = secrets.system.mountpoints;
+      sys_mountpoints = {
+        "/boot" = { device = "/dev/disk/by-uuid/0B7B-5F96"; fsType = "vfat"; };
+        "/" = { device = "/dev/disk/by-uuid/66773c39-ea86-4b86-ae8b-31a4e56bf46b"; fsType = "ext4"; };
+      };
 
-  fileSystems = {
-    # NOTE: CORE OS PARTITIONS
-    "/boot" = { device = "/dev/disk/by-uuid/0B7B-5F96"; fsType = "vfat"; };
-    "/" = { device = "/dev/disk/by-uuid/66773c39-ea86-4b86-ae8b-31a4e56bf46b"; fsType = "ext4"; };
-
-    # NOTE: DATA PARTITIONS
-    "/mnt/dpool0/media" = { device = "dpool0/media"; fsType = "zfs"; };
-    "/mnt/dpool0/media/videos" = { device = "dpool0/media/videos"; fsType = "zfs"; };
-    "/mnt/dpool0/media/movies" = { device = "dpool0/media/movies"; fsType = "zfs"; };
-    "/mnt/dpool0/media/music" = { device = "dpool0/media/music"; fsType = "zfs"; };
-    "/mnt/dpool0/media/photos" = { device = "dpool0/media/photos"; fsType = "zfs"; };
-    "/mnt/dpool0/media/shows" = { device = "dpool0/media/shows"; fsType = "zfs"; };
-    "/mnt/dpool0/other" = { device = "dpool0/other"; fsType = "zfs"; };
-    "/mnt/dpool0/secrets" = { device = "dpool0/secrets"; fsType = "zfs"; };
-    "/mnt/dpool0/notes" = { device = "dpool0/notes"; fsType = "zfs"; };
-    "/mnt/dpool0/appdata" = { device = "dpool0/appdata"; fsType = "zfs"; };
-    "/mnt/dpool0/backups" = { device = "dpool0/backups"; fsType = "zfs"; };
-    "/mnt/dpool0/resources" = { device = "dpool0/resources"; fsType = "zfs"; };
-    "/mnt/dpool0/resources/llms" = { device = "dpool0/resources/llms"; fsType = "zfs"; };
-    "/mnt/dpool0/resources/databases" = { device = "dpool0/resources/databases"; fsType = "zfs"; };
-    "/mnt/dpool0/resources/wordlists" = { device = "dpool0/resources/wordlists"; fsType = "zfs"; };
-    "/mnt/dpool0/resources/other" = { device = "dpool0/resources/other"; fsType = "zfs"; };
-    "/mnt/dpool0/personal" = { device = "dpool0/personal"; fsType = "zfs"; };
-    "/mnt/dpool0/personal/documents" = { device = "dpool0/personal/documents"; fsType = "zfs"; };
-    "/mnt/dpool0/personal/finances" = { device = "dpool0/personal/finances"; fsType = "zfs"; };
-    "/mnt/dpool0/personal/projects" = { device = "dpool0/personal/projects"; fsType = "zfs"; };
-    "/mnt/dpool0/personal/archives" = { device = "dpool0/personal/archives"; fsType = "zfs"; };
-    "/mnt/dpool0/personal/other" = { device = "dpool0/personal/other"; fsType = "zfs"; };
-    "/mnt/dpool0/media/magazines" = { device = "dpool0/media/magazines"; fsType = "zfs"; };
-    "/mnt/dpool0/media/audiobooks" = { device = "dpool0/media/audiobooks"; fsType = "zfs"; };
-    "/mnt/dpool0/media/books" = { device = "dpool0/media/books"; fsType = "zfs"; };
-    "/mnt/dpool0/downloads" = { device = "dpool0/downloads"; fsType = "zfs"; };
-    "/mnt/dpool0/downloads/torrents" = { device = "dpool0/downloads/torrents"; fsType = "zfs"; };
-    "/mnt/dpool0/media/audio" = { device = "dpool0/media/audio"; fsType = "zfs"; };
-  };
+      mkzfsmounts = mountpoints:
+        builtins.map
+          (mountpoint: {
+            name = mountpoint.path;
+            value = {
+              device = mountpoint.device;
+              fsType = "zfs";
+            };
+          })
+          mountpoints;
+    in
+    sys_mountpoints // builtins.listToAttrs (mkzfsmounts zfs_mountpoints);
 
   networking = {
     useDHCP = lib.mkDefault false;
