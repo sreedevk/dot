7ce31ae9260a001b2f5ddf664ec4c0cf892188e4
sreedev <sreedevpadmakumar@gmail.com>
2024-06-23 11:00:28 -0400

    moved databases to different zfs dataset for performance

--- nixos/hosts/nullptrderef1/containers/firefly.nix
+++ nixos/hosts/nullptrderef1/containers/firefly.nix
@@ -4,26 +4,24 @@
       autoStart = true;
       image = "mariadb:lts";
       ports = [ "${secrets.firefly.database.port}:3306" ];
-      extraOptions =
-        [ "--add-host=nullptrderef1:${opts.lanAddress}" "--no-healthcheck" ];
-      volumes =
-        [ "${opts.paths.application_data}/firefly/db:/var/lib/mysql" ];
+      cmd = [ "--max-connections=512" ];
+      extraOptions = [ "--add-host=nullptrderef1:${opts.lanAddress}" "--no-healthcheck" ];
+      volumes = [ "${opts.paths.application_databases}/firefly:/var/lib/mysql" ];
       environment = {
-        MYSQL_RANDOM_ROOT_PASSWORD = "yes";
-        MYSQL_USER = secrets.firefly.database.username;
-        MYSQL_PASSWORD = secrets.firefly.database.password;
+        MARIADB_ROOT_PASSWORD = secrets.firefly.database.password;
         MYSQL_DATABASE = secrets.firefly.database.name;
-        TZ = opts.timeZone;
-        PUID = opts.adminUID;
+        MYSQL_PASSWORD = secrets.firefly.database.password;
+        MYSQL_USER = secrets.firefly.database.username;
         PGID = opts.adminGID;
+        PUID = opts.adminUID;
+        TZ = opts.timeZone;
       };
     };
 
     "firefly-app" = {
       autoStart = true;
       image = "fireflyiii/core:latest";
-      extraOptions =
-        [ "--add-host=nullptrderef1:${opts.lanAddress}" "--no-healthcheck" ];
+      extraOptions = [ "--add-host=nullptrderef1:${opts.lanAddress}" "--no-healthcheck" ];
       dependsOn = [ "firefly-db" ];
       ports = [ "${secrets.firefly.app.port}:8080" ];
       volumes = [
@@ -31,20 +29,20 @@
       ];
       environment = {
         APP_ENV = "production";
-        SITE_OWNER = secrets.firefly.app.site_owner;
         APP_KEY = secrets.firefly.app.secret;
-        TZ = opts.timeZone;
-        PUID = opts.adminUID;
-        PGID = opts.adminGID;
-        DB_CONNECTION = "mysql";
-        DB_HOST = secrets.firefly.database.host;
-        DB_PORT = secrets.firefly.database.port;
-        DB_DATABASE = secrets.firefly.database.name;
-        DB_USERNAME = secrets.firefly.database.username;
-        DB_PASSWORD = secrets.firefly.database.password;
-        MYSQL_USE_SSL = "false";
-        MYSQL_SSL_VERIFY_SERVER_CERT = "false";
         APP_URL = "http://${secrets.firefly.app.host}";
+        DB_CONNECTION = "mysql";
+        DB_DATABASE = secrets.firefly.database.name;
+        DB_HOST = secrets.firefly.database.host;
+        DB_PASSWORD = secrets.firefly.database.password;
+        DB_PORT = secrets.firefly.database.port;
+        DB_USERNAME = secrets.firefly.database.username;
+        MYSQL_SSL_VERIFY_SERVER_CERT = "false";
+        MYSQL_USE_SSL = "false";
+        PGID = opts.adminGID;
+        PUID = opts.adminUID;
+        SITE_OWNER = secrets.firefly.app.site_owner;
+        TZ = opts.timeZone;
       };
     };
   };
