58e6f7714fb0b72685dd7242743293027fa2c960
sreedev <sreedevpadmakumar@gmail.com>
2024-06-22 23:49:07 -0400

    moved icecast config to new module

--- nixos/hosts/nullptrderef1/configuration.nix
+++ nixos/hosts/nullptrderef1/configuration.nix
@@ -214,35 +214,6 @@ in
       alsa.support32Bit = true;
     };
 
-    icecast = {
-      enable = true;
-      hostname = "nullptrderef1";
-      listen = {
-        address = "0.0.0.0";
-        port = 8099;
-      };
-      admin = {
-        user = "admin";
-        password = secrets.icecast.password;
-      };
-      extraConf = ''
-        <authentication>
-           <source-password>${secrets.icecast.password}</source-password>
-           <relay-user>relay</relay-user>
-           <relay-password>${secrets.icecast.password}</relay-password>
-           <admin-user>admin</admin-user>
-           <admin-password>${secrets.icecast.password}</admin-password>
-        </authentication>
-        <mount type="normal">
-          <mount-name>/radio</mount-name>
-          <username>radiosource</username>
-          <password>${secrets.icecast.password}</password>
-          <dump-file>/tmp/dump-example1.ogg</dump-file>
-          <stream-name>New York City Radio Stream</stream-name>
-        </mount>
-      '';
-    };
-
     cockpit = {
       enable = true;
       port = 8008;
