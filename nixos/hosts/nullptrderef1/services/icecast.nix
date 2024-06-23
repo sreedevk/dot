290c75c3bdb3c181e1bf328cd5fdde763e779712
sreedev <sreedevpadmakumar@gmail.com>
2024-06-23 00:15:45 -0400

    reorg

--- nixos/hosts/nullptrderef1/services/icecast.nix
+++ nixos/hosts/nullptrderef1/services/icecast.nix
@@ -1,4 +1,16 @@
 { config, lib, pkgs, secrets, ... }: {
+  systemd.services.radio-streaming = {
+    description = "enable audio streaming from XHDATA D-328 Radio";
+    enable = true;
+    after = [ "network.target" ];
+    wants = [ "network.target" ];
+    serviceConfig = {
+      ExecStart =
+        "${pkgs.ffmpeg}/bin/ffmpeg -f alsa -ac 2 -ar 44100 -i hw:0 -acodec libmp3lame -b:a 128k -content_type audio/mpeg -f mp3 icecast://radiosource:${secrets.icecast.password}@0.0.0.0:8099/radio";
+      Restart = "always";
+    };
+  };
+
   services.icecast = {
     enable = true;
     hostname = "nullptrderef1";
