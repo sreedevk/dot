c11db698d6b40998a228dc058a909a11bb8d384a
sreedev <sreedevpadmakumar@gmail.com>
2024-06-23 00:47:04 -0400

    moved opts out to flakes

--- nixos/hosts/nullptrderef1/containers/default.nix
+++ nixos/hosts/nullptrderef1/containers/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, secrets, ... }: {
+{ config, lib, pkgs, secrets, opts, ... }: {
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
@@ -7,33 +7,7 @@
     autoPrune.enable = true;
   };
 
-  virtualisation.oci-containers.containers =
-    let
-      opts = {
-        applicationUserName = "nullptrderef1";
-        lanAddress = "192.168.1.179";
-        timeZone = "America/New_York";
-        adminUID = "1000";
-        adminGID = "100";
-        paths = {
-          application_data = "/mnt/dpool0/appdata";
-          downloads = "/mnt/dpool0/downloads";
-          magazines = "/mnt/dpool0/media/magazines";
-          qbt_images = "/mnt/dpool0/media/photos/other";
-          images = "/mnt/dpool0/media/photos";
-          torrent_watch = "/mnt/dpool0/downloads/torrents";
-          movies = "/mnt/dpool0/media/movies";
-          audiobooks = "/mnt/dpool0/media/audiobooks";
-          books = "/mnt/dpool0/media/books";
-          music = "/mnt/dpool0/media/music";
-          videos = "/mnt/dpool0/media/videos";
-          television = "/mnt/dpool0/media/shows";
-
-          podmanSocket = "/var/run/podman/podman.sock";
-        };
-      };
-    in
-    {
+  virtualisation.oci-containers.containers = {
       # Jellyfin Media Player
       "jellyfin" = {
         autoStart = true;
--- nixos/hosts/nullptrderef1/services/firewall.nix
+++ nixos/hosts/nullptrderef1/services/firewall.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, secrets, ... }: {
+{ config, pkgs, ... }: {
   networking.firewall = {
     enable = true;
     allowPing = false;
