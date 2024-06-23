04853b669ac3f07e3964adc09e2e093ae4271be9
sreedev <sreedevpadmakumar@gmail.com>
2024-06-23 19:14:32 -0400

    homer migrated

--- nixos/opts.nix
+++ nixos/opts.nix
@@ -37,6 +37,7 @@
     filebrowser.app_port = "6660";
     rss-bridge.app_port = "8768";
     homebox.app_port = "3100";
+    homer.app_port = "80";
     farfalle = {
       app_port = "3199";
       api_port = "8199";
