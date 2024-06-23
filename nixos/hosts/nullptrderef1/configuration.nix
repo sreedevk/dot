247070347be69dfe6f0e111dc1db0b9e9a46d263
sreedev <sreedevpadmakumar@gmail.com>
2024-06-22 23:53:10 -0400

    moved telemetry configs to new module

--- nixos/hosts/nullptrderef1/configuration.nix
+++ nixos/hosts/nullptrderef1/configuration.nix
@@ -228,67 +228,6 @@ in
         pools = [ "dpool0" ];
       };
     };
-
-    netdata = {
-      enable = true;
-      config = {
-        global = {
-          "update every" = 15;
-        };
-        ml = {
-          "enabled" = "yes";
-        };
-      };
-      configDir = {
-        "stream.conf" = pkgs.writeText "stream.conf" ''
-          [stream]
-            enabled = yes
-            destination = nullptrderef1:19999
-            api key = ${secrets.netdata.api_key}
-          [UUID]
-            enabled = yes
-        '';
-      };
-    };
-
-    prometheus = {
-      enable = true;
-      port = 9001;
-      exporters = {
-        node = {
-          enable = true;
-          enabledCollectors = [
-            "systemd"
-            "zfs"
-          ];
-          port = 9002;
-          openFirewall = true;
-        };
-      };
-      scrapeConfigs = [
-        {
-          job_name = "nullptrderef1";
-          static_configs = [{
-            targets = [ "0.0.0.0:${toString config.services.prometheus.exporters.node.port}" ];
-          }];
-        }
-      ];
-    };
-
-    grafana = {
-      enable = true;
-      settings = {
-        security = {
-          admin_password = "$__file{${pkgs.writeText "grafpass" secrets.grafana.password}}";
-        };
-        server = {
-          domain = "nullptrderef1";
-          protocol = "http";
-          http_port = 2442;
-          http_addr = "0.0.0.0";
-        };
-      };
-    };
   };
 
   security.sudo.wheelNeedsPassword = false;
--- nixos/hosts/nullptrderef1/services/default.nix
+++ nixos/hosts/nullptrderef1/services/default.nix
@@ -1,3 +1,9 @@
 { config, lib, pkgs, secrets, ... }: {
-  imports = [ ./adguard.nix ./mullvad.nix ./firewall.nix ./icecast.nix ];
+  imports = [
+    ./adguard.nix
+    ./mullvad.nix
+    ./firewall.nix
+    ./icecast.nix
+    ./telemetry.nix
+  ];
 }
