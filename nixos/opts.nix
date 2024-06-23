c01fefc23f1f1801ea5378638d6cbb3afabaa3fc
sreedev <sreedevpadmakumar@gmail.com>
2024-06-23 12:04:00 -0400

    extracted configurable opts to opts.nix

---
+++ nixos/opts.nix
@@ -0,0 +1,23 @@
+{
+  applicationUserName = "nullptrderef1";
+  lanAddress = "192.168.1.179";
+  timeZone = "America/New_York";
+  adminUID = "1000";
+  adminGID = "100";
+  paths = {
+    application_data = "/mnt/dpool0/appdata";
+    application_databases = "/mnt/dpool0/appdata/databases";
+    downloads = "/mnt/dpool0/downloads";
+    magazines = "/mnt/dpool0/media/magazines";
+    qbt_images = "/mnt/dpool0/media/photos/other";
+    images = "/mnt/dpool0/media/photos";
+    torrent_watch = "/mnt/dpool0/downloads/torrents";
+    movies = "/mnt/dpool0/media/movies";
+    audiobooks = "/mnt/dpool0/media/audiobooks";
+    books = "/mnt/dpool0/media/books";
+    music = "/mnt/dpool0/media/music";
+    videos = "/mnt/dpool0/media/videos";
+    television = "/mnt/dpool0/media/shows";
+    podmanSocket = "/var/run/podman/podman.sock";
+  };
+}
