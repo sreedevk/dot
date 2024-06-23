9218a74fb9a8c0c385a80a32a926284c35ad8a71
sreedev <sreedevpadmakumar@gmail.com>
2024-06-23 12:55:35 -0400

    linkding issue fixes

--- nixos/hosts/nullptrderef1/containers/linkding.nix
+++ nixos/hosts/nullptrderef1/containers/linkding.nix
@@ -6,8 +6,8 @@
       extraOptions =
         [ "--add-host=nullptrderef1:${opts.lanAddress}" "--no-healthcheck" ];
       volumes =
-        [ "${opts.paths.application_data}/Linkding:/etc/linkding/data" ];
-      ports = [ "9090:9090" ];
+        [ "${opts.paths.application_data}/linkding:/etc/linkding/data" ];
+      ports = [ "${secrets.linkding.app.port}:9090" ];
       environment = {
         TZ = opts.timeZone;
         PUID = opts.adminUID;
