e8e25c92dd53a09f6f5fcd4f0d91f40c16013174
sreedev <sreedevpadmakumar@gmail.com>
2024-06-23 00:15:45 -0400

    reorg

--- nixos/hosts/nullptrderef1/services/firewall.nix
+++ nixos/hosts/nullptrderef1/services/firewall.nix
@@ -8,61 +8,59 @@
       53
       80
       443
-      2342
-      2442
-      3001
-      3100
-      3134
-      3333
-      4533
-      4848
-      5000
-      5055
-      5299
-      6003
-      6660
-      6767
-      6800
-      6880
-      6881
-      6969
-      7474
-      7575
-      7777
-      7878
-      8000
-      8001
-      8004
-      8008
-      8024
-      8080
-      8081
-      8083
-      8096
-      8099
-      8181
-      8191
-      8686
-      8787
-      8808
-      8989
-      9000
-      9001
-      9002
-      9080
-      9090
-      9117
-      9443
-      9696
-      9801
-      9999
-      11434
-      13378
-      18999
-      19999
-      32400
-      53589
+      2342  # PhotoPrism
+      2442  # Grafana
+      3001  # Uptime Kuma
+      3100  # HomeBox
+      3134  # Ollama Web
+      3333  # Huginn
+      4533  # Navidrome
+      5000  # Kavita
+      5055  # JellySeer
+      6003  # Firefly App
+      6660  # FileBrowser
+      6767  # Bazarr
+      6800  # Aria2 (RPC)
+      6880  # Aria2 (Web)
+      6881  # qBittorrent Nox
+      6969  # aar
+      7474  # AutoBrr
+      7777  # Ntfy
+      7878  # Radarr
+      8000  # Adguard
+      8001  # qBittorrent WebUI
+      8004  # OpenBooks
+      8008  # Cockpit
+      8024  # Portrainer 1
+      8080  # TaskChampion Sync Server
+      8081  # MeTube
+      8096  # JellyFin
+      8099  # IceCast
+      8191  # FlareSolverr
+      8686  # Lidarr
+      8787  # Readarr
+      8808  # FreshRSS
+      8989  # Sonarr
+      9000  # TheLounge
+      9001  # Prometheus
+      9002  # Prometheus Node
+      9080  # Portrainer 2
+      9090  # Linkding
+      9117  # Jackett
+      9443  # Portrainer 3
+      9696  # Prowlarr
+      9801  # VaultWarden
+      11434 # Ollama API
+      13378 # AudioBook Shelf
+      19999 # NetData
     ];
-    allowedUDPPorts = [ 21 22 53 6881 8096 13378 ];
+    allowedUDPPorts = [
+      21
+      22
+      53
+      6881  # qBittorrent Nox
+      8096  # JellyFin
+      13378 # AudioBook Shelf
+    ];
   };
 }
