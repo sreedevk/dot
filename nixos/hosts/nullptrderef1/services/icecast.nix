0718c00f26b7b0f173e4f3764081fc0dc672d1eb
sreedev <sreedevpadmakumar@gmail.com>
2024-06-24 10:55:16 -0400

    netdata + icecast migrated

--- nixos/hosts/nullptrderef1/services/icecast.nix
+++ nixos/hosts/nullptrderef1/services/icecast.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, secrets, ... }: {
+{ config, pkgs, secrets, opts, ... }: {
   systemd.services.radio-streaming = {
     description = "enable audio streaming from XHDATA D-328 Radio";
     enable = true;
@@ -6,7 +6,7 @@
     wants = [ "network.target" ];
     serviceConfig = {
       ExecStart =
-        "${pkgs.ffmpeg}/bin/ffmpeg -f alsa -ac 2 -ar 44100 -i hw:0 -acodec libmp3lame -b:a 128k -content_type audio/mpeg -f mp3 icecast://radiosource:${secrets.icecast.password}@0.0.0.0:8099/radio";
+        "${pkgs.ffmpeg}/bin/ffmpeg -f alsa -ac 2 -ar 44100 -i hw:0 -acodec libmp3lame -b:a 128k -content_type audio/mpeg -f mp3 icecast://radiosource:${secrets.icecast.password}@0.0.0.0:${opts.ports.icecast}/radio";
       Restart = "always";
     };
   };
@@ -16,7 +16,7 @@
     hostname = "nullptrderef1";
     listen = {
       address = "0.0.0.0";
-      port = 8099;
+      port = opts.ports.icecast;
     };
     admin = {
       user = "admin";
