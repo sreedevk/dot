cdbc36c89ead9f08e1c3eb2b6938c6d1249ea8c3
sreedev <sreedevpadmakumar@gmail.com>
2024-06-25 08:32:19 -0400

    port reorg

--- nixos/hosts/nullptrderef1/containers/portrainer.nix
+++ nixos/hosts/nullptrderef1/containers/portrainer.nix
@@ -5,7 +5,7 @@
       image = "portainer/portainer-ce:latest";
       extraOptions =
         [ "--add-host=nullptrderef1:${opts.lanAddress}" "--no-healthcheck" ];
-      ports = [ "8024:8000" "${opts.ports.potrainer_web_secure}:9443" "${opts.ports.portrainer_web}:9000" ];
+      ports = [ "${opts.ports.portrainer_misc}:8000" "${opts.ports.portrainer_web_secure}:9443" "${opts.ports.portrainer_web}:9000" ];
       volumes = [
         "${opts.paths.podmanSocket}:/var/run/docker.sock"
         "${opts.paths.application_data}/Portrainer:/data"
