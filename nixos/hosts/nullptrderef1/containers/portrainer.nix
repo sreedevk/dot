24f2f29ce3625bfdc913c3a2fe6ea4ed72149671
sreedev <sreedevpadmakumar@gmail.com>
2024-06-25 08:23:13 -0400

    adguard + portrainer migrated

--- nixos/hosts/nullptrderef1/containers/portrainer.nix
+++ nixos/hosts/nullptrderef1/containers/portrainer.nix
@@ -5,7 +5,7 @@
       image = "portainer/portainer-ce:latest";
       extraOptions =
         [ "--add-host=nullptrderef1:${opts.lanAddress}" "--no-healthcheck" ];
-      ports = [ "8024:8000" "9443:9443" "9080:9000" ];
+      ports = [ "8024:8000" "${opts.ports.potrainer_web_secure}:9443" "${opts.ports.portrainer_web}:9000" ];
       volumes = [
         "${opts.paths.podmanSocket}:/var/run/docker.sock"
         "${opts.paths.application_data}/Portrainer:/data"
