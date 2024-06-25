ebf44068e7665c05ce7bf5f7760f70c5b7018530
sreedev <sreedevpadmakumar@gmail.com>
2024-06-25 08:32:19 -0400

    port reorg

--- nixos/hosts/nullptrderef1/services/firewall.nix
+++ nixos/hosts/nullptrderef1/services/firewall.nix
@@ -7,77 +7,77 @@ in
     enable = true;
     allowPing = false;
     allowedTCPPorts = builtins.map mkPort [
+      opts.ports.adguard_dns
+      opts.ports.adguard_web
       opts.ports.archivebox
+      opts.ports.aria_rpc
+      opts.ports.aria_web
       opts.ports.audiobookshelf
       opts.ports.autobrr
       opts.ports.baikal
       opts.ports.bazarr
       opts.ports.cloudbeaver
+      opts.ports.cockpit
       opts.ports.docuseal
+      opts.ports.farfalle_api
+      opts.ports.farfalle_app
       opts.ports.filebrowser
+      opts.ports.firefly_app
+      opts.ports.firefly_db
       opts.ports.flaresolverr
       opts.ports.freshrss
+      opts.ports.ftp
+      opts.ports.grafana
       opts.ports.homebox
       opts.ports.homer
+      opts.ports.https
       opts.ports.huginn
+      opts.ports.icecast
       opts.ports.jackett
       opts.ports.jellyfin
       opts.ports.jellyseer
       opts.ports.kavita
+      opts.ports.lidarr
       opts.ports.linkding
       opts.ports.memos
       opts.ports.metube
       opts.ports.navidrome
+      opts.ports.netdata
       opts.ports.ntfy
       opts.ports.olivetin
       opts.ports.ollama-api
       opts.ports.ollama-web
       opts.ports.openbooks
+      opts.ports.photoprism_app
+      opts.ports.photoprism_db
       opts.ports.plex
+      opts.ports.portrainer_misc
+      opts.ports.portrainer_web
+      opts.ports.portrainer_web_secure
+      opts.ports.prometheus_app
+      opts.ports.prometheus_node
       opts.ports.prowlarr
+      opts.ports.qbittorrent-p2p
+      opts.ports.qbittorrent-web
       opts.ports.radarr
       opts.ports.readarr
       opts.ports.rss-bridge
       opts.ports.searxng
       opts.ports.sonarr
+      opts.ports.ssh
+      opts.ports.taskchampion
       opts.ports.thelounge
       opts.ports.uptime-kuma
       opts.ports.vaultwarden
       opts.ports.znc
-      opts.ports.firefly_db
-      opts.ports.firefly_app
-      opts.ports.farfalle_api
-      opts.ports.farfalle_app
-      opts.ports.aria_web
-      opts.ports.aria_rpc
-      opts.ports.photoprism_app
-      opts.ports.photoprism_db
-      opts.ports.grafana
-      opts.ports.prometheus_node
-      opts.ports.prometheus_app
-      opts.ports.lidarr
-      opts.ports.qbittorrent-web
-      opts.ports.qbittorrent-p2p
-      opts.ports.netdata
-      opts.ports.icecast
-      opts.ports.adguard_web
-      opts.ports.adguard_dns
-      opts.ports.portrainer_web
-      opts.ports.potrainer_web_secure
-      21
-      22
-      443
-      8008 # Cockpit
-      8024 # Portrainer 1
-      8080 # TaskChampion Sync Server
     ];
     allowedUDPPorts = builtins.map mkPort [
+      opts.ports.adguard_dns
       opts.ports.audiobookshelf
+      opts.ports.ftp
       opts.ports.jellyfin
       opts.ports.qbittorrent-p2p
-      opts.ports.adguard_dns
-      21
-      22
+      opts.ports.ssh
     ];
   };
 }
