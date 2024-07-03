a6dc26543931bcba1c6fed0df85d61ef666f5fe7
sreedev <sreedevpadmakumar@gmail.com>
2024-07-02 22:35:36 -0400

    nix: gitea db added
    (cherry picked from commit 076be3f44ac2ab5f66b6894330e172620198f177)

--- nixos/hosts/nullptrderef1/containers/gitea.nix
+++ nixos/hosts/nullptrderef1/containers/gitea.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, secrets, opts, ... }: {
   virtualisation.oci-containers.containers = {
-    "gitea" = {
+    "gitea-app" = {
       autoStart = true;
       image = "gitea/gitea:latest";
       extraOptions =
@@ -17,6 +17,24 @@
         "${opts.ports.gitea_http}:3000"
         "${opts.ports.gitea_ssh}:22"
       ];
+    };
+  };
+
+  "gitea-db" = {
+    autoStart = true;
+    image = "mariadb:lts";
+    ports = [ "${opts.ports.gitea_db}:3306" ];
+    cmd = [ "--max-connections=512" ];
+    extraOptions = [ "--add-host=nullptrderef1:${opts.lanAddress}" "--no-healthcheck" ];
+    volumes = [ "${opts.paths.application_databases}/gitea:/var/lib/mysql" ];
+    environment = {
+      MARIADB_ROOT_PASSWORD = secrets.gitea_database_password;
+      MYSQL_DATABASE = secrets.gitea_database_name;
+      MYSQL_PASSWORD = secrets.gitea_database_password;
+      MYSQL_USER = secrets.gitea_database_username;
+      PGID = opts.adminGID;
+      PUID = opts.adminUID;
+      TZ = opts.timeZone;
     };
   };
 }
