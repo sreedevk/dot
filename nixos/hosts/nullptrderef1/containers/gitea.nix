1bccaff039ec722f266272bc2b13aea6eeb64dc4
sreedev <sreedevpadmakumar@gmail.com>
2024-07-03 02:45:30 +0000

    nix: gitea db changed from sqlite to mysql

--- nixos/hosts/nullptrderef1/containers/gitea.nix
+++ nixos/hosts/nullptrderef1/containers/gitea.nix
@@ -9,6 +9,11 @@
         TZ = opts.timeZone;
         PUID = opts.adminUID;
         PGID = opts.adminGID;
+        GITEA__database__DB_TYPE = "mysql";
+        GITEA__database__HOST = "db:${opts.ports.gitea_db}";
+        GITEA__database__NAME = secrets.gitea_database_name;
+        GITEA__database__USER = secrets.gitea_database_username;
+        GITEA__database__PASSWD = secrets.gitea_database_password;
       };
       volumes = [
         "${opts.paths.application_data}/gitea:/data"
@@ -18,23 +23,22 @@
         "${opts.ports.gitea_ssh}:22"
       ];
     };
-  };
-
-  "gitea-db" = {
-    autoStart = true;
-    image = "mariadb:lts";
-    ports = [ "${opts.ports.gitea_db}:3306" ];
-    cmd = [ "--max-connections=512" ];
-    extraOptions = [ "--add-host=nullptrderef1:${opts.lanAddress}" "--no-healthcheck" ];
-    volumes = [ "${opts.paths.application_databases}/gitea:/var/lib/mysql" ];
-    environment = {
-      MARIADB_ROOT_PASSWORD = secrets.gitea_database_password;
-      MYSQL_DATABASE = secrets.gitea_database_name;
-      MYSQL_PASSWORD = secrets.gitea_database_password;
-      MYSQL_USER = secrets.gitea_database_username;
-      PGID = opts.adminGID;
-      PUID = opts.adminUID;
-      TZ = opts.timeZone;
+    "gitea-db" = {
+      autoStart = true;
+      image = "mariadb:lts";
+      ports = [ "${opts.ports.gitea_db}:3306" ];
+      cmd = [ "--max-connections=512" ];
+      extraOptions = [ "--add-host=nullptrderef1:${opts.lanAddress}" "--no-healthcheck" ];
+      volumes = [ "${opts.paths.application_databases}/gitea:/var/lib/mysql" ];
+      environment = {
+        MARIADB_ROOT_PASSWORD = secrets.gitea_database_password;
+        MYSQL_DATABASE = secrets.gitea_database_name;
+        MYSQL_PASSWORD = secrets.gitea_database_password;
+        MYSQL_USER = secrets.gitea_database_username;
+        PGID = opts.adminGID;
+        PUID = opts.adminUID;
+        TZ = opts.timeZone;
+      };
     };
   };
 }
