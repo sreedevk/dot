c09e2017be9821aa94dd6e6257b40c49a91edc67
sreedev <sreedevpadmakumar@gmail.com>
2024-07-04 02:41:16 +0000

    arch: keybase migrated to nix

--- nixos/users/sreedev.nix
+++ nixos/users/sreedev.nix
@@ -6,6 +6,7 @@
     ./common/dunst.nix
     ./common/firefox.nix
     ./common/fontconfig.nix
+    ./common/keybase.nix
     ./common/keyboard.nix
     ./common/misc.nix
     ./common/packages/cli.nix
