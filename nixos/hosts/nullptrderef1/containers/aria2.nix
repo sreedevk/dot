86947d1d5fc6fdb45d0ba47ef61b67e5ccdfbe43
sreedev <sreedevpadmakumar@gmail.com>
2024-07-04 22:59:30 +0000

    aria2 running in privileged mode

--- nixos/hosts/nullptrderef1/containers/aria2.nix
+++ nixos/hosts/nullptrderef1/containers/aria2.nix
@@ -4,7 +4,7 @@
       autoStart = true;
       image = "hurlenko/aria2-ariang";
       extraOptions =
-        [ "--add-host=nullptrderef1:${opts.lanAddress}" "--no-healthcheck" ];
+        [ "--add-host=nullptrderef1:${opts.lanAddress}" "--no-healthcheck" "--privileged" ];
       ports = [ "${opts.ports.aria_web}:8080" "${opts.ports.aria_rpc}:6800" ];
       volumes = [
         "${opts.paths.downloads}/Aria2:/aria2/data"
