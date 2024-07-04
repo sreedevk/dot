9e78b23d6aafba7198909a9e362137a08afd82da
sreedev <sreedevpadmakumar@gmail.com>
2024-07-04 03:29:48 +0000

    general cleanup

--- nixos/users/pi.nix
+++ nixos/users/pi.nix
@@ -1,8 +1,8 @@
 { configs, pkgs, secrets, username, ... }: {
   imports = [
+    ./common/core-packages.nix
     ./common/misc.nix
     ./common/neovim.nix
-    ./common/packages/cli.nix
     ./common/ssh.nix
     ./common/taskwarrior.nix
     ./common/tmux.nix
--- nixos/users/sreedev.nix
+++ nixos/users/sreedev.nix
@@ -3,18 +3,34 @@
     ./common/autorandr.nix
     ./common/autotiling.nix
     ./common/awscli.nix
+    ./common/core-packages.nix
     ./common/dunst.nix
     ./common/firefox.nix
     ./common/fontconfig.nix
     ./common/keybase.nix
     ./common/keyboard.nix
     ./common/misc.nix
-    ./common/packages/cli.nix
-    ./common/packages/gui.nix
     ./common/ssh.nix
     ./common/stylix.nix
     ./common/taskwarrior.nix
     ./common/tmux.nix
+    ./common/x86-packages.nix
     ./common/zsh.nix
+  ];
+
+  home.packages = with pkgs; [
+    arandr
+    autorandr
+    autotiling
+    awscli2
+    cinnamon.nemo-with-extensions
+    dbeaver-bin
+    droidcam
+    dunst
+    emacs
+    floorp
+    jira-cli-go
+    spotify
+    (nerdfonts.override { fonts = [ "Iosevka" ]; })
   ];
 }
