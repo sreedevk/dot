e9b188c58093a05912bca118f7a5d016dc585179
sreedev <sreedevpadmakumar@gmail.com>
2024-07-07 01:12:35 +0000

    added deploy user

---
+++ nixos/users/deploy.nix
@@ -0,0 +1,11 @@
+{ pkgs, secrets, lib, inputs, opts, system, username, ... }: {
+
+  imports = [
+    ./common/core-packages.nix
+    ./common/keybase.nix
+    ./common/misc.nix
+    ./common/neovim.nix
+    ./common/tmux.nix
+    ./common/zsh.nix
+  ];
+}
