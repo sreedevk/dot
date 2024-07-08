9c4641148a76c30d797e4c7c01ea63717ce6af83
sreedev <sreedevpadmakumar@gmail.com>
2024-07-08 03:23:58 +0000

    fixed typo in nextcloud setup

--- nixos/hosts/nullptrderef1/containers/nextcloud.nix
+++ nixos/hosts/nullptrderef1/containers/nextcloud.nix
@@ -1,24 +1,26 @@
 { config, lib, pkgs, secrets, opts, ... }: {
-  autoStart = true;
-  image = "nextcloud/all-in-one:latest";
-  extraOptions = [
-    "--add-host=nullptrderef1:${opts.lanAddress}"
-    "--no-healthcheck"
-    "--sig-proxy=false"
-  ];
-  ports = [
-    "${opts.ports.nextcloud-http}:80"
-    "${opts.ports.nextcloud-https}:8443"
-    "${opts.ports.nextcloud-app}:8080"
-  ];
-  volumes = [
-    "${opts.paths.podmanSocket}:/var/run/docker.sock:ro"
-    "${opts.paths.application_data}/nextcloud:/mnt/docker-aio-config"
-  ];
+  nextcloud = {
+    autoStart = true;
+    image = "nextcloud/all-in-one:latest";
+    extraOptions = [
+      "--add-host=nullptrderef1:${opts.lanAddress}"
+      "--no-healthcheck"
+      "--sig-proxy=false"
+    ];
+    ports = [
+      "${opts.ports.nextcloud-http}:80"
+      "${opts.ports.nextcloud-https}:8443"
+      "${opts.ports.nextcloud-app}:8080"
+    ];
+    volumes = [
+      "${opts.paths.podmanSocket}:/var/run/docker.sock:ro"
+      "${opts.paths.application_data}/nextcloud:/mnt/docker-aio-config"
+    ];
 
-  environment = {
-    TZ = opts.timeZone;
-    PUID = opts.adminUID;
-    PGID = opts.adminGID;
+    environment = {
+      TZ = opts.timeZone;
+      PUID = opts.adminUID;
+      PGID = opts.adminGID;
+    };
   };
 }
