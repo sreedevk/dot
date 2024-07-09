6ce5f71764de0df6a13a311068ea462ab3e1d01b
sreedev <sreedevpadmakumar@gmail.com>
2024-07-09 16:47:58 +0000

    added: joplin, podgrab, tubearchivist & gotify

---
+++ nixos/hosts/nullptrderef1/containers/podgrab.nix
@@ -0,0 +1,28 @@
+{ pkgs, opts, secrets, ... }:
+{
+  virtualisation.oci-containers.containers = {
+    podgrab = {
+      autoStart = true;
+      image = "akhilrex/podgrab";
+      extraOptions = [
+        "--add-host=nullptrderef1:${opts.lanAddress}"
+        "--privileged"
+        "--no-healthcheck"
+      ];
+      ports = [
+        "${opts.ports.podgrab}:8080"
+      ];
+      volumes = [
+        "${opts.paths.application_data}/podgrab/config:/config"
+        "${opts.paths.application_data}/podgrab/assets:/assets"
+      ];
+      environment = {
+        CHECK_FREQUENCY = "240";
+        PASSWORD = "${secrets.podgrab-password}"; # username podgrab
+        TZ = opts.timeZone;
+        PUID = opts.adminUID;
+        PGID = opts.adminGID;
+      };
+    };
+  };
+}
