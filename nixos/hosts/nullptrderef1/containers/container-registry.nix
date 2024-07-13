bafaae37d47e4bd20a110489229ab41d3ace3900
sreedev <sreedevpadmakumar@gmail.com>
2024-07-13 14:16:59 +0000

    registry image update

--- nixos/hosts/nullptrderef1/containers/container-registry.nix
+++ nixos/hosts/nullptrderef1/containers/container-registry.nix
@@ -3,10 +3,9 @@
   virtualisation.oci-containers.containers = {
     "container-registry" = {
       autoStart = true;
-      image = "registry:2";
+      image = "registry:2.7";
       extraOptions = [ "--network=host" "--no-healthcheck" ];
       ports = [ "${opts.ports.container-registry}:5000" ];
-
       volumes = [ "${opts.paths.application_data}/container-registry:/data" ];
       environment = {
         TZ = opts.timeZone;
