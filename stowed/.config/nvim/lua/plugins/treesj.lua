7fd70efe26e1d08331bb368e6ddcb03035204a12
sreedev <sreedevpadmakumar@gmail.com>
2024-07-13 16:36:00 +0000

    neovim: treesj fixes

--- stowed/.config/nvim/lua/plugins/treesj.lua
+++ stowed/.config/nvim/lua/plugins/treesj.lua
@@ -1,6 +1,5 @@
 return {
   'Wansmer/treesj',
-  keys = { '<leader>m', '<leader>j', '<leader>s' },
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   config = function()
     require('treesj').setup({
