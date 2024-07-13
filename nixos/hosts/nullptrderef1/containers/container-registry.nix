b87ee867a7f7518eab17b39a337dc41a73b695aa
sreedev <sreedevpadmakumar@gmail.com>
2024-07-13 14:49:59 +0000

    container registry adjustments

--- nixos/hosts/nullptrderef1/containers/container-registry.nix
+++ nixos/hosts/nullptrderef1/containers/container-registry.nix
@@ -1,18 +1,35 @@
 { pkgs, config, opts, secrets, ... }:
 {
+  # container-registry-web
   virtualisation.oci-containers.containers = {
-    "container-registry" = {
+    "container-registry-server" = {
       autoStart = true;
       image = "registry:2.7";
       extraOptions = [ "--add-host=nullptrderef1:${opts.lanAddress}" "--no-healthcheck" ];
-      ports = [ "${opts.ports.container-registry}:5000" ];
-      volumes = [ "${opts.paths.application_data}/container-registry:/data" ];
+      ports = [ "${opts.ports.container-registry-server}:5000" ];
+      volumes = [ "${opts.paths.application_data}/containers/server:/var/lib/registry" ];
+      environment = {
+        REGISTRY_HTTP_SECRET = secrets.registry_http_secret;
+        TZ = opts.timeZone;
+        USER_GID = opts.adminGID;
+        USER_UID = opts.adminUID;
+      };
+    };
+
+    "container-registry-web" = {
+      autoStart = true;
+      dependsOn = [ "container-registry-server" ];
+      image = "quiq/registry-ui";
+      extraOptions = [ "--add-host=nullptrderef1:${opts.lanAddress}" "--no-healthcheck" "--privileged" ];
+      ports = [ "${opts.ports.container-registry-web}:8000" ];
+      volumes = [
+        "${opts.paths.application_data}/containers/web/data:/opt/data"
+        "${opts.paths.application_data}/containers/web/config.yml:/opt/config.yml:ro"
+      ];
       environment = {
         TZ = opts.timeZone;
         USER_UID = opts.adminUID;
         USER_GID = opts.adminGID;
-        REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY = "/data";
-        REGISTRY_HTTP_SECRET = secrets.registry_http_secret;
       };
     };
   };
