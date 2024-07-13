9f0440168bfcc3acdc536e59da5875df1202c9a5
sreedev <sreedevpadmakumar@gmail.com>
2024-07-13 14:14:50 +0000

    nix flakes updated

--- nixos/hosts/nullptrderef1/containers/container-registry.nix
+++ nixos/hosts/nullptrderef1/containers/container-registry.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, opts, ... }:
+{ pkgs, config, opts, secrets, ... }:
 {
   virtualisation.oci-containers.containers = {
     "container-registry" = {
@@ -6,12 +6,14 @@
       image = "registry:2";
       extraOptions = [ "--network=host" "--no-healthcheck" ];
       ports = [ "${opts.ports.container-registry}:5000" ];
+
       volumes = [ "${opts.paths.application_data}/container-registry:/data" ];
       environment = {
         TZ = opts.timeZone;
         USER_UID = opts.adminUID;
         USER_GID = opts.adminGID;
         REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY = "/data";
+        REGISTRY_HTTP_SECRET = secrets.registry_http_secret;
       };
     };
   };
