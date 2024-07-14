5abcb13e94b6253350ff2d52f0343f3d33d19531
sreedev <sreedevpadmakumar@gmail.com>
2024-07-14 16:49:45 +0000

    packages re-org

--- nixos/users/common/neovim.nix
+++ nixos/users/common/neovim.nix
@@ -1,7 +1,20 @@
-{ pkgs, username, ... }: {
-  home.packages = with pkgs; [
-    neovim
-    nodejs-slim
-    tree-sitter
+{ pkgs, username, ... }:
+let
+  language_servers =
+    with pkgs; [
+      glas # gleam
+    ];
+
+  neovim_pkgs =
+    with pkgs; [
+      neovim
+      nodejs-slim
+      tree-sitter
+    ];
+in
+{
+  home.packages = builtins.concatLists [
+    language_servers
+    neovim_pkgs
   ];
 }
