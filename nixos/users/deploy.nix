73511c49bd26bd3e79640c86e2da6f106616e5a3
sreedev <sreedev@icloud.com>
2024-07-15 13:10:52 +0000

    nixified git config

--- nixos/users/deploy.nix
+++ nixos/users/deploy.nix
@@ -1,11 +1,12 @@
 { pkgs, secrets, lib, inputs, opts, system, username, ... }: {
 
   imports = [
+    ./common/base.nix
     ./common/core-packages.nix
     ./common/fastfetch.nix
+    ./common/git.nix
     ./common/htop.nix
     ./common/keybase.nix
-    ./common/misc.nix
     ./common/neovim.nix
     ./common/tmux.nix
     ./common/zsh.nix
