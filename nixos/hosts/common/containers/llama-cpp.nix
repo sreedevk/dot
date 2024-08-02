c8de400f04393ace35786489dca8e32fbbf60e47
sreedev <sreedevpadmakumar@gmail.com>
2024-08-02 13:52:13 +0000

    llama-cpp resource limits

--- nixos/hosts/common/containers/llama-cpp.nix
+++ nixos/hosts/common/containers/llama-cpp.nix
@@ -1,28 +1,20 @@
 { opts, secrets, ... }:
-let
-  models = {
-    meta-llama-3 = "meta-llama-3-8b-instruct.Q5_K.gguf";
-    gemma-2 = "gemma-2-27b-it-Q4_K_L.gguf";
-    dolphin-mistral-2-7b = "dolphin-2.2.1-mistral-7b.Q6_K.gguf";
-    dolphin-mistral-nemo = "dolphin-2.9.3-mistral-nemo-Q5_K_S.gguf";
-    codellama-7b = "codellama-7b.Q6_K.gguf";
-  };
-
-  llama-cpp-opts = {
-    model = models.meta-llama-3;
-    n = "512";
-  };
-in
 {
   virtualisation.oci-containers.containers = {
     "llama-cpp-server" = {
-      autoStart = false; # memory consumption is high, start only on use.
+      autoStart = true;
       image = "ghcr.io/ggerganov/llama.cpp:server";
-      extraOptions = [ "--add-host=nullptrderef1:${opts.lanAddress}" "--no-healthcheck" ];
+      extraOptions = [
+        "--add-host=nullptrderef1:${opts.lanAddress}"
+        "--no-healthcheck"
+        "--memory=16g"
+        "--memory-swap=32g"
+        "--cpus=4"
+      ];
       ports = [ "${opts.ports.llama-cpp-server}:8000" ];
       volumes = [ "${opts.paths.llama-cpp-models}:/models:ro" ];
       entrypoint = "/llama-server";
-      cmd = [ "-m" "/models/${llama-cpp-opts.model}" "--port" "8000" "--host" "0.0.0.0" "-n" "${llama-cpp-opts.n}" ];
+      cmd = [ "-m" "/models/dolphin-2.2.1-mistral-7b.Q6_K.gguf" "--port" "8000" "--host" "0.0.0.0" "-n" "512" ];
       environment = {
         TZ = opts.timeZone;
         PUID = opts.adminUID;
