bba355681c3e3734bc9d9da067164a9fd4969155
sreedev <sreedev@icloud.com>
2024-08-03 13:27:05 +0000

    parameterize git signing enable

--- nixos/opts.nix
+++ nixos/opts.nix
@@ -10,4 +10,7 @@
     rpi4b = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDekQbvhkhlu7kM2smT8RNwi5Uyv90vPLPsSsyForqHjLAxhxz5dpTcW8q/ocye6eOkpAoFWELawL/Umszo34XglQM8gh6gnQceadzxJe7hm+adAgPKrvY95uGYLRfupSpteZIPpByDaLdtGPWjt7gSHnjcN+5DFmkmIzpccf1Gq3SKtfKbkXBoF6sHt0VGpUL7BtjcHZmk7ZoaHQGBYMjcEdHwMQ+Cwa3pBfUhjhCZu7wSQ6MKRBmtN7Z0QUNWt4zvaClgJFSy0gK9b5fIDLFsKVQ6MlEIR0PFhDl2E4kdMjIuFKc1QYFRif8F4TumAHIFED5oYj6iIS9bYP8HXK7w3MEHsTgyVPXN9wW9riwPFKYUMcC8JoZHa+Zfjlz+shcG8yVT3r4nUbWuP0RRNQAuwb9LwFhLthvvBhDatutBhCpZUvdzC4t+exfaDh3PqjJT5yxiFEugr7Gev04WywU75CCrhp+36aKJgJxQXxsWgf0LjrWuu0a4ouiMtFCzvm0=";
   };
 
+  git = {
+    enable-signing = false;
+  };
 }
--- nixos/users/admin/opts.nix
+++ nixos/users/admin/opts.nix
@@ -1,1 +1,5 @@
-{}
+{
+  git = {
+    enable-signing = false;
+  };
+}
