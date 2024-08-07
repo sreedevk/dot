657dd2164041c1174d832a3ec1280c3f88b2610f
sreedev <sreedev@icloud.com>
2024-08-07 00:39:07 +0000

    nixGLNvidia enabled for firefox

--- nixos/users/common/firefox.nix
+++ nixos/users/common/firefox.nix
@@ -1,8 +1,55 @@
-{ pkgs, lib, firefox-addons, system, ... }:
+{ pkgs, lib, config, firefox-addons, system, ... }:
+let
+  nixGLSource = builtins.fetchTarball {
+    url = "https://github.com/guibou/nixGL/archive/310f8e49a149e4c9ea52f1adf70cdc768ec53f8a.tar.gz";
+    sha256 = "1crnbv3mdx83xjwl2j63rwwl9qfgi2f1lr53zzjlby5lh50xjz4n";
+  };
+
+  nixGLNvidia = (pkgs.callPackage "${nixGLSource}/nixGL.nix" { }).auto.nixGLNvidia;
+
+  # SRC: https://git.jakstys.lt/motiejus/config/commit/9f61b9a378f28eb72da051fdaa55ec972cab1057
+  mkWrapped = wrap: orig-pkg: execName:
+    pkgs.makeOverridable
+      (
+        attrs:
+        let
+          pkg = orig-pkg.override attrs;
+          outs = pkg.meta.outputsToInstall;
+          paths = pkgs.lib.attrsets.attrVals outs pkg;
+          nonTrivialOuts = pkgs.lib.lists.remove "out" outs;
+          metaAttributes =
+            pkgs.lib.attrsets.getAttrs
+              (
+                [
+                  "name"
+                  "pname"
+                  "version"
+                  "meta"
+                ]
+                ++ nonTrivialOuts
+              )
+              pkg;
+        in
+        pkgs.symlinkJoin (
+          {
+            inherit paths;
+            nativeBuildInputs = [ pkgs.makeBinaryWrapper ];
+            postBuild = ''
+              mv $out/bin/${execName} $out/bin/.${execName}-mkWrapped-original
+              makeWrapper ${wrap}/bin/${wrap.name} $out/bin/${execName} --add-flags $out/bin/.${execName}-mkWrapped-original
+            '';
+          }
+          // metaAttributes
+        )
+      )
+      { };
+
+  glNvidia = mkWrapped nixGLNvidia;
+in
 {
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox;
+    package = glNvidia pkgs.firefox-bin "firefox";
     profiles.main = {
       isDefault = true;
       containersForce = true;
