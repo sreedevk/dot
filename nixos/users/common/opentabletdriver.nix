9a88e930b610f882e6d6aff037cf16442a5a05e4
sreedev <sreedev@icloud.com>
2024-08-07 01:12:59 +0000

    opentabletdriver service moved to nix

---
+++ nixos/users/common/opentabletdriver.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    opentabletdriver
+  ];
+
+  systemd.user =
+    {
+      services = {
+        opentabletdriver = {
+          Unit = {
+            Description = "OpenTabletDriver Daemon";
+            PartOf = "graphical-session.target";
+            After = "graphical-session.target";
+          };
+          Service = {
+            ExecStart = "${pkgs.opentabletdriver}/bin/otd-daemon";
+            Restart = "always";
+            RestartSec = 3;
+          };
+          Install = {
+            WantedBy = ["graphical-session.target"];
+          };
+        };
+      };
+    };
+}
