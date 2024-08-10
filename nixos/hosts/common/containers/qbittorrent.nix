30a179f622affa894e6e36b1aabae4322c07d971
sreedev <sreedev@icloud.com>
2024-08-10 20:50:01 +0000

    qbt installation more deterministic

--- nixos/hosts/common/containers/qbittorrent.nix
+++ nixos/hosts/common/containers/qbittorrent.nix
@@ -1,4 +1,22 @@
-{ config, lib, pkgs, secrets, opts, ... }: {
+{ config, lib, pkgs, secrets, opts, ... }:
+let
+  vuetorrentSrc = builtins.fetchGit {
+    name = "vuetorrent";
+    url = "https://github.com/VueTorrent/VueTorrent";
+    rev = "7aae128a7a439723c2e728cc5005510f1f5c72ce";
+    ref = "latest-release";
+    shallow = true;
+  };
+in
+{
+
+  environment.etc = {
+    "vuetorrent" = {
+      enable = true;
+      source = vuetorrentSrc;
+    };
+  };
+
   networking.firewall.allowedTCPPorts =
     builtins.map pkgs.lib.strings.toInt (with opts.ports; [
       qbittorrent-p2p
@@ -22,7 +40,7 @@
       ];
       environment = {
         QBT_EULA = "accept";
-        QBT_VERSION = "4.6.4-1";
+        QBT_VERSION = "latest";
         QBT_WEBUI_PORT = "${opts.ports.qbittorrent-web}";
         TZ = opts.timeZone;
         USER_UID = opts.adminUID;
@@ -30,13 +48,13 @@
       };
       volumes = [
         "${opts.paths.application_data}/qbittorrent/config/:/config"
-        "${opts.paths.application_data}/vuetorrent:/vuetorrent"
         "${opts.paths.downloads}:/downloads"
         "${opts.paths.torrent_watch}:/torrents"
         "${opts.paths.qbt_images}:/images"
         "${opts.paths.videos}:/videos"
         "${opts.paths.books}:/books"
         "${opts.paths.magazines}:/magazines"
+        "/etc/vuetorrent:/vuetorrent:ro"
       ];
       extraOptions = [
         "--network=host"
@@ -44,6 +62,9 @@
         "--memory=8g"
         "--memory-swap=16g"
         "--cpus=4"
+        "--read-only"
+        "--stop-timeout=1800"
+        "--tmpfs=/tmp"
       ];
     };
   };
