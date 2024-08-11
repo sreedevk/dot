8eeead58264ca57493cfed4d2e2c697b3cc8be51
sreedev <sreedev@icloud.com>
2024-08-11 17:57:46 +0000

    appdata-sync script updated

--- nixos/hosts/common/scripts/appdata-sync.nix
+++ nixos/hosts/common/scripts/appdata-sync.nix
@@ -1,11 +1,25 @@
 { pkgs, opts, ... }:
 let
   appdata-sync =
-    pkgs.writeShellScriptBin "backup-appdata" ''
+    pkgs.writeShellScriptBin "appdata-sync" ''
       rsync -aAXv --progress --delete --size-only "${opts.paths.application_data}/" "${opts.paths.application_data_archive}/"
       rsync -aAXv --progress --delete --size-only "${opts.paths.application_databases}/" "${opts.paths.application_databases_archive}/"
     '';
 in
 {
   environment.systemPackages = [ appdata-sync ];
+
+  # systemd.services.appdata-sync = {
+  #   description = "appdata sync";
+  #   enable = true;
+  #   after = [ "network.target" ];
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig = {
+  #     ExecStart = ''
+  #       ${pkgs.taskchampion-sync-server}/bin/taskchampion-sync-server --port ${opts.ports.taskchampion} --data-dir ${opts.paths.application_data}/TaskChampion
+  #     '';
+  #     Restart = "always";
+  #   };
+  # };
+
 }
