2b6956badac132871e8221c550ab6a28321740a1
sreedev <sreedev@icloud.com>
2024-08-11 16:15:24 +0000

    rsync file structure bug issue fixed in appdata-sync

--- nixos/hosts/common/scripts/appdata-sync.nix
+++ nixos/hosts/common/scripts/appdata-sync.nix
@@ -2,8 +2,8 @@
 let
   appdata-sync =
     pkgs.writeShellScriptBin "backup-appdata" ''
-      rsync -aAXv --progress --delete --size-only "${opts.paths.application_data}" "${opts.paths.application_data_archive}"
-      rsync -aAXv --progress --delete --size-only "${opts.paths.application_databases}" "${opts.paths.application_databases_archive}"
+      rsync -aAXv --progress --delete --size-only "${opts.paths.application_data}/" "${opts.paths.application_data_archive}/"
+      rsync -aAXv --progress --delete --size-only "${opts.paths.application_databases}/" "${opts.paths.application_databases_archive}/"
     '';
 in
 {
