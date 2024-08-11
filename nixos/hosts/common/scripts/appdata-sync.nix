c2706a3504fca29c6102a03f375cdf5e7ecafd1f
sreedev <sreedev@icloud.com>
2024-08-11 18:09:27 +0000

    appdata-sync service+timer added

--- nixos/hosts/common/scripts/appdata-sync.nix
+++ nixos/hosts/common/scripts/appdata-sync.nix
@@ -2,24 +2,33 @@
 let
   appdata-sync =
     pkgs.writeShellScriptBin "appdata-sync" ''
-      rsync -aAXv --progress --delete --size-only "${opts.paths.application_data}/" "${opts.paths.application_data_archive}/"
-      rsync -aAXv --progress --delete --size-only "${opts.paths.application_databases}/" "${opts.paths.application_databases_archive}/"
+      ${pkgs.rsync}/bin/rsync -aAXv --progress --delete --size-only "${opts.paths.application_data}/" "${opts.paths.application_data_archive}/"
+      ${pkgs.rsync}/bin/rsync -aAXv --progress --delete --size-only "${opts.paths.application_databases}/" "${opts.paths.application_databases_archive}/"
     '';
 in
 {
   environment.systemPackages = [ appdata-sync ];
 
-  # systemd.services.appdata-sync = {
-  #   description = "appdata sync";
-  #   enable = true;
-  #   after = [ "network.target" ];
-  #   wantedBy = [ "multi-user.target" ];
-  #   serviceConfig = {
-  #     ExecStart = ''
-  #       ${pkgs.taskchampion-sync-server}/bin/taskchampion-sync-server --port ${opts.ports.taskchampion} --data-dir ${opts.paths.application_data}/TaskChampion
-  #     '';
-  #     Restart = "always";
-  #   };
-  # };
+  systemd.services.appdata-sync = {
+    description = "appdata sync";
+    enable = true;
+    script = ''
+      set -eu
+      ${appdata-sync}/bin/appdata-sync
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
 
+  };
+
+  systemd.timers.appdata-sync-trigger = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnBootSec = "5m";
+      OnUnitActiveSec = "10m";
+      Unit = "appdata-sync.service";
+    };
+  };
 }
