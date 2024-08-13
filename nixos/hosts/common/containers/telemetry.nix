888ed644bc131b39f376e833487e9b54aa804b0e
sreedev <sreedev@icloud.com>
2024-08-13 01:29:41 +0000

    prometheus zfs collector

--- nixos/hosts/common/containers/telemetry.nix
+++ nixos/hosts/common/containers/telemetry.nix
@@ -13,9 +13,17 @@
     enable = true;
     port = pkgs.lib.strings.toInt opts.ports.prometheus_node;
     # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/nixos/modules/services/monitoring/prometheus/exporters.nix
-    enabledCollectors = [ "systemd" ];
+    enabledCollectors = [ "systemd" "influxdb" ];
     # /nix/store/zgsw0yx18v10xa58psanfabmg95nl2bb-node_exporter-1.8.1/bin/node_exporter  --help
-    extraFlags = [ "--collector.ethtool" "--collector.softirqs" "--collector.tcpstat" "--collector.wifi" ];
+    extraFlags = [
+      "--collector.ethtool"
+      "--collector.softirqs"
+      "--collector.tcpstat"
+      "--collector.wifi"
+      "--collector.zfs"
+      "--collector.processes"
+      "--collector.filesystem"
+    ];
   };
 
   services.netdata = {
