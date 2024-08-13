c1989058dd4eaef5e630f12d8d74f481f37a6f4a
sreedev <sreedev@icloud.com>
2024-08-13 01:41:23 +0000

    node exporter unknown flag issue fixed

--- nixos/hosts/common/containers/telemetry.nix
+++ nixos/hosts/common/containers/telemetry.nix
@@ -13,7 +13,7 @@
     enable = true;
     port = pkgs.lib.strings.toInt opts.ports.prometheus_node;
     # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/nixos/modules/services/monitoring/prometheus/exporters.nix
-    enabledCollectors = [ "systemd" "influxdb" ];
+    enabledCollectors = [ "systemd" ];
     # /nix/store/zgsw0yx18v10xa58psanfabmg95nl2bb-node_exporter-1.8.1/bin/node_exporter  --help
     extraFlags = [
       "--collector.ethtool"
