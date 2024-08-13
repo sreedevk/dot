902e4a9d258a512e60fd809d7ffc4d933cf6cf63
sreedev <sreedev@icloud.com>
2024-08-13 17:13:15 +0000

    backup sync service enabled

--- nixos/users/sreedev/backup.nix
+++ nixos/users/sreedev/backup.nix
@@ -7,7 +7,8 @@ let
 
   create-backup-archives =
     pkgs.writeShellScriptBin "create-backup-archives" ''
-      ${pkgs.p7zip}/bin/7z a -t7z -mx=9 /opt/backups/$(hostname)-$(date -I)-tunecore-archive.7z /home/${username}/Data/tunecore
+      ${pkgs.p7zip}/bin/7z a -t7z -mx=9 /opt/backups/data-tunecore-archive-$(date -I).7z /home/${username}/Data/tunecore
+      ${pkgs.p7zip}/bin/7z a -t7z -mx=9 /opt/backups/data-notebook-archive-$(date -I).7z /home/${username}/Data/notebook
     '';
 in
 {
@@ -22,16 +23,39 @@ in
           ExecStart = "${create-backup-archives}/bin/create-backup-archives";
         };
       };
+
+      sync-backups-archives = {
+        Unit = { Description = "sync backup archives"; };
+        Service = {
+          Type = "simple";
+          ExecStart = "${sync-backups-archives}/bin/sync-backups-archives";
+        };
+      };
+
     };
 
     timers = {
+      sync-backups-archives-timers = {
+        Unit = {
+          Description = "Timer for syncing backup archives";
+        };
+        Timer = {
+          OnBootSec = "4h";
+          OnUnitActiveSec = "1w";
+          Unit = "sync-backups-archives.service";
+        };
+        Install = {
+          WantedBy = [ "timers.target" ];
+        };
+      };
+
       create-backup-archives-timers = {
         Unit = {
           Description = "Timer for creating backup archives";
         };
         Timer = {
-          OnBootSec = "60min";
-          OnUnitActiveSec = "daily";
+          OnBootSec = "4h";
+          OnUnitActiveSec = "1d";
           Unit = "create-backup-archives.service";
         };
         Install = {
