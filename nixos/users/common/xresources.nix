b1326503e4b07cbc0021f863b4f618e3d5b891bb
sreedev <sreedev@icloud.com>
2024-08-15 14:46:00 +0000

    nixified xresources props

--- nixos/users/common/xresources.nix
+++ nixos/users/common/xresources.nix
@@ -1,53 +1,36 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   xresources = {
-    extraConfig = ''
-      Xft.dpi: 128
-      Xcursor.size: 18
-      Xft.autohint: 0
-      Xft.lcdfilter:  lcddefault
-      Xft.hintstyle:  hintfull
-      Xft.hinting: 1
-      Xft.antialias: 1
-      Xft.rgba: rgb
-
-      !{{{ Colors
-      ! special
-      *foreground: #bdcfe3
-      *background: #141c21
-
-      ! black
-      *color0: #263640
-      *color8: #4a697d
-
-      ! red
-      *color1: #d12f2c
-      *color9: #fa3935
-
-      ! green
-      *color2:  #819400
-      *color10: #a4bd00
-
-      ! yellow
-      *color3:  #b08500
-      *color11: #d9a400
-
-      ! blue
-      *color4:  #2587cc
-      *color12: #2ca2f5
-
-      ! magenta
-      *color5:  #696ebf
-      *color13: #8086e8
-
-      ! cyan
-      *color6:  #289c93
-      *color14: #33c5ba
-
-      ! white
-      *color7:  #bfbaac
-      *color15: #fdf6e3
-      !}}}
-    '';
+    properties = {
+      "Xft.dpi" = lib.mkForce 128;
+      "Xcursor.size" = lib.mkForce 18;
+      "Xft.autohint" = 0;
+      "Xft.lcdfilter" = lib.mkForce "lcddefault";
+      "Xft.hintstyle" = lib.mkForce "hintfull";
+      "Xft.hinting" = 1;
+      "Xft.antialias" = 1;
+      "Xft.rgba" = "rgb";
+      "*.faceName" = "Iosevka NF";
+      "*.faceSize" = 28;
+      "*.renderFont" = true;
+      "*foreground" = lib.mkForce "#bdcfe3";
+      "*background" = lib.mkForce "#141c21L";
+      "*color0" = lib.mkForce "#263640";
+      "*color8" = lib.mkForce "#4a697d";
+      "*color1" = lib.mkForce "#d12f2c";
+      "*color9" = lib.mkForce "#fa3935";
+      "*color2" = lib.mkForce "#819400";
+      "*color10" = lib.mkForce "#a4bd00";
+      "*color3" = lib.mkForce "#b08500";
+      "*color11" = lib.mkForce "#d9a400";
+      "*color4" = lib.mkForce "#2587cc";
+      "*color12" = lib.mkForce "#2ca2f5";
+      "*color5" = lib.mkForce "#696ebf";
+      "*color6" = lib.mkForce "#289c93";
+      "*color14" = lib.mkForce "#33c5ba";
+      "*color13" = lib.mkForce "#8086e8";
+      "*color7" = lib.mkForce "#bfbaac";
+      "*color15" = lib.mkForce "#fdf6e3";
+    };
   };
 }
