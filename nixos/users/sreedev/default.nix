eb113d1ee9271af3b94d131d0faddc6a417d6cde
sreedev <sreedev@icloud.com>
2024-08-17 02:01:41 +0000

    cleanup

--- nixos/users/sreedev/default.nix
+++ nixos/users/sreedev/default.nix
@@ -37,60 +37,49 @@
   ];
 
   home.packages =
-    let
-      cli-tools =
-        with pkgs; [
-          doctl
-          duckdb
-          glab
-          glow
-          hledger
-          instaloader
-          irssi
-          jira-cli-go
-          just
-          k9s
-          mdbook
-          nixpkgs-fmt
-          nmap
-          nushell
-          pandoc
-          tea
-          texliveFull
-          tmuxinator
-          yt-dlp
-          zola
-        ];
-
-      gui-tools =
-        with pkgs; [
-          (nerdfonts.override { fonts = [ "Iosevka" ]; })
-          arandr
-          autorandr
-          brightnessctl
-          dbeaver-bin
-          droidcam
-          emacs
-          feh
-          gimp-with-plugins
-          libreoffice-fresh
-          lmms
-          maim
-          nemo-with-extensions
-          nicotine-plus
-          nsxiv
-          openttd
-          playerctl
-          python311Packages.i3ipc
-          python312Packages.supervisor
-          qflipper
-          rofi
-          slack
-          spotify
-        ];
-    in
-    builtins.concatLists [
-      cli-tools
-      gui-tools
+    with pkgs; [
+      (nerdfonts.override { fonts = [ "Iosevka" ]; })
+      arandr
+      autorandr
+      brightnessctl
+      dbeaver-bin
+      doctl
+      droidcam
+      duckdb
+      emacs
+      feh
+      gimp-with-plugins
+      glab
+      glow
+      hledger
+      instaloader
+      irssi
+      jira-cli-go
+      just
+      k9s
+      libreoffice-fresh
+      lmms
+      maim
+      mdbook
+      nemo-with-extensions
+      nicotine-plus
+      nixpkgs-fmt
+      nmap
+      nsxiv
+      nushell
+      openttd
+      pandoc
+      playerctl
+      python311Packages.i3ipc
+      python312Packages.supervisor
+      qflipper
+      rofi
+      slack
+      spotify
+      tea
+      texliveFull
+      tmuxinator
+      yt-dlp
+      zola
     ];
 }
