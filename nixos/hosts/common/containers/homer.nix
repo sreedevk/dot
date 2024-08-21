90d841bfe188e068fcdf9cc1a7dbdad915d99907
sreedevk <sreedev@icloud.com>
2024-08-21 00:36:55 +0000

    Revert "homer config moved to etc"
    This reverts commit c8cd0dacef5e60d09732fcf5a1b04dbf0d9f08c5.
    
    (cherry picked from commit ddf75edc252ded065528c255944dc88f7293b996)

--- nixos/hosts/common/containers/homer.nix
+++ nixos/hosts/common/containers/homer.nix
@@ -1,22 +1,4 @@
-{ config, lib, pkgs, secrets, opts, ... }:
-let
-  homerConfigSrc = builtins.fetchGit {
-    name = "homer";
-    url = "https://gitea.nullptr.sh/nullptrderef1/homer";
-    rev = "6486e7540293116459b07997d755ff35ac96368d";
-    ref = "main";
-    shallow = true;
-  };
-in
-{
-
-  environment.etc = {
-    "homer" = {
-      enable = true;
-      source = homerConfigSrc;
-    };
-  };
-
+{ config, lib, pkgs, secrets, opts, ... }: {
   networking.firewall.allowedTCPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ homer ]);
   virtualisation.oci-containers.containers = {
     "homer" = {
@@ -24,9 +6,7 @@ in
       image = "b4bz/homer:latest";
       extraOptions =
         [ "--add-host=${opts.hostname}:${opts.lanAddress}" "--no-healthcheck" ];
-      volumes = [
-        "/etc/homer:/www/assets:ro"
-      ];
+      volumes = [ "${opts.paths.application_data}/homer/:/www/assets" ];
       ports = [ "${opts.ports.homer}:8080" ];
       environment = {
         TZ = opts.timeZone;
