051d8737ebb6371fdc10db82a085b698d0a0f1ac
sreedevk <sreedev@icloud.com>
2024-08-27 23:59:26 +0000

    secrets migration to agenix

--- nixos/hosts/common/services/mullvad.nix
+++ nixos/hosts/common/services/mullvad.nix
@@ -1,12 +1,13 @@
-{ config, secrets, ... }: {
+{ config, ... }: {
   services.mullvad-vpn = { enable = true; };
 
   systemd.services."mullvad-daemon".postStart =
     let mullvad = config.services.mullvad-vpn.package;
-    in ''
+    in
+    ''
       while ! ${mullvad}/bin/mullvad status > /dev/null; do sleep 1; done
-      ${mullvad}/bin/mullvad account get | grep "Not logged in" && ${mullvad}/bin/mullvad account login ${secrets.mullvad_account or ""}
-      ${mullvad}/bin/mullvad relay set location ${secrets.mullvad_location or "any"}
+      ${mullvad}/bin/mullvad account get | grep "Not logged in" && ${mullvad}/bin/mullvad account login $(cat ${config.age.secrets.vpn-acc.path})
+      ${mullvad}/bin/mullvad relay set location $(cat ${config.age.secrets.vpn-loc.path})
       ${mullvad}/bin/mullvad lockdown-mode set on
       ${mullvad}/bin/mullvad lan set allow
       ${mullvad}/bin/mullvad tunnel set ipv6 off
