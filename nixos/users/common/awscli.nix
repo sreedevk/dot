f36ee552fd731681f18971b2e12416b6555c0f4d
sreedevk <sreedev@icloud.com>
2024-08-28 19:44:30 +0000

    aws migration complete

--- nixos/users/common/awscli.nix
+++ nixos/users/common/awscli.nix
@@ -1,19 +1,17 @@
-{ pkgs, nixpkgs-stable, config, secrets, ... }: {
+{ pkgs, nixpkgs-stable, config, ... }: {
 
   programs.awscli = {
     enable = true;
-    package = nixpkgs-stable.awscli2;
+    package = pkgs.awscli2;
     settings = {
       "default" = {
-        region = secrets.aws_region or "us-east-1";
+        region = "us-east-1";
         output = "json";
       };
     };
     credentials = {
       "default" = {
         "credential_process" = "${pkgs.coreutils}/bin/cat ${config.age.secrets.aws-secrets.path}";
-        # "aws_access_key_id" = secrets.aws_access_key_id or "";
-        # "aws_secret_access_key" = secrets.aws_secret_access_key or "";
       };
     };
   };
