8f9d5098afe9a3946aba237f99439894a19e0117
sreedevk <sreedev@icloud.com>
2024-08-28 22:52:38 +0000

    linkding migrated

--- nixos/hosts/common/containers/linkding.nix
+++ nixos/hosts/common/containers/linkding.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, secrets, opts, ... }: {
+{ config, lib, pkgs, opts, ... }: {
   networking.firewall.allowedTCPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ linkding-app linkding-db ]);
 
   virtualisation.oci-containers.containers = {
@@ -12,18 +12,12 @@
       volumes =
         [ "${opts.paths.application_data}/linkding:/etc/linkding/data" ];
       ports = [ "${opts.ports.linkding-app}:9090" ];
+      environmentFiles = [ config.age.secrets.linkding_env.path ];
       environment = {
+        LD_DB_PORT = opts.ports.linkding-db;
         TZ = opts.timeZone;
         PUID = opts.adminUID;
         PGID = opts.adminGID;
-        LD_DB_ENGINE = "postgres";
-        LD_DB_DATABASE = secrets.linkding_db_name;
-        LD_DB_USER = secrets.linkding_db_username;
-        LD_DB_PASSWORD = secrets.linkding_db_password;
-        LD_DB_HOST = secrets.linkding_db_host;
-        LD_DB_PORT = opts.ports.linkding-db;
-        LD_SUPERUSER_NAME = secrets.linkding_superuser;
-        LD_SUPERUSER_PASSWORD = secrets.linkding_password;
       };
     };
 
@@ -34,13 +28,11 @@
       ports = [ "${opts.ports.linkding-db}:5432" ];
       extraOptions =
         [ "--add-host=${opts.hostname}:${opts.lanAddress}" "--no-healthcheck" ];
+      environmentFiles = [ config.age.secrets.linkding_env.path ];
       environment = {
         TZ = opts.timeZone;
         PUID = opts.adminUID;
         PGID = opts.adminGID;
-        POSTGRES_USER = secrets.linkding_db_username;
-        POSTGRES_PASSWORD = secrets.linkding_db_password;
-        POSTGRES_DB = secrets.linkding_db_name;
       };
     };
 
