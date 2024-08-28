8ee99fbe5711233707c510e703108c075708c0b1
sreedevk <sreedev@icloud.com>
2024-08-28 22:18:12 +0000

    gotify_env migrated

--- nixos/hosts/common/containers/gotify.nix
+++ nixos/hosts/common/containers/gotify.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, secrets, opts, ... }: {
+{ config, lib, pkgs, opts, ... }: {
   networking.firewall.allowedTCPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ gotify ]);
   networking.firewall.allowedUDPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ gotify ]);
   virtualisation.oci-containers.containers = {
@@ -8,8 +8,8 @@
       extraOptions = [ "--add-host=${opts.hostname}:${opts.lanAddress}" "--no-healthcheck" ];
       ports = [ "${opts.ports.gotify}:80" ];
       volumes = [ "${opts.paths.application_data}/gotify:/app/data" ];
+      environmentFiles = [ config.age.secrets.gotify_env.path ];
       environment = {
-        GOTIFY_DEFAULTUSER_PASS = "${secrets.gotify-password}";
         TZ = opts.timeZone;
         PUID = opts.adminUID;
         PGID = opts.adminGID;
