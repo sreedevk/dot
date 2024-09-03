ae94645db5cd79d4ef0db13d727522e5f964f03b
sreedevk <sreedev@icloud.com>
2024-09-03 21:42:22 +0000

    moved directory management to actual

--- nixos/hosts/common/containers/actual.nix
+++ nixos/hosts/common/containers/actual.nix
@@ -1,13 +1,17 @@
 { config, lib, pkgs, opts, ... }: {
   networking.firewall.allowedTCPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ actual-app ]);
 
+  systemd.tmpfiles.rules = [
+    "d ${opts.paths.application_data}/Actual 0755 ${opts.adminUID} ${opts.adminGID} -"
+  ];
+
   virtualisation.oci-containers.containers = {
     actual-app = {
       autoStart = true;
       extraOptions = [ "--add-host=${opts.hostname}:${opts.lanAddress}" "--no-healthcheck" ];
       image = "docker.io/actualbudget/actual-server:latest";
       ports = [ "${opts.ports.actual-app}:5006" ];
-      volumes = [ "${opts.paths.application_data}/actual:/data" ];
+      volumes = [ "${opts.paths.application_data}/Actual:/data" ];
       environment = {
         PGID = opts.adminGID;
         PUID = opts.adminUID;
