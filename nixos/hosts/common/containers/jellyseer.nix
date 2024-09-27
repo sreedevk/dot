bbdb9a99fa38e1da82facbb850ace7304fcfa85f
sreedevk <sreedev@icloud.com>
2024-09-27 21:17:53 +0000

    migrate: jellyseer

--- nixos/hosts/common/containers/jellyseer.nix
+++ nixos/hosts/common/containers/jellyseer.nix
@@ -1,12 +1,17 @@
 { config, lib, pkgs, opts, ... }: {
   networking.firewall.allowedTCPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ jellyseer ]);
+
+  systemd.tmpfiles.rules = [
+    "d ${opts.paths.app_datafiles}/jellyseer 0755 ${opts.adminUID} ${opts.adminGID} -"
+  ];
+
   virtualisation.oci-containers.containers = {
     "jellyseer" = {
       autoStart = true;
       image = "fallenbagel/jellyseerr:latest";
       extraOptions =
         [ "--add-host=${opts.hostname}:${opts.lanAddress}" "--no-healthcheck" ];
-      volumes = [ "${opts.paths.application_data}/Jellyseer/:/app/config" ];
+      volumes = [ "${opts.paths.app_datafiles}/jellyseer:/app/config" ];
       ports = [ "${opts.ports.jellyseer}:5055" ];
       environment = {
         TZ = opts.timeZone;
