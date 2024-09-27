3ac08ff7489e8b8b176f32b2109c3f057345848b
sreedevk <sreedev@icloud.com>
2024-09-27 20:36:18 +0000

    migrate: container registry

--- nixos/hosts/common/containers/container-registry.nix
+++ nixos/hosts/common/containers/container-registry.nix
@@ -4,6 +4,11 @@
   networking.firewall.allowedTCPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ container-registry-server container-registry-web ]);
   networking.firewall.allowedUDPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ container-registry-server ]);
 
+  systemd.tmpfiles.rules = [
+    "d ${opts.paths.app_datafiles}/containers/server 0755 ${opts.adminUID} ${opts.adminGID} -"
+    "d ${opts.paths.app_datafiles}/containers/web 0755 ${opts.adminUID} ${opts.adminGID} -"
+  ];
+
   # container-registry-web
   virtualisation.oci-containers.containers = {
     "container-registry-server" = {
@@ -11,7 +16,7 @@
       image = "registry:2.7";
       extraOptions = [ "--add-host=${opts.hostname}:${opts.lanAddress}" "--no-healthcheck" ];
       ports = [ "${opts.ports.container-registry-server}:5000" ];
-      volumes = [ "${opts.paths.application_data}/containers/server:/var/lib/registry" ];
+      volumes = [ "${opts.paths.app_datafiles}/containers/server:/var/lib/registry" ];
       labels = {
         "kuma.${opts.hostname}.group.name" = "${opts.hostname}";
         "kuma.registry.http.parent_name" = "${opts.hostname}";
@@ -33,9 +38,9 @@
       extraOptions = [ "--add-host=${opts.hostname}:${opts.lanAddress}" "--no-healthcheck" "--privileged" ];
       ports = [ "${opts.ports.container-registry-web}:8000" ];
       volumes = [
-        "${opts.paths.application_data}/containers/web/data:/opt/data"
-        "${opts.paths.application_data}/containers/web/config.yml:/opt/config.yml:ro"
-        "${opts.paths.application_data}/containers/web/registry_password_file:/run/secrets/registry_password_file:ro"
+        "${opts.paths.app_datafiles}/containers/web/data:/opt/data"
+        "${opts.paths.app_datafiles}/containers/web/config.yml:/opt/config.yml:ro"
+        "${opts.paths.app_datafiles}/containers/web/registry_password_file:/run/secrets/registry_password_file:ro"
       ];
       environment = {
         TZ = opts.timeZone;
