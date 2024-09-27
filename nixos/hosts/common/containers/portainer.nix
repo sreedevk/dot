0999e1e94a49b982e773b6be9c774d3b03a68d1d
sreedevk <sreedev@icloud.com>
2024-09-27 22:50:18 +0000

    portainer migrated

--- nixos/hosts/common/containers/portainer.nix
+++ nixos/hosts/common/containers/portainer.nix
@@ -7,6 +7,10 @@
       portainer_web_secure
     ]);
 
+  systemd.tmpfiles.rules = [
+    "d ${opts.paths.app_datafiles}/Portainer 0755 ${opts.adminUID} ${opts.adminGID} -"
+  ];
+
   virtualisation.oci-containers.containers = {
     portainer = {
       autoStart = true;
@@ -16,7 +20,7 @@
       ports = [ "${opts.ports.portainer_misc}:8000" "${opts.ports.portainer_web_secure}:9443" "${opts.ports.portainer_web}:9000" ];
       volumes = [
         "${opts.paths.podmanSocket}:/var/run/docker.sock"
-        "${opts.paths.application_data}/Portainer:/data"
+        "${opts.paths.app_datafiles}/Portainer:/data"
       ];
       environment = {
         TZ = opts.timeZone;
