16395bc8a218527b855114b9a421b025287cefa7
sreedevk <sreedev@icloud.com>
2024-09-27 21:01:21 +0000

    migrate: homer

--- nixos/hosts/common/containers/homer.nix
+++ nixos/hosts/common/containers/homer.nix
@@ -1,12 +1,17 @@
 { config, lib, pkgs, opts, ... }: {
   networking.firewall.allowedTCPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ homer ]);
+
+  systemd.tmpfiles.rules = [
+    "d ${opts.paths.app_datafiles}/homer 0755 ${opts.adminUID} ${opts.adminGID} -"
+  ];
+
   virtualisation.oci-containers.containers = {
     "homer" = {
       autoStart = true;
       image = "b4bz/homer:latest";
       extraOptions =
         [ "--add-host=${opts.hostname}:${opts.lanAddress}" "--no-healthcheck" ];
-      volumes = [ "${opts.paths.application_data}/homer/:/www/assets" ];
+      volumes = [ "${opts.paths.app_datafiles}/homer:/www/assets" ];
       ports = [ "${opts.ports.homer}:8080" ];
       environment = {
         TZ = opts.timeZone;
