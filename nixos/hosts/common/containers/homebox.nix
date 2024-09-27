701206126a0ffebfa38b5b6bace096a63a12ac2e
sreedevk <sreedev@icloud.com>
2024-09-27 20:58:42 +0000

    migrate: homebox

--- nixos/hosts/common/containers/homebox.nix
+++ nixos/hosts/common/containers/homebox.nix
@@ -1,5 +1,10 @@
 { config, lib, pkgs, opts, ... }: {
   networking.firewall.allowedTCPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ homebox ]);
+
+  systemd.tmpfiles.rules = [
+    "d ${opts.paths.app_datafiles}/homebox 0755 ${opts.adminUID} ${opts.adminGID} -"
+  ];
+
   virtualisation.oci-containers.containers = {
     "homebox" = {
       autoStart = true;
@@ -7,7 +12,7 @@
       extraOptions =
         [ "--add-host=${opts.hostname}:${opts.lanAddress}" "--no-healthcheck" ];
       ports = [ "${opts.ports.homebox}:7745" ];
-      volumes = [ "${opts.paths.application_data}/homebox:/data" ];
+      volumes = [ "${opts.paths.app_datafiles}/homebox:/data" ];
       environment = {
         HBOX_LOG_LEVEL = "info";
         HBOX_LOG_FORMAT = "text";
