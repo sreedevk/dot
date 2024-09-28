3a89d0c28308371b2fc2585a27e43b984bc53e08
sreedevk <sreedev@icloud.com>
2024-09-28 16:35:00 +0000

    actual budgets migrated

--- nixos/hosts/common/containers/actual.nix
+++ nixos/hosts/common/containers/actual.nix
@@ -1,17 +1,12 @@
 { config, lib, pkgs, opts, ... }: {
   networking.firewall.allowedTCPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ actual-app ]);
-
-  systemd.tmpfiles.rules = [
-    "d ${opts.paths.app_datafiles}/Actual 0755 ${opts.adminUID} ${opts.adminGID} -"
-  ];
-
   virtualisation.oci-containers.containers = {
     actual-app = {
       autoStart = true;
       extraOptions = [ "--add-host=${opts.hostname}:${opts.lanAddress}" "--no-healthcheck" ];
       image = "docker.io/actualbudget/actual-server:latest";
       ports = [ "${opts.ports.actual-app}:5006" ];
-      volumes = [ "${opts.paths.app_datafiles}/Actual:/data" ];
+      volumes = [ "actual_budget_data:/data" ];
       labels = {
         "kuma.${opts.hostname}.group.name" = "${opts.hostname}";
         "kuma.actual.http.parent_name" = "${opts.hostname}";
