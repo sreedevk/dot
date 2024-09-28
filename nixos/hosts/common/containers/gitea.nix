b0d62916fa7d89aedad02845b296f9ce35e0ea42
sreedevk <sreedev@icloud.com>
2024-09-28 21:56:13 +0000

    gitea migrated to podman volumes

--- nixos/hosts/common/containers/gitea.nix
+++ nixos/hosts/common/containers/gitea.nix
@@ -2,11 +2,6 @@
   networking.firewall.allowedTCPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ gitea_db gitea_http gitea_ssh ]);
   networking.firewall.allowedUDPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ gitea_ssh ]);
 
-  systemd.tmpfiles.rules = [
-    "d ${opts.paths.app_datafiles}/gitea 0755 ${opts.adminUID} ${opts.adminGID} -"
-    "d ${opts.paths.app_databases}/gitea 0755 ${opts.adminUID} ${opts.adminGID} -"
-  ];
-
   virtualisation.oci-containers.containers = {
     "gitea-app" = {
       autoStart = true;
@@ -23,7 +18,7 @@
         GITEA__database__HOST = "${opts.hostname}:${opts.ports.gitea_db}";
       };
       volumes = [
-        "${opts.paths.app_datafiles}/gitea:/data"
+        "gitea_app:/data"
       ];
       labels = {
         "kuma.${opts.hostname}.group.name" = "${opts.hostname}";
@@ -42,7 +37,7 @@
       ports = [ "${opts.ports.gitea_db}:3306" ];
       cmd = [ "--max-connections=512" ];
       extraOptions = [ "--add-host=${opts.hostname}:${opts.lanAddress}" "--no-healthcheck" ];
-      volumes = [ "${opts.paths.app_databases}/gitea:/var/lib/mysql" ];
+      volumes = [ "gitea_db:/var/lib/mysql" ];
       environmentFiles = [ config.age.secrets.gitea_env.path ];
       environment = {
         PGID = opts.adminGID;
