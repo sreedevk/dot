f5ef2f6cfd40eedb7b592c78acccc2d1c623adde
sreedevk <sreedev@icloud.com>
2024-09-29 19:24:42 +0000

    watch your lan migrated

--- nixos/hosts/common/containers/wyl.nix
+++ nixos/hosts/common/containers/wyl.nix
@@ -5,16 +5,12 @@
     watch-your-lan
   ]);
 
-  systemd.tmpfiles.rules = [
-    "d ${opts.paths.app_datafiles}/WatchYourLan 0755 ${opts.adminUID} ${opts.adminGID} -"
-  ];
-
   virtualisation.oci-containers.containers = {
     "watchyourlan" = {
       autoStart = true;
       image = "aceberg/watchyourlan:v2";
       extraOptions = [ "--network=host" "--no-healthcheck" "--privileged" ];
-      volumes = [ "${opts.paths.app_datafiles}/WatchYourLan:/data/WatchYourLAN" ];
+      volumes = [ "wyl_app:/data/WatchYourLAN" ];
       labels = {
         "kuma.${opts.hostname}.group.name" = "${opts.hostname}";
         "kuma.wyl.http.parent_name" = "${opts.hostname}";
