0c4ba2741dbd946d9cb46e3a7cca5b530ef07baa
sreedevk <sreedev@icloud.com>
2024-09-29 18:56:48 +0000

    stirling pdf migrated

--- nixos/hosts/common/containers/stirling-pdf.nix
+++ nixos/hosts/common/containers/stirling-pdf.nix
@@ -1,24 +1,16 @@
 { pkgs, opts, ... }:
 {
   networking.firewall.allowedTCPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ stirling-pdf ]);
-
-  systemd.tmpfiles.rules = [
-    "d ${opts.paths.app_datafiles}/StirlingPDF/TrainingData 0755 ${opts.adminUID} ${opts.adminGID} -"
-    "d ${opts.paths.app_datafiles}/StirlingPDF/ExtraConfigs 0755 ${opts.adminUID} ${opts.adminGID} -"
-    "d ${opts.paths.app_datafiles}/StirlingPDF/Logs 0755 ${opts.adminUID} ${opts.adminGID} -"
-    "d ${opts.paths.app_datafiles}/StirlingPDF/CustomFiles 0755 ${opts.adminUID} ${opts.adminGID} -"
-  ];
-
   virtualisation.oci-containers.containers = {
     "stirling-pdf" = {
       autoStart = true;
       image = "frooodle/s-pdf:latest";
       extraOptions = [ "--no-healthcheck" ];
       volumes = [
-        "${opts.paths.app_datafiles}/StirlingPDF/TrainingData:/usr/share/tessdata"
-        "${opts.paths.app_datafiles}/StirlingPDF/ExtraConfigs:/configs"
-        "${opts.paths.app_datafiles}/StirlingPDF/Logs:/logs"
-        "${opts.paths.app_datafiles}/StirlingPDF/CustomFiles:/customFiles"
+        "stirling_pdf_trainingdata:/usr/share/tessdata"
+        "stirling_pdf_configs:/configs"
+        "stirling_pdf_logs:/logs"
+        "stirling_pdf_customfiles:/customFiles"
       ];
       ports = [ "${opts.ports.stirling-pdf}:8080" ];
       environment = {
