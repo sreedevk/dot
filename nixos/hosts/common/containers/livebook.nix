e15f495972367e210d0b60a939d90d256070693d
sreedevk <sreedev@icloud.com>
2024-09-29 22:38:53 +0000

    livebook migrated

--- nixos/hosts/common/containers/livebook.nix
+++ nixos/hosts/common/containers/livebook.nix
@@ -1,11 +1,5 @@
 { config, lib, pkgs, opts, ... }: {
-
   networking.firewall.allowedTCPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ livebook_api livebook_http ]);
-
-  systemd.tmpfiles.rules = [
-    "d ${opts.paths.app_datafiles}/livebook 0755 ${opts.adminUID} ${opts.adminGID} -"
-  ];
-
   virtualisation.oci-containers.containers = {
     "livebook" = {
       autoStart = true;
@@ -13,7 +7,7 @@
       extraOptions =
         [ "--add-host=${opts.hostname}:${opts.lanAddress}" "--no-healthcheck" ];
       ports = [ "${opts.ports.livebook_http}:8080" "${opts.ports.livebook_api}:8081" ];
-      volumes = [ "${opts.paths.app_datafiles}/livebook:/data" ];
+      volumes = [ "livebook_data:/data" ];
       environmentFiles = [ config.age.secrets.livebook_env.path ];
       environment = {
         TZ = opts.timeZone;
