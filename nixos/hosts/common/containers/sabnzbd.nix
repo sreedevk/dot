a01e6c18a6e20b3af038569860189d88d104c97a
sreedevk <sreedev@icloud.com>
2024-10-20 18:51:57 +0000

    sabnzbd permissions issue fixed

--- nixos/hosts/common/containers/sabnzbd.nix
+++ nixos/hosts/common/containers/sabnzbd.nix
@@ -23,7 +23,7 @@
       extraOptions = [
         "--add-host=${opts.hostname}:${opts.lanAddress}"
         "--no-healthcheck"
-        "--user=${opts.adminUID}"
+        "--user=${opts.adminUID}:${opts.adminGID}"
       ];
       volumes = [
         "${opts.paths.app_datafiles}/sabnzbd:/config"
