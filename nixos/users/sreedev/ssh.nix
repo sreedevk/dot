ab6d6a5cfa780d26923a14ac6c7fc60e423a82f5
sreedevk <sreedev@icloud.com>
2024-10-26 13:19:06 +0000

    set ssh keys to use default names

--- nixos/users/sreedev/ssh.nix
+++ nixos/users/sreedev/ssh.nix
@@ -12,28 +12,28 @@
         hostname = "sree.dev";
         user = "deploy";
         identitiesOnly = true;
-        identityFile = "~/.ssh/devtechnica";
+        identityFile = "~/.ssh/id_ed25519";
       };
 
       "github.com" = {
         hostname = "github.com";
         user = "git";
         identitiesOnly = true;
-        identityFile = "~/.ssh/devtechnica";
+        identityFile = "~/.ssh/id_ed25519";
       };
 
       "nullptr.sh" = {
         hostname = "100.117.8.42";
         user = "admin";
         identitiesOnly = true;
-        identityFile = "~/.ssh/devtechnica";
+        identityFile = "~/.ssh/id_ed25519";
       };
 
       "nullptrderef1" = {
         hostname = "192.168.1.179";
         user = "admin";
         identitiesOnly = true;
-        identityFile = "~/.ssh/devtechnica";
+        identityFile = "~/.ssh/id_ed25519";
       };
 
       "gitea.nullptr.sh" = {
@@ -41,21 +41,21 @@
         user = "git";
         port = 222;
         identitiesOnly = true;
-        identityFile = "~/.ssh/devtechnica";
+        identityFile = "~/.ssh/id_ed25519";
       };
 
       "gitlab.com" = {
         hostname = "gitlab.com";
         user = "git";
         identitiesOnly = true;
-        identityFile = "~/.ssh/devtechnica";
+        identityFile = "~/.ssh/id_ed25519";
       };
 
       "rpi4b" = {
         hostname = "192.168.1.152";
         user = "pi";
         identitiesOnly = true;
-        identityFile = "~/.ssh/devtechnica";
+        identityFile = "~/.ssh/id_ed25519";
       };
     };
   };
