f039331bedb1403a2c714fcb4587ead6f32a1f1f
sreedevk <sreedev@icloud.com>
2024-11-04 02:36:59 +0000

    animations improved

--- nixos/users/common/hyprland/default.nix
+++ nixos/users/common/hyprland/default.nix
@@ -70,17 +70,17 @@ in
             col.shadow          = rgba(1a1a1aee)
 
             blur {
-              enabled   = true
-              noise = 0.01
-              special = false
-              new_optimizations = true
-              brightness = 1
-              xray      = true
-              size      = 6
-              passes    = 4
-              vibrancy  = 0.1696
-              popups = true
-              contrast = 1
+              enabled            = true
+              noise              = 0.01
+              special            = false
+              new_optimizations  = true
+              brightness         = 1
+              xray               = true
+              size               = 3
+              passes             = 4
+              vibrancy           = 0.1696
+              popups             = true
+              contrast           = 1
               popups_ignorealpha = 0.6
             }
         }
@@ -91,14 +91,32 @@ in
       enable = true;
       text = ''
         animations {
-            enabled   = true
-            bezier    = myBezier, 0.05, 0.9, 0.1, 1.05
-            animation = windows, 1, 6, myBezier
-            animation = windowsOut, 1, 6, default, popin 80%
-            animation = border, 1, 10, default
-            animation = borderangle, 1, 8, default
-            animation = fade, 1, 5, default
-            animation = workspaces, 1, 2, default
+          enabled = true
+          bezier = linear, 0, 0, 1, 1
+          bezier = md3_standard, 0.2, 0, 0, 1
+          bezier = md3_decel, 0.05, 0.7, 0.1, 1
+          bezier = md3_accel, 0.3, 0, 0.8, 0.15
+          bezier = overshot, 0.05, 0.9, 0.1, 1.1
+          bezier = crazyshot, 0.1, 1.5, 0.76, 0.92 
+          bezier = hyprnostretch, 0.05, 0.9, 0.1, 1.0
+          bezier = menu_decel, 0.1, 1, 0, 1
+          bezier = menu_accel, 0.38, 0.04, 1, 0.07
+          bezier = easeInOutCirc, 0.85, 0, 0.15, 1
+          bezier = easeOutCirc, 0, 0.55, 0.45, 1
+          bezier = easeOutExpo, 0.16, 1, 0.3, 1
+          bezier = softAcDecel, 0.26, 0.26, 0.15, 1
+          bezier = md2, 0.4, 0, 0.2, 1 # use with .2s duration
+          animation = windows, 1, 3, md3_decel, popin 60%
+          animation = windowsIn, 1, 3, md3_decel, popin 60%
+          animation = windowsOut, 1, 3, md3_accel, popin 60%
+          animation = border, 1, 10, default
+          animation = fade, 1, 3, md3_decel
+          animation = layersIn, 1, 3, menu_decel, slide
+          animation = layersOut, 1, 1.6, menu_accel
+          animation = fadeLayersIn, 1, 2, menu_decel
+          animation = fadeLayersOut, 1, 4.5, menu_accel
+          animation = workspaces, 1, 7, menu_decel, slide
+          animation = specialWorkspace, 1, 3, md3_decel, slidevert
         }
       '';
     };
