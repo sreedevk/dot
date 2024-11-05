899f7725d614a2d970c3aa4d604868a58c837c7a
sreedevk <sreedev@icloud.com>
2024-11-05 18:08:22 +0000

    hyprland plugins removed

--- nixos/users/common/hyprland/default.nix
+++ nixos/users/common/hyprland/default.nix
@@ -210,19 +210,6 @@ in
             name        = epic-mouse-v1
             sensitivity = -0.5
         }
-
-        plugin {
-            hyprexpo {
-                columns = 3
-                gap_size = 5
-                bg_col = rgb(000000)
-                workspace_method = first 1 # [center/first] [workspace] e.g. first 1 or center m+1
-
-                enable_gesture = false # laptop touchpad, 4 fingers
-                gesture_distance = 300 # how far is the "max"
-                gesture_positive = false
-            }
-        }
       '';
     };
   };
