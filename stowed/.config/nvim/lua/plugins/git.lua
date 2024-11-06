8ead3eb9e4a443a917d6e5e769cc559500c22f7f
sreedevk <sreedev@icloud.com>
2024-11-06 14:29:47 +0000

    diffview added to git pluins group

--- stowed/.config/nvim/lua/plugins/git.lua
+++ stowed/.config/nvim/lua/plugins/git.lua
@@ -1,5 +1,26 @@
 return {
   {
+    "sindrets/diffview.nvim",
+    lazy = true,
+    cmd = {
+      "DiffviewOpen",
+      "DiffviewFileHistory",
+      "DiffviewFocusFiles",
+      "DiffviewToggleFiles",
+      "DiffviewRefresh",
+    },
+    keys = {
+      "<Leader>do",
+      "<Leader>dc",
+      "<Leader>dh",
+    },
+    config = function()
+      vim.api.nvim_set_keymap('n', '<Leader>do', ":DiffviewOpen ", { noremap = true })
+      vim.api.nvim_set_keymap('n', '<Leader>dc', "<cmd>DiffviewClose<CR>", { noremap = true })
+      vim.api.nvim_set_keymap('n', '<Leader>dh', "<cmd>DiffviewFileHistory %<CR>", { noremap = true })
+    end
+  },
+  {
     'tpope/vim-fugitive',
     config = function()
       vim.api.nvim_set_keymap('n', '<Leader>gb', [[<cmd>Git blame<CR>]], { noremap = true })
