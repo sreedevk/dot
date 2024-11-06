69a1e721c02450e036a6b27cf311291675fd0ce9
sreedevk <sreedev@icloud.com>
2024-11-06 15:43:01 -0500

    jujutsu commit gpg signing enabled

--- nixos/users/common/jujutsu.nix
+++ nixos/users/common/jujutsu.nix
@@ -7,6 +7,11 @@
       ui = {
         default-command = "log";
       };
+      signing = {
+        sign-all = true;
+        backend = "gpg";
+        key = "B8C402B16E80E17C";
+      };
       user = {
         name = "sreedevk";
         email = "sreedev@icloud.com";
