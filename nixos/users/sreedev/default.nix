2ce6c5c450cbcd8d42badb4746af933415c19607
sreedevk <sreedev@icloud.com>
2024-11-09 18:55:28 +0000

    fzf options improved

--- nixos/users/sreedev/default.nix
+++ nixos/users/sreedev/default.nix
@@ -53,6 +53,7 @@
       bitwarden-cli
       brightnessctl
       dbeaver-bin
+      delta
       doctl
       duckdb
       emacs
--- stowed/.zsh/zinit.zsh
+++ stowed/.zsh/zinit.zsh
@@ -6,33 +6,53 @@ export ZVM_VI_INSERT_ESCAPE_BINDKEY=jj
 [ -f "/usr/share/fzf/completion.zsh"   ] && zvm_after_init_commands+=('source /usr/share/fzf/completion.zsh')
 [ -f "$HOME/.zsh/plugins.zsh" ] && source "$HOME/.zsh/plugins.zsh"
 
-# FZF
+
 export FZF_TAB_GROUP_COLORS=(
     $'\033[94m' $'\033[32m' $'\033[33m' $'\033[35m' $'\033[31m' $'\033[38;5;27m' $'\033[36m' \
     $'\033[38;5;100m' $'\033[38;5;98m' $'\033[91m' $'\033[38;5;80m' $'\033[92m' \
     $'\033[38;5;214m' $'\033[38;5;165m' $'\033[38;5;124m' $'\033[38;5;120m'
 )
 
-export FZF_COMPLETION_OPTS="--select-1 --exit-0"
+export FZF_TMUX=1
+export FZF_COMPLETION_DIR_COMMANDS="cd pushd rmdir tree"
 
-zstyle ':fzf-tab:*' group-colors $FZF_TAB_GROUP_COLORS
-zstyle ':completion:*:git-checkout:*' sort false
+export FZF_COMPLETION_OPTS="--height 60% \
+--info=inline    \
+--border sharp   \
+--layout reverse \
+--prompt '∷ '    \
+--pointer ▶      \
+--marker ⇒       \
+--select-1       \
+--exit-0"
+
+FZF_LS_PREVIEW='[[ -d ${(Q)realpath} ]] && eza -1 --color=always ${(Q)realpath} || bat --color=always ${(Q)realpath} 2> /dev/null'
+FZF_GIT_SHOW_PREVIEW='case "$group" in
+    "commit tag") git show --color=always $word ;;
+    *) git show --color=always $word | delta ;;
+    esac'
+
+zstyle ':fzf-tab:*'                                 fzf-options $FZF_COMPLETION_OPTS
+zstyle ':fzf-tab:*'                                 group-colors $FZF_TAB_GROUP_COLORS
+zstyle ':fzf-tab:*'                                 switch-group ',' '.'
+zstyle ':completion:*:git-checkout:*'               sort false
 zstyle ':fzf-tab:complete:git-(add|diff|restore):*' fzf-preview 'git diff $word | delta'
-zstyle ':completion:*:descriptions' format '[%d]'
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
-zstyle ':fzf-tab:*' switch-group ',' '.'
-zstyle ':fzf-tab:complete:systemctl-*:*' fzf-preview 'SYSTEMD_COLORS=1 systemctl status $word'
-zstyle ':fzf-tab:complete:ls:*' fzf-preview 'bat --color=always ${(Q)realpath} 2> /dev/null'
-zstyle ':fzf-tab:complete:git-show:*' fzf-preview \
-	'case "$group" in
-	"commit tag") git show --color=always $word ;;
-	*) git show --color=always $word | delta ;;
-	esac'
+zstyle ':completion:*:descriptions'                 format '[%d]'
+zstyle ':completion:*'                              list-colors ${(s.:.)LS_COLORS}
+zstyle ':fzf-tab:complete:cd:*'                     fzf-preview 'eza -1 --color=always $realpath'
+zstyle ':fzf-tab:complete:systemctl-*:*'            fzf-preview 'SYSTEMD_COLORS=1 systemctl status $word'
+zstyle ':fzf-tab:complete:ls:*'                     fzf-preview $FZF_LS_PREVIEW
+zstyle ':fzf-tab:complete:git-show:*'               fzf-preview $FZF_GIT_SHOW_PREVIEW
+
+export FZF_CTRL_T_OPTS=$FZF_COMPLETION_OPTS
+export FZF_CTRL_R_OPTS=$FZF_COMPLETION_OPTS
+export FZF_ALT_C_OPTS=$FZF_COMPLETION_OPTS
 
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+
 [ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
 [ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+
 source "${ZINIT_HOME}/zinit.zsh"
 
 zinit light Aloxaf/fzf-tab
