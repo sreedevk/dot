deecc87c6483e57e42a5b26661ead8e78c55f8bc
sreedevk <sreedev@icloud.com>
2024-11-09 18:31:36 +0000

    resize submaps added

--- nixos/users/common/hyprland/modules/submaps.nix
+++ nixos/users/common/hyprland/modules/submaps.nix
@@ -4,12 +4,16 @@
     ".config/hypr/submaps.conf" = {
       enable = true;
       text = ''
-        bind = ALT, R, submap, resize
+        bind = SUPER, R, submap, resize
         submap = resize
         binde = , right, resizeactive, 10 0
         binde = , left, resizeactive, -10 0
         binde = , up, resizeactive, 0 -10
         binde = , down, resizeactive, 0 10
+        binde = , L, resizeactive, 10 0
+        binde = , H, resizeactive, -10 0
+        binde = , K, resizeactive, 0 -10
+        binde = , J, resizeactive, 0 10
         bind = , escape, submap, reset 
         submap = reset
       '';
