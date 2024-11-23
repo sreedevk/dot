d3e424948afafa3869840d9460ea9377ac656d28
sreedevk <sreedev@icloud.com>
2024-11-23 00:57:06 +0000

    fixed an issue with neogit spinners

--- stowed/.config/nvim/lua/plugins/git.lua
+++ stowed/.config/nvim/lua/plugins/git.lua
@@ -41,7 +41,21 @@ return {
     config = function()
       vim.api.nvim_set_keymap('n', '<Leader>gi', "<cmd>Neogit<CR>", { noremap = true })
 
-      return true
+      local neogit = require("neogit")
+      neogit.setup({
+        disable_hint = true,
+        graph_style = "unicode",
+        process_spinner = false,
+        git_services = {
+          ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
+          ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
+          ["gitlab.com"] =
+          "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+          ["azure.com"] =
+          "https://dev.azure.com/${owner}/_git/${repository}/pullrequestcreate?sourceRef=${branch_name}&targetRef=${target}",
+          ["gitea.nullptr.sh"] = "https://gitea.nullptr.sh/${owner}/${repository}/compare/${branch_name}",
+        },
+      })
     end
   }
 }
