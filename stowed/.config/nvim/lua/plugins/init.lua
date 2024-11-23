2a05553f9e71172dd8b1dd1a8b5e8de713c850c7
sreedevk <sreedev@icloud.com>
2024-11-23 01:58:25 +0000

    add mini.ai

--- stowed/.config/nvim/lua/plugins/init.lua
+++ stowed/.config/nvim/lua/plugins/init.lua
@@ -12,6 +12,14 @@ return {
   'lervag/vimtex',
 
   {
+    'echasnovski/mini.ai',
+    version = '*',
+    config = function()
+      require('mini.ai').setup()
+    end
+  },
+
+  {
     'MeanderingProgrammer/render-markdown.nvim',
     opts = {},
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
