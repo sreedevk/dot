f46979b0bf818d952005156089fcbb7f16b1b781
sreedevk <sreedev@icloud.com>
2024-11-25 23:26:45 +0000

    mini.nvim commit updates

--- stowed/.config/nvim/lua/plugins/mini.lua
+++ stowed/.config/nvim/lua/plugins/mini.lua
@@ -3,8 +3,8 @@ return {
     'echasnovski/mini.comment',
     lazy = true,
     keys = {
-      "gcE",
-      { "gc", mode = "v" }
+      { "gcc", mode = "n" },
+      { "gc",  mode = "v" }
     },
     config = function()
       require('mini.comment').setup()
