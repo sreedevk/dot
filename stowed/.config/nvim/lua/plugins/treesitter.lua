e367e00445f2a83eab917dce93e3931e6ef9fa73
sreedevk <sreedev@icloud.com>
2024-11-25 13:45:59 +0000

    neovim configure ts textobjects

--- stowed/.config/nvim/lua/plugins/treesitter.lua
+++ stowed/.config/nvim/lua/plugins/treesitter.lua
@@ -25,6 +25,34 @@ return {
         auto_install = false,
         incremental_selection = { enable = false },
         indent = { enable = true },
+        textobjects = {
+          swap = {
+            enable = true,
+            swap_next = {
+              ["<C-.>"] = "@parameter.inner",
+            },
+            swap_previous = {
+              ["<C-,>"] = "@parameter.inner",
+            },
+          },
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+              ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+            },
+          },
+          selection_modes = {
+            ['@parameter.outer'] = 'v', -- charwise
+            ['@function.outer'] = 'V',  -- linewise
+            ['@class.outer'] = '<c-v>', -- blockwise
+          },
+          include_surrounding_whitespace = true,
+        },
         playground = {
           enable = true,
           disable = {},
