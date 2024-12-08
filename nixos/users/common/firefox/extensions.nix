4278eafcf6ab2d8722fdd49d598e8f252dcdaa8c
sreedevk <sreedev@icloud.com>
2024-12-08 22:54:33 +0000

    firefox: remove unused extensions

--- nixos/users/common/firefox/extensions.nix
+++ nixos/users/common/firefox/extensions.nix
@@ -2,26 +2,18 @@
 with firefox-addons.packages."${system}"; [
   adnauseam
   bitwarden
-  buster-captcha-solver
-  clearcache
   consent-o-matic
   darkreader
-  duckduckgo-privacy-essentials
   enhancer-for-youtube
   hoarder
-  keybase
-  linkding-extension
-  mullvad
   multi-account-containers
   noscript
   open-url-in-container
   reddit-enhancement-suite
-  sidebery
   sponsorblock
   tab-reloader
   temporary-containers
   tubearchivist-companion
   ublock-origin
   vimium
-  web-archives
 ]
