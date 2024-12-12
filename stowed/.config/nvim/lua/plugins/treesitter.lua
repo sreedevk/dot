708b9e3f43ff2a7b2c583ce27ee9182e7fa25884
sreedevk <sreedev@icloud.com>
2024-12-12 22:24:49 +0000

    treewalker added to nvim

--- stowed/.config/nvim/lua/plugins/treesitter.lua
+++ stowed/.config/nvim/lua/plugins/treesitter.lua
@@ -108,5 +108,17 @@ return {
   {
     'nvim-treesitter/nvim-treesitter-refactor',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  },
+  {
+    "aaronik/treewalker.nvim",
+    opts = {
+      highlight = true
+    },
+    config = function()
+      vim.api.nvim_set_keymap('n', '<C-j>', ':Treewalker Down<CR>', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<C-k>', ':Treewalker Up<CR>', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<C-h>', ':Treewalker Left<CR>', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<C-l>', ':Treewalker Right<CR>', { noremap = true })
+    end
   }
 }
