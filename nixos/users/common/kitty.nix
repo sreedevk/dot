98c898ba940d7d24916d49223f35cff47d9dcd44
sreedevk <sreedev@icloud.com>
2024-12-24 22:58:53 +0000

    kitty scrollback pager migrated to neovim

--- nixos/users/common/kitty.nix
+++ nixos/users/common/kitty.nix
@@ -5,7 +5,6 @@ in
 {
 
   stylix.targets.kitty.enable = true;
-
   xdg.desktopEntries = {
     kitty = {
       name = "Kitty";
@@ -16,9 +15,9 @@ in
       categories = [ "System" "TerminalEmulator" ];
     };
   };
-
   programs.kitty = {
     enable = true;
+    package = nixglmod.nixGLWrapped pkgs.kitty "kitty";
     settings = {
       scrollback_lines = 10000;
       enable_audio_bell = false;
@@ -29,36 +28,9 @@ in
       "ctrl+=" = "change_font_size all +2.0";
       "ctrl+-" = "change_font_size all -2.0";
     };
-    package = nixglmod.nixGLWrapped pkgs.kitty "kitty";
     extraConfig = ''
-      foreground               #e0def4
-      background               #191724
-      selection_foreground     #e0def4
-      selection_background     #403d52
-      cursor                   #524f67
-      cursor_text_color        #e0def4
-      url_color                #c4a7e7
-      active_tab_foreground    #e0def4
-      active_tab_background    #26233a
-      inactive_tab_foreground  #6e6a86
-      inactive_tab_background  #191724
-      color0                   #26233a
-      color8                   #6e6a86
-      color1                   #eb6f92
-      color9                   #eb6f92
-      color2                   #31748f
-      color10                  #31748f
-      color3                   #f6c177
-      color11                  #f6c177
-      color4                   #9ccfd8
-      color12                  #9ccfd8
-      color5                   #c4a7e7
-      color13                  #c4a7e7
-      color6                   #ebbcba
-      color14                  #ebbcba
-      color7                   #e0def4
-      color15                  #e0def4
-
+      copy_on_select yes
+      scrollback_pager ${pkgs.neovim}/bin/nvim -c "lua require('utils'):colorize()"
     '';
   };
 }
--- stowed/.config/nvim/lua/utils.lua
+++ stowed/.config/nvim/lua/utils.lua
@@ -22,6 +22,33 @@ function M.convert_cword_to_snake()
   vim.cmd('normal! "_ciw' .. snakeCaseWord)
 end
 
+function M.colorize()
+  vim.wo.number = false
+  vim.wo.relativenumber = false
+  vim.wo.statuscolumn = ""
+  vim.wo.signcolumn = "no"
+  vim.opt.listchars = { space = " " }
+
+  local buf = vim.api.nvim_get_current_buf()
+
+  local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+  while #lines > 0 and vim.trim(lines[#lines]) == "" do
+    lines[#lines] = nil
+  end
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, {})
+
+  vim.b[buf].minianimate_disable = true
+
+  vim.api.nvim_chan_send(vim.api.nvim_open_term(buf, {}), table.concat(lines, "\r\n"))
+  vim.keymap.set("n", "q", "<cmd>qa!<cr>", { silent = true, buffer = buf })
+  vim.api.nvim_create_autocmd("TextChanged", { buffer = buf, command = "normal! G$" })
+  vim.api.nvim_create_autocmd("TermEnter", { buffer = buf, command = "stopinsert" })
+
+  vim.defer_fn(function()
+    vim.b[buf].minianimate_disable = false
+  end, 2000)
+end
+
 function M.fetchjson()
   local url    = vim.fn.expand('<cWORD>')
   local handle = io.popen('curl ' .. url)
