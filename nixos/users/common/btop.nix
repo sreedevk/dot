a467d327fe58e90772b8e65e7b42ff58b584c1f8
sreedevk <sreedev@icloud.com>
2024-12-26 03:32:33 +0000

    stylix btop conflicts resolved

--- nixos/users/common/btop.nix
+++ nixos/users/common/btop.nix
@@ -1,9 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, stylix, ... }:
 {
+  stylix.targets.btop.enable = true;
+
   programs.btop = {
     enable = true;
     settings = {
-      theme_background = true;
       truecolor = true;
       force_tty = false;
       presets = "cpu:1:default,proc:0:default cpu:0:default,mem:0:default,net:0:default cpu:0:block,net:0:tty";
