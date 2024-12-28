9d6388d819c37bd9b0c3c02b5ea80afe6947fd56
sreedevk <sreedev@icloud.com>
2024-12-28 17:39:47 +0000

    gpg agent config added (disabled) + toggle term improvements

--- nixos/users/common/gpg.nix
+++ nixos/users/common/gpg.nix
@@ -1,5 +1,13 @@
 { pkgs, opts, ... }:
 {
+
+  services.gpg-agent = {
+    enable = false;
+    pinentryPackage = pkgs.pinentry-curses;
+    defaultCacheTtl = 86400;
+    maxCacheTtl = 86400;
+  };
+
   programs.gpg = {
     enable = true;
     mutableKeys = true;
@@ -7,6 +15,7 @@
     settings = {
       no-greeting = true;
       default-key = "54E80FA653BDD4DC6700A695B8C402B16E80E17C";
+      pinentry-mode = "loopback";
       default-recipient-self = true;
       force-v3-sigs = true;
       escape-from-lines = true;
