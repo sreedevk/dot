b9409419c8b2f9cbae90ff05a8be64eb5557e903
sreedevk <sreedev@icloud.com>
2024-12-28 01:32:02 +0000

    added an additional colorscheme for nvim

--- stowed/.config/nvim/lua/plugins/colorscheme.lua
+++ stowed/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,21 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function ()
-    vim.cmd([[colorscheme tokyonight-night]])
-  end
+  {
+    "theamallalgi/zitchdog",
+    lazy = false,
+    enabled = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme zitchdog-grape]])
+    end
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    enabled = true,
+    priority = 1000,
+    opts = {},
+    config = function()
+      vim.cmd([[colorscheme tokyonight-night]])
+    end
+  }
 }
