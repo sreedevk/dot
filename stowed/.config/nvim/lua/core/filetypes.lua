f1a839f4bf8008633cfc6081071ed6077dc7382e
sreedevk <sreedev@icloud.com>
2024-12-31 00:19:50 +0000

    neovim filetypes added

--- stowed/.config/nvim/lua/core/filetypes.lua
+++ stowed/.config/nvim/lua/core/filetypes.lua
@@ -2,6 +2,7 @@ vim.filetype.add {
   extension = {
     h = "c",
     CSV = "csv",
+    lock = 'yaml',
     scheme = "scheme",
     ex = "elixir",
     exs = "elixir",
@@ -17,10 +18,17 @@ vim.filetype.add {
     janet = "janet"
   },
   filename = {
+    ['NEOGIT_COMMIT_EDITMSG'] = 'NeogitCommitMessage',
     ["docker-compose.yml"] = "yaml.docker-compose",
     ["docker-compose.yaml"] = "yaml.docker-compose",
     ["compose.yml"] = "yaml.docker-compose",
     ["compose.yaml"] = "yaml.docker-compose",
     ["mix.lock"] = "elixir"
+  },
+  pattern = {
+    ['.*%.conf'] = 'conf',
+    ['.*%.theme'] = 'conf',
+    ['.*%.gradle'] = 'groovy',
+    ['^.env%..*'] = 'bash',
   },
 }
