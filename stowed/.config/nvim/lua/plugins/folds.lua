e52e6c5612d6d9cbd8c89af91a0b3d694b9443dd
sreedevk <sreedev@icloud.com>
2024-12-31 03:08:39 +0000

    neovim folds improved

---
+++ stowed/.config/nvim/lua/plugins/folds.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    lazy = true,
+    ft = { "markdown" },
+    init = function()
+      vim.o.foldcolumn = '1'
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+    end,
+    opts = {}
+  },
+  {
+    "jghauser/fold-cycle.nvim",
+    lazy = true,
+    ft = { "markdown" },
+    keys = {
+      { '<tab>',   function() require('fold-cycle').open() end,  desc = "Open Folds",  silent = true },
+      { '<s-tab>', function() require('fold-cycle').close() end, desc = "Close Folds", silent = true }
+    },
+    opts = {
+      open_if_max_closed = true,
+      close_if_max_opened = true,
+      softwrap_movement_fix = false
+    },
+  }
+}
