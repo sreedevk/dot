dbd174669944465c0582c48b79583e0fd1cec499
sreedevk <sreedev@icloud.com>
2025-01-06 15:15:07 +0000

    kitty opacity settings fixed

--- nixos/users/common/kitty/settings.nix
+++ nixos/users/common/kitty/settings.nix
@@ -1,22 +1,22 @@
 { config, ... }:
 {
-  scrollback_lines = 10000;
-  enable_audio_bell = false;
-  resize_draw_strategy = "scale";
-  update_check_interval = 0;
-  opacity = 0.95;
+  active_tab_font_style = "italic";
+  background_opacity = 0.8;
   bell_on_tab = " ";
   copy_on_select = true;
   cursor_blink_interval = 0;
   cursor_trail = 0;
   cursor_trail_start_threshold = 100;
   disable_ligatures = "cursor";
+  enable_audio_bell = false;
   font_family = "Iosevka NF";
   font_size = 14.0;
-  scrollback_pager = "${config.programs.neovim.package}/bin/nvim -c \"lua require('core.utils'):colorize()\"";
   pointer_shape_when_dragging = "hand";
-  touch_scroll_multiplier = 2.0;
-  tab_bar_edge = "top";
+  resize_draw_strategy = "scale";
+  scrollback_lines = 10000;
+  scrollback_pager = "${config.programs.neovim.package}/bin/nvim -c \"lua require('core.utils'):colorize()\"";
   tab_bar_align = "left";
-  active_tab_font_style = "italic";
+  tab_bar_edge = "top";
+  touch_scroll_multiplier = 2.0;
+  update_check_interval = 0;
 }
