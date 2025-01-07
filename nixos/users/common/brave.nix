1b5d1203b1ee8d6633dacaebe99feff6b25ce2e9
sreedevk <sreedev@icloud.com>
2025-01-07 22:10:23 +0000

    brave flags separated from brave module

--- nixos/users/common/brave.nix
+++ nixos/users/common/brave.nix
@@ -6,20 +6,4 @@ in
   home.packages = [
     (nixglmod.nixGLWrapped pkgs.brave "brave")
   ];
-
-  home.file = {
-    ".config/brave-flags.conf" = {
-      enable = true;
-      # --disable-features=Vulkan,UseChromeOSDirectVideoDecoder 
-      text = ''
-        --disable-gpu-memory-buffer-video-frames
-        --ozone-platform=wayland
-        --ozone-platform-hint=auto
-        --enable-features=WebRTCPipeWireCapturer,VaapiVideoDecodeLinuxGL,VaapiVideoDecoder,VaapiIgnoreDriverChecks
-        --ignore-gpu-blocklist
-        --enable-gpu-rasterization
-        --enable-zero-copy
-      '';
-    };
-  };
 }
