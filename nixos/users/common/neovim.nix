80cdb26796303e4814333e6989cd1a6b2e93c081
sreedevk <sreedev@icloud.com>
2025-01-08 16:53:36 +0000

    neovim desktop entry fixed

--- nixos/users/common/neovim.nix
+++ nixos/users/common/neovim.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, ... }:
+{ pkgs, inputs, config, ... }:
 let
   language_servers = with pkgs; [
     clojure-lsp
@@ -36,15 +36,35 @@ in
 
   xdg.desktopEntries = {
     neovim = {
-      name = "Neovim";
+      name = "NeoVim";
+      icon = "nvim";
       genericName = "Text Editor";
-      exec = "nvim %F";
+      exec = "${config.programs.kitty.package}/bin/kitty -- ${config.programs.neovim.package}/bin/nvim %F";
       comment = "Text Editor";
       mimeType = [
         "text/plain"
         "text/html"
+        "text/x-chdr"
+        "text/x-csrc"
+        "text/x-c++hdr"
+        "text/x-c++src"
+        "text/x-java"
+        "text/x-dsrc"
+        "text/x-pascal"
+        "text/x-perl"
+        "text/x-python"
+        "application/x-php"
+        "application/x-httpd-php3"
+        "application/x-httpd-php4"
+        "application/x-httpd-php5"
+        "application/xml"
+        "text/html"
+        "text/css"
+        "text/x-sql"
+        "text/x-diff"
       ];
       terminal = false;
+      categories = [ "GTK" "Development" "IDE" "TextEditor" ];
       type = "Application";
     };
   };
