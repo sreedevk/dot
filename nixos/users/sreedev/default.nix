0afc915c97a96e33658d3658fb9f4e1689601409
sreedevk <sreedev@icloud.com>
2025-01-29 14:57:56 +0000

    nixpkgs updated

--- nixos/users/sreedev/default.nix
+++ nixos/users/sreedev/default.nix
@@ -16,6 +16,7 @@ in
     ../common/fastfetch.nix
     ../common/firefox
     ../common/fontconfig.nix
+    ../common/ghostty.nix
     ../common/git.nix
     ../common/github.nix
     ../common/gpg.nix
