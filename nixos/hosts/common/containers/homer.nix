080626dff80ea85b721ff8f656c1b6b5eab36e19
sreedevk <sreedev@icloud.com>
2025-03-30 11:40:21 +0000

    homer config git sourced

--- nixos/hosts/common/containers/homer.nix
+++ nixos/hosts/common/containers/homer.nix
@@ -1,5 +1,22 @@
-{ config, lib, pkgs, opts, ... }: {
+{ config, lib, pkgs, opts, ... }:
+let
+  homerConfigSource = builtins.fetchGit {
+    name = "homer";
+    url = "https://xgit.nullptr.sh/nullptrderef1/homer";
+    rev = "5504fa7b8b48eb59e27a6d3c4da68725a95f9718";
+    ref = "main";
+    shallow = true;
+  };
+in
+{
   networking.firewall.allowedTCPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ homer ]);
+
+  environment.etc = {
+    "homerconf" = {
+      enable = true;
+      source = homerConfigSource;
+    };
+  };
 
   systemd.tmpfiles.rules = [
     "d ${opts.paths.app_datafiles}/homer 0755 ${opts.adminUID} ${opts.adminGID} -"
@@ -11,7 +28,10 @@
       image = "b4bz/homer:latest";
       extraOptions =
         [ "--add-host=${opts.hostname}:${opts.lanAddress}" "--no-healthcheck" ];
-      volumes = [ "${opts.paths.app_datafiles}/homer:/www/assets" ];
+      volumes = [
+        "/etc/homerconf:/www/assets:ro"
+        # "${opts.paths.app_datafiles}/homer:/www/assets"
+      ];
       ports = [ "${opts.ports.homer}:8080" ];
       environment = {
         TZ = opts.timeZone;
