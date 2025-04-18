01a67540309ab8b3d13022548e33fdc12b625d84
sreedevk <sreedev@icloud.com>
2025-04-18 09:55:17 +0000

    vim plug reproducible

--- nixos/users/common/vim.nix
+++ nixos/users/common/vim.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 let
   vimPlug = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim";
-    sha256 = "1nywzjd9nfr7sqqbdi69wza305q3vp26i0390j1884wdz6awid10";
+    url = "https://raw.githubusercontent.com/junegunn/vim-plug/baa66bcf349a6f6c125b0b2b63c112662b0669e1/plug.vim";
+    sha256 = "01kkg3c05si97r4kkdhn5n17kcqp56hjh4d74l9aajd0d629kn62";
   };
 in
 {
