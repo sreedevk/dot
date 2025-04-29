2e6df7bc581b1513269e011da6682723b42382fa
sreedevk <sreedev@icloud.com>
2025-04-29 13:36:27 +0000

    feat: automatic asdf reshim post nix switch

--- nixos/users/common/asdf.nix
+++ nixos/users/common/asdf.nix
@@ -1,6 +1,13 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   home.packages = with pkgs; [ asdf-vm ];
+
+  home.activation = {
+    reshimASDF = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      ${pkgs.asdf-vm}/bin/asdf reshim
+    '';
+  };
+
   home.file = {
     ".tool-versions" = {
       enable = true;
@@ -8,13 +15,13 @@
       text =
         let
           versions = [
-            { tool = "bun"; version = "1.2.8"; }
-            { tool = "deno"; version = "2.2.6"; }
+            { tool = "bun"; version = "1.2.11"; }
+            { tool = "deno"; version = "2.2.12"; }
             { tool = "elixir"; version = "1.18.3-otp-27"; }
-            { tool = "erlang"; version = "27.3.1"; }
-            { tool = "gleam"; version = "1.9.1"; }
+            { tool = "erlang"; version = "27.3.3"; }
+            { tool = "gleam"; version = "1.10.0"; }
             { tool = "golang"; version = "1.24.2"; }
-            { tool = "nim"; version = "2.2.2"; }
+            { tool = "nim"; version = "2.2.4"; }
             { tool = "ruby"; version = "3.4.2"; }
             { tool = "zig"; version = "0.14.0"; }
           ];
