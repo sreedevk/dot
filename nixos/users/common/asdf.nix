d02df788a02e252822e2b20383ffcef5b1697ef1
sreedevk <sreedev@icloud.com>
2025-05-23 11:20:10 +0000

    added elm to asdf

--- nixos/users/common/asdf.nix
+++ nixos/users/common/asdf.nix
@@ -24,6 +24,7 @@
             { tool = "nim"; version = "2.2.4"; }
             { tool = "ruby"; version = "3.4.2"; }
             { tool = "zig"; version = "0.14.0"; }
+            { tool = "elm"; version = "0.19.1"; }
           ];
         in
         builtins.concatStringsSep "\n" (builtins.map (v: "${v.tool} ${v.version}") versions);
