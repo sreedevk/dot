2f1b4817a1f0583b11c4159a33f07bd8c9028696
sreedevk <sreedev@icloud.com>
2025-05-26 11:30:05 +0000

    re-enabled brave install via nix due to GPU issues

--- nixos/users/sreedev/default.nix
+++ nixos/users/sreedev/default.nix
@@ -9,6 +9,7 @@ in
     ../common/asdf.nix
     ../common/awscli.nix
     ../common/base.nix
+    ../common/brave.nix
     ../common/btop.nix
     ../common/cargo.nix
     ../common/core-packages.nix
