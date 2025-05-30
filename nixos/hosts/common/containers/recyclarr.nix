e77ffa695d3bc9ff857ec0b0076b12dbf4ba32a4
sreedevk <sreedev@icloud.com>
2025-05-30 13:13:44 +0000

    recyclarr config improvements

--- nixos/hosts/common/containers/recyclarr.nix
+++ nixos/hosts/common/containers/recyclarr.nix
@@ -7,17 +7,20 @@
   virtualisation.oci-containers.containers = {
     recyclarr = {
       autoStart = opts.autostart-non-essential-services;
-      image = "recyclarr/recyclarr:latest";
+      image = "ghcr.io/recyclarr/recyclarr:latest";
       dependsOn = [
         "sonarr"
         "radarr"
       ];
+      user = "${opts.adminUID}:${opts.adminGID}";
       extraOptions = [ "--add-host=${opts.hostname}:${opts.lanAddress}" "--no-healthcheck" ];
       volumes = [ "${opts.paths.app_datafiles}/recyclarr:/config" ];
       environment = {
         TZ = opts.timeZone;
+        CRON_SCHEDULE = "@daily";
         PUID = opts.adminUID;
         PGID = opts.adminGID;
+        RECYCLARR_CREATE_CONFIG = true;
       };
     };
   };
