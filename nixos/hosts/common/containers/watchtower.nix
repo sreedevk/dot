4b2315ee87591d2e61d530a0d69416181824e161
sreedevk <sreedev@icloud.com>
2025-06-02 20:27:24 +0000

    added extra args on watchtowerr

--- nixos/hosts/common/containers/watchtower.nix
+++ nixos/hosts/common/containers/watchtower.nix
@@ -17,6 +17,8 @@
         WATCHTOWER_NOTIFICATION_GOTIFY_TLS_SKIP_VERIFY = "true";
         WATCHTOWER_NOTIFICATION_GOTIFY_URL = "http://${opts.hostname}:${opts.ports.gotify}";
         WATCHTOWER_ROLLING_RESTART = "false";
+        # WATCHTOWER_DISABLE_CONTAINERS = ""; disable on particular conainers with comma/space separated strings
+        # WATCHTOWER_LABEL_ENABLE = "true"; label: com.centurylinklabs.watchtower.enable=true
       };
     };
   };
