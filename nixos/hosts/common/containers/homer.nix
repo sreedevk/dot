9a88deebe2e347edf7cdc1f0a1c20aad8af33641
sreedevk <sreedev@icloud.com>
2025-06-02 11:51:20 +0000

    homer config cleanup

--- nixos/hosts/common/containers/homer.nix
+++ nixos/hosts/common/containers/homer.nix
@@ -1,13 +1,5 @@
-{ config, lib, pkgs, opts, ... }:
+{ pkgs, opts, ... }:
 let
-  homerConfigSource = builtins.fetchGit {
-    name = "homer";
-    url = "https://git.external.nullptr.sh/nullptrderef1/homer";
-    rev = "86d55dc9f802f0916f7668969edf99015ffd53e9";
-    ref = "main";
-    shallow = true;
-  };
-
   homerConfigDirectory = builtins.path {
     name = "homer-config";
     path = ../configurations/homer;
@@ -15,22 +7,6 @@ let
 in
 {
   networking.firewall.allowedTCPPorts = builtins.map pkgs.lib.strings.toInt (with opts.ports; [ homer ]);
-
-  environment.etc = {
-    "homerconf" = {
-      enable = true;
-      source = homerConfigSource;
-    };
-  };
-
-  systemd.services."podman-homer" = {
-    restartTriggers = [ homerConfigSource ];
-  };
-
-  systemd.tmpfiles.rules = [
-    "d ${opts.paths.app_datafiles}/homer 0755 ${opts.adminUID} ${opts.adminGID} -"
-  ];
-
   virtualisation.oci-containers.containers = {
     "homer" = {
       autoStart = true;
