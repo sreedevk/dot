1aa0124a3e9a724497c67b303ee00b39c4c3bae3
sreedevk <sreedev@icloud.com>
2025-06-30 11:42:10 +0000

    egl vendor library filename var added to hyprland session

--- nixos/users/common/hyprland/opts.nix
+++ nixos/users/common/hyprland/opts.nix
@@ -23,6 +23,7 @@ in
     __NV_PRIME_RENDER_OFFLOAD = "1";
     __VK_LAYER_NV_optimus = "NVIDIA_only";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    __EGL_VENDOR_LIBRARY_FILENAMES = "/usr/share/glvnd/egl_vendor.d/10_nvidia.json";
     AQ_DRM_DEVICES = "/dev/dri/card0:/dev/dri/card1";
     GBM_BACKEND = "nvidia-drm";
     GDK_DPI_SCALE = "1";
