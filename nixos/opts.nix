80e631cb4fe33809078c850aa2c1cbd3daaf1b46
sreedevk <sreedev@icloud.com>
2025-07-16 17:40:01 -0400

    restic moved to custom systemd service

--- nixos/opts.nix
+++ nixos/opts.nix
@@ -10,6 +10,21 @@
     wallpaper = "wallhaven-p96odm.png";
   };
 
+  addresses = {
+    lan = {
+      nullptrderef1 = "192.168.1.179";
+      devstation = "192.168.1.249";
+      rpi4b = "192.168.1.151";
+      devtechnica = "138.197.225.180";
+    };
+    tailscale = {
+      nullptrderef1 = "100.117.8.42";
+      devstation = "100.109.36.108";
+      rpi4b = "";
+      devtechnica = "";
+    };
+  };
+
   publicKeys = {
     devstation = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIyTIQBuC8gK9HjVViXha1VVTc8mStsrWU1umEM0puuP";
     nullptrderef1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2pNg6tFrSaQ+oTlGAhboBgQQ+I7S3sXoyM1DfOsI0f";
--- nixos/users/sreedev/restic.nix
+++ nixos/users/sreedev/restic.nix
@@ -1,22 +1,55 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 {
-  services.restic = {
-    enable = true;
-    backups = {
-      remote-backup-nullptrderef1 = {
-        repository = "sftp:nullptrderef1:/mnt/dpool0/backups/devstation/restic-repository";
-        passwordFile = config.age.secrets.restic_backup_password.path;
-        initialize = false;
-        paths = [
-          "${builtins.getEnv "HOME"}/Data/work"
-          "${builtins.getEnv "HOME"}/Data/finances"
-          "${builtins.getEnv "HOME"}/Data/notebook"
-          "${builtins.getEnv "HOME"}/Data/resources"
-        ];
-        pruneOpts = [ "--keep-daily 7" ];
-        timerConfig = {
+  systemd.user = {
+    services = {
+      restic-backup = {
+        Unit = {
+          Description = "Restic Backup Process to nullptrderef1 Server";
+          Documentation = "info:restic man:restic backup(1) https://taskwarrior.org/docs/";
+          After = [
+            "network-online.target"
+            "agenix.service"
+          ];
+          Wants = [
+            "network-online.target"
+            "agenix.service"
+          ];
+        };
+        Service = {
+          Type = "oneshot";
+          Environment = [
+            "RESTIC_PASSWORD_FILE=${config.age.secrets.restic_backup_password.path}"
+            "RESTIC_REPOSITORY=sftp:nullptrderef1:/mnt/dpool0/backups/devstation/restic-repository"
+          ];
+          ExecStart = [
+            # backup
+            "${pkgs.restic}/bin/restic backup --skip-if-unchanged ${builtins.getEnv "HOME"}/Data/work"
+            "${pkgs.restic}/bin/restic backup --skip-if-unchanged ${builtins.getEnv "HOME"}/Data/finances"
+            "${pkgs.restic}/bin/restic backup --skip-if-unchanged ${builtins.getEnv "HOME"}/Data/resources"
+            "${pkgs.restic}/bin/restic backup --skip-if-unchanged ${builtins.getEnv "HOME"}/Data/notebook"
+
+            # prune and check
+            "${pkgs.restic}/bin/restic unlock"
+            "${pkgs.restic}/bin/restic forget --prune --keep-daily 7"
+            "${pkgs.restic}/bin/restic check"
+          ];
+        };
+      };
+    };
+
+    timers = {
+      restic-backup-timers = {
+        Unit = {
+          Description = "Timer for Restic Backup Process";
+        };
+        Timer = {
+          OnBootSec = "5min";
           OnCalendar = "00:05";
           RandomizedDelaySec = "5h";
+          Unit = "taskwarrior-sync.service";
+        };
+        Install = {
+          WantedBy = [ "timers.target" ];
         };
       };
     };
