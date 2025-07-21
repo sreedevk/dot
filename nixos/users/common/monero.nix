899285322d9568cff7240f9b5db76142d9639036
sreedevk <sreedev@icloud.com>
2025-07-21 13:33:20 +0000

    removed restricted rpc xmr temporarily

--- nixos/users/common/monero.nix
+++ nixos/users/common/monero.nix
@@ -24,7 +24,7 @@
         Service = {
           Type = "simple";
           ExecStart = [
-            "${pkgs.monero-cli}/bin/monerod --non-interactive --config-file=${config.age.secrets.moneroconf.path} --data-dir=${builtins.getEnv "HOME"}/.bitmonero/ --rpc-bind-ip=0.0.0.0  --rpc-bind-port=18081 --restricted-rpc --confirm-external-bind"
+            "${pkgs.monero-cli}/bin/monerod --non-interactive --config-file=${config.age.secrets.moneroconf.path} --data-dir=${builtins.getEnv "HOME"}/.bitmonero/ --rpc-bind-ip=0.0.0.0 --rpc-bind-port=18081 --confirm-external-bind"
           ];
         };
         Install = {
