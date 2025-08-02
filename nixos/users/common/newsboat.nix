9695b0c575e8c225c29624f66c08cbf57a51c24f
sreedevk <sreedev@icloud.com>
2025-08-02 13:29:37 +0000

    newsboat browser option linked to nix opts

--- nixos/users/common/newsboat.nix
+++ nixos/users/common/newsboat.nix
@@ -1,11 +1,11 @@
-{ pkgs, config, ... }:
+{ pkgs, config, opts, ... }:
 {
   programs.newsboat = {
     enable = true;
     autoReload = false;
-    reloadThreads = 8;
+    reloadThreads = 10;
     reloadTime = 120;
-    browser = "brave";
+    browser = "${opts.desktop.browser.bin}";
     maxItems = 30;
     extraConfig = ''
       # ---- OPTS
@@ -50,8 +50,8 @@
       bind-key ^E      edit-flags
 
       # macros
-      macro y set browser "echo %u | xclip -sel clip"; open-in-browser; set browser brave
-      macro a set browser mpv; one; set browser brave
+      macro y set browser "echo %u | xclip -sel clip"; open-in-browser; set browser ${opts.desktop.browser.bin}
+      macro a set browser mpv; one; set browser ${opts.desktop.browser.bin}
 
       # notification
       notify-format "Newsboat: %f unread feeds (%n unread articles total)"
