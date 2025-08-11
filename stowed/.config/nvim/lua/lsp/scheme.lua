fdc9dbdd16f22bb9380d513298f0d20c8eb45421
sreedevk <sreedev@icloud.com>
2025-08-11 18:14:32 +0000

    scheme langserver configuration fixed

--- stowed/.config/nvim/lua/lsp/scheme.lua
+++ stowed/.config/nvim/lua/lsp/scheme.lua
@@ -3,7 +3,14 @@ require('core.utils').setup_lsp {
   custom = false,
   enable = true,
   filetypes = { 'scheme' },
-  cmd = { 'scheme-langserver' }
+  cmd = {
+    'scheme-langserver',
+    '/tmp/scheme-langserver.log',
+    --enable multi-thread
+    'enable',
+    --disable type inference, because it's on very early stage.
+    'diable',
+  }
 }
 
 -- chicken scheme lsp server
