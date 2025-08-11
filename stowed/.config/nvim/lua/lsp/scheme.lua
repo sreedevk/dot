eddad32c5726c70fae492e3ff2ad619adc5555d3
sreedevk <sreedev@icloud.com>
2025-08-11 14:15:25 +0000

    replaced chicken scheme lsp with scheme-langserver

--- stowed/.config/nvim/lua/lsp/scheme.lua
+++ stowed/.config/nvim/lua/lsp/scheme.lua
@@ -1,7 +1,16 @@
 require('core.utils').setup_lsp {
-  name = 'chicken-scheme-lsp',
-  custom = true,
+  name = 'scheme-langserver',
+  custom = false,
   enable = true,
   filetypes = { 'scheme' },
-  cmd = { 'chicken-lsp-server' },
+  cmd = { 'scheme-langserver' }
 }
+
+-- chicken scheme lsp server
+-- require('core.utils').setup_lsp {
+--   name = 'chicken-scheme-lsp',
+--   custom = true,
+--   enable = true,
+--   filetypes = { 'scheme' },
+--   cmd = { 'chicken-lsp-server' },
+-- }
