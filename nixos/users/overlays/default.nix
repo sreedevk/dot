c2a9bbfdf18d1c673f3beaed921e47a02ce36cc7
sreedevk <sreedev@icloud.com>
2025-08-27 00:55:24 +0000

    generalize lmms hm overlay for all stable packages

--- nixos/users/overlays/default.nix
+++ nixos/users/overlays/default.nix
@@ -1,8 +1,8 @@
 { inputs, ... }:
 [
   (import ./agenix.nix { inherit inputs; })
-  (import ./lmms.nix { inherit inputs; })
   (import ./quickshell.nix { inherit inputs; })
+  (import ./stable.nix { inherit inputs; })
   inputs.nixgl.overlay
   inputs.nur.overlays.default
 ]
