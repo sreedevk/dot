3cb1b6409a50bd0bbc9728e8cd0963fd811c9693
sreedevk <sreedev@icloud.com>
2025-09-13 22:38:49 +0000

    vim plug updated

--- nixos/users/common/vim.nix
+++ nixos/users/common/vim.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 let
   vimPlug = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/junegunn/vim-plug/baa66bcf349a6f6c125b0b2b63c112662b0669e1/plug.vim";
+    url = "https://raw.githubusercontent.com/junegunn/vim-plug/904dac1530cfec0880cd44e4f7f206eb2e9e3247/plug.vim";
     sha256 = "01kkg3c05si97r4kkdhn5n17kcqp56hjh4d74l9aajd0d629kn62";
   };
 in
