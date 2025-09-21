cbd6c95004280ef751e6a1f1034ac846faed3238
sreedevk <sreedev@icloud.com>
2025-09-21 20:33:05 +0000

    standalone nixpkgs config updated

--- stowed/.config/nixpkgs/config.nix
+++ stowed/.config/nixpkgs/config.nix
@@ -1,3 +1,9 @@
 {
   allowUnfree = true;
+  allowBroken = true;
+  packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/main.tar.gz") {
+      inherit pkgs;
+    };
+  };
 }
