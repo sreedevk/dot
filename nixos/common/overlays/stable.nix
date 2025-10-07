dfa8fb1130008483bcbfd147721d2044e5a19043
sreedevk <sreedev@icloud.com>
2025-10-07 10:12:11 +0000

    fix unstable packages

--- nixos/common/overlays/stable.nix
+++ nixos/common/overlays/stable.nix
@@ -1,4 +1,4 @@
 { inputs, ... }:
 _: prev: {
-  inherit (inputs.stablepkgs.legacyPackages.${prev.system}) lmms jellyfin-media-player;
+  inherit (inputs.stablepkgs.legacyPackages.${prev.system}) lmms jellyfin-media-player ledger libvdpau-va-gl sonic-pi awscli2;
 }
