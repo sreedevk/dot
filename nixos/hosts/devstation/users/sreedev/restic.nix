c6674e8579996905e344acb2d7b6e1ce975f043c
sreedevk <sreedev@icloud.com>
2025-10-16 15:36:26 +0000

    Revert "restic: migrate to home-manager module"
    This reverts commit e95b274f919982a3377d13b04fa8fef51834814f.

--- nixos/hosts/devstation/users/sreedev/restic.nix
+++ nixos/hosts/devstation/users/sreedev/restic.nix
@@ -1,5 +1,6 @@
 { pkgs
 , config
+, opts
 , ...
 }:
 {
@@ -8,30 +9,65 @@
     restic
   ];
 
-  services.restic = {
-    enable = true;
-    backups = {
-      "sreedev@devstation" = {
-        repository = "sftp:nullptr.sh:/mnt/dpool1/backups/sreedev@devstation";
-        passwordFile = config.age.secrets.restic_backup_password.path;
-        pruneOpts = [
-          "--keep-daily 7"
-          "--keep-weekly 5"
-          "--keep-monthly 6"
-          "--keep-yearly 0"
-        ];
-        paths = builtins.map (v: "${builtins.getEnv "HOME"}/${v}") [
-          ".thunderbird"
-          "Data/finances"
-          "Data/work"
-          "Data/resources"
-          "Data/notebook"
-        ];
-        runCheck = true;
-        timerConfig = {
+  systemd.user = {
+    services = {
+      restic-backup = {
+        Unit = {
+          Description = "Restic Backup Process to Apollo Server";
+          Documentation = "info:restic man:restic backup(1)";
+          After = [
+            "network-online.target"
+            "agenix.service"
+          ];
+          Wants = [
+            "network-online.target"
+            "agenix.service"
+          ];
+        };
+        Service = {
+          Type = "oneshot";
+          Environment = [
+            "RESTIC_PASSWORD_FILE=${config.age.secrets.restic_backup_password.path}"
+            "RESTIC_REPOSITORY=sftp:nullptr.sh:/mnt/dpool1/backups/${opts.hostname}/restic-repository"
+          ];
+
+          ExecStart =
+            let
+              homedir = dirs: (builtins.map (v: "${builtins.getEnv "HOME"}/${v}") dirs);
+              backupdirs = builtins.concatStringsSep " " (homedir [
+                ".thunderbird"
+                "Data/finances"
+                "Data/work"
+                "Data/resources"
+                "Data/notebook"
+              ]);
+            in
+            [
+              # backup
+              "${pkgs.restic}/bin/restic backup --skip-if-unchanged ${backupdirs}"
+
+              # prune and check
+              "${pkgs.restic}/bin/restic unlock"
+              "${pkgs.restic}/bin/restic forget --prune --keep-daily 7"
+              "${pkgs.restic}/bin/restic check"
+            ];
+        };
+      };
+    };
+
+    timers = {
+      restic-backup-timers = {
+        Unit = {
+          Description = "Timer for Restic Backup Process";
+        };
+        Timer = {
+          OnBootSec = "5min";
           OnCalendar = "00:05";
-          Persistent = true;
           RandomizedDelaySec = "5h";
+          Unit = "restic-backup.service";
+        };
+        Install = {
+          WantedBy = [ "timers.target" ];
         };
       };
     };
