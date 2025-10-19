9281365b26b501e6f65aabbd4f38c710e602160d
sreedevk <sreedev@icloud.com>
2025-10-19 12:33:16 +0000

    added scope shortcut for supercollider in neovim

--- stowed/.config/nvim/lua/packages/supercollider.lua
+++ stowed/.config/nvim/lua/packages/supercollider.lua
@@ -20,18 +20,21 @@ return {
     scnvim.setup({
       keymaps = {
         ['<M-CR>'] = {
-          -- map('editor.send_line', { 'i', 'n' }),
           map('editor.send_block', { 'i', 'n' }),
           map('editor.send_selection', 'x'),
         },
+
         ['<M-e>'] = map('postwin.toggle', { 'i', 'n' }),
         ['<M-L>'] = map('postwin.clear', { 'n', 'i' }),
         ['<C-k>'] = map('signature.show', { 'n', 'i' }),
+
         ['<leader>scx'] = map('sclang.hard_stop', { 'n', 'x', 'i' }),
         ['<leader>scs'] = map('sclang.start'),
         ['<leader>scr'] = map('sclang.recompile'),
+
         ['<leader>scb'] = map_expr('s.boot'),
         ['<leader>scm'] = map_expr('s.meter'),
+        ['<leader>sco'] = map_expr('s.scope'),
       },
       editor = {
         highlight = {
