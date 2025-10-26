9ce0ad0765d79170aa36238f77fc2f3514889938
sreedevk <sreedev@icloud.com>
2025-10-26 23:43:58 +0000

    taskwarrior add unplanned tag +coefficient

--- nixos/common/hm/taskwarrior.nix
+++ nixos/common/hm/taskwarrior.nix
@@ -60,50 +60,30 @@ let
     coefficients = {
       user = {
         tags = [
-          {
-            name = "important";
-            coefficient = 15.0;
-          }
+          { name = "important"; coefficient = 15.0;  }
+          { name = "unplanned"; coefficient = -10.0; }
         ];
         projects = [
-          {
-            name = "learn:tech";
-            coefficient = 5.0;
-          }
-          {
-            name = "blog";
-            coefficient = 5.0;
-          }
-          {
-            name = "opensource";
-            coefficient = 5.0;
-          }
-          {
-            name = "personal:homenet";
-            coefficient = 5.0;
-          }
-          {
-            name = "work";
-            coefficient = 8.0;
-          }
-          {
-            name = "other";
-            coefficient = 4.0;
-          }
+          { name = "learn:tech"; coefficient = 5.0;       }
+          { name = "blog"; coefficient = 5.0;             }
+          { name = "opensource"; coefficient = 5.0;       }
+          { name = "personal:homenet"; coefficient = 5.0; }
+          { name = "work"; coefficient = 8.0;             }
+          { name = "other"; coefficient = 4.0;            }
         ];
       };
 
       system = {
-        due = 12.0;
-        blocking = 2.0;
-        scheduled = 5.0;
-        active = 4.0;
-        age = 2.0;
+        due         = 12.0;
+        blocking    = 2.0;
+        scheduled   = 5.0;
+        active      = 4.0;
+        age         = 2.0;
         annotations = 1.0;
-        tags = 1.0;
-        project = 1.0;
-        waiting = -3.0;
-        blocked = -5.0;
+        tags        = 1.0;
+        project     = 1.0;
+        waiting     = -3.0;
+        blocked     = -5.0;
       };
       uda = {
         H = 6.0;
