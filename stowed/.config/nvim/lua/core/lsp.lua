ae48b8f1a66147ee4c28b3c10858f18cfc944412
sreedevk <sreedev@icloud.com>
2025-11-02 15:55:21 +0000

    disable bacon-ls

--- stowed/.config/nvim/lua/core/lsp.lua
+++ stowed/.config/nvim/lua/core/lsp.lua
@@ -1,7 +1,7 @@
 vim.lsp.enable({
   "lua_ls", "astro", "clojure_lsp", "tailwindcss", "elixirls", "elmls", "erlangls", "eslint",
   "fennel_ls", "gleam", "hls", "html", "janet_lsp", "jsonls", "jsonnet_ls", "ltex_plus", "marksman",
-  "nimls", "nil_ls", "ocamllsp", "pylsp", "ruby_lsp", "rust_analyzer", "bacon_ls", "metals",
+  "nimls", "nil_ls", "ocamllsp", "pylsp", "ruby_lsp", "rust_analyzer", "metals",
   "scheme_langserver", "taplo", "ts_ls", "tinymist", "yamlls", "zls"
 })
 
@@ -9,7 +9,7 @@ vim.lsp.config('rust_analyzer', {
   settings = {
     ['rust-analyzer'] = {
       diagnostics = {
-        enable = false,
+        enable = true, -- disable this when using "bacon_ls" as a diagnostics lsp for rust
       }
     }
   }
