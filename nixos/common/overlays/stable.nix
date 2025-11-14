ae47faf947b0c5b1d02f594791a8ce7719311904
sreedevk <sreedev@icloud.com>
2025-11-14 23:42:37 +0000

    move fixed packages out of stable channel

--- nixos/common/overlays/stable.nix
+++ nixos/common/overlays/stable.nix
@@ -2,13 +2,8 @@
 _: prev: {
   inherit (inputs.stablepkgs.legacyPackages.${prev.stdenv.hostPlatform.system})
     ardour
-    awscli2
     jellyfin-media-player
-    ledger
-    libvdpau-va-gl
-    lmms
     obs-studio
     obs-studio-plugins
-    sonic-pi
     ;
 }
