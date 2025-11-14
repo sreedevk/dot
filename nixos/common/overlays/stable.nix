5122bc9300521ac6c883a8c148ec9f3d3cc06531
sreedevk <sreedev@icloud.com>
2025-11-14 20:05:12 +0000

    stabilize obs

--- nixos/common/overlays/stable.nix
+++ nixos/common/overlays/stable.nix
@@ -1,4 +1,14 @@
 { inputs, ... }:
 _: prev: {
-  inherit (inputs.stablepkgs.legacyPackages.${prev.stdenv.hostPlatform.system}) lmms jellyfin-media-player ledger libvdpau-va-gl sonic-pi awscli2;
+  inherit (inputs.stablepkgs.legacyPackages.${prev.stdenv.hostPlatform.system})
+    ardour
+    awscli2
+    jellyfin-media-player
+    ledger
+    libvdpau-va-gl
+    lmms
+    obs-studio
+    obs-studio-plugins
+    sonic-pi
+    ;
 }
