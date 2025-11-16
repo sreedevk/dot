b934c02c07edaf24ba43f047876570afc842618a
sreedevk <sreedev@icloud.com>
2025-11-16 13:05:15 +0000

    added furnace: chiptune tracker

--- nixos/hosts/devstation/users/sreedev/modules/packages.nix
+++ nixos/hosts/devstation/users/sreedev/modules/packages.nix
@@ -21,6 +21,7 @@
         (config.lib.nixGL.wrap pkgs.sqlitebrowser)
         (config.lib.nixGL.wrap pkgs.tor-browser)
         (config.lib.nixGL.wrapOffload pkgs.davinci-resolve)
+        (config.lib.nixGL.wrapOffload pkgs.furnace)
         (config.lib.nixGL.wrapOffload pkgs.gimp3-with-plugins)
         (config.lib.nixGL.wrapOffload pkgs.jellyfin-media-player)
         (config.lib.nixGL.wrapOffload pkgs.upscayl)
