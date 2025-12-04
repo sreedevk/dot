1623ee6bae08d444edf06f94df7bcf62e719c5aa
sreedevk <sreedev@icloud.com>
2025-12-04 11:41:20 +0000

    hopefully this fixes the keyboard layout being set to "UNKNOKWN" in noctalia bar

--- nixos/common/hm/hyprland/opts.nix
+++ nixos/common/hm/hyprland/opts.nix
@@ -94,7 +94,7 @@ in
 
   inputs = {
     kb_layout = "us,apl";
-    kb_variant = ",dyalog";
+    kb_variant = "qwerty,dyalog";
     kb_model = "";
     kb_options = "ctrl:nocaps,compose:ralt,grp:shifts_toggle";
     kb_rules = "";
