3b698f0ebc00a490942260c032d0830bf6bab841
sreedevk <sreedev@icloud.com>
2026-01-04 22:35:48 +0000

    update vicinae

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     llama-cpp.url  = "github:ggml-org/llama.cpp?ref=b7541";
     nixpkgs.url    = "github:nixos/nixpkgs?ref=nixos-unstable&shallow=1";
     stablepkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11&shallow=1";
-    vicinae.url    = "github:vicinaehq/vicinae?ref=v0.18.0";
+    vicinae.url    = "github:vicinaehq/vicinae?ref=v0.18.1";
 
     disko = {
       url                    = "github:nix-community/disko";
