116a7c163e9753ef9692d3e6a0f8c34b470517fb
sreedevk <sreedev@icloud.com>
2026-01-19 11:52:06 +0000

    setup koito

--- secrets.nix
+++ secrets.nix
@@ -46,6 +46,7 @@ with builtins;
   "secrets/nyc0/jellystat_env.age".publicKeys                 = attrValues nyc0;
   "secrets/nyc0/jira-token.age".publicKeys                    = attrValues nyc0;
   "secrets/nyc0/k3s_token.age".publicKeys                     = attrValues nyc0;
+  "secrets/nyc0/koito_env.age".publicKeys                     = attrValues nyc0;
   "secrets/nyc0/miniflux_env.age".publicKeys                  = attrValues nyc0;
   "secrets/nyc0/n8n_env.age".publicKeys                       = attrValues nyc0;
   "secrets/nyc0/next_explorer_env.age".publicKeys             = attrValues nyc0;
