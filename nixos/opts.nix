0c11688446982da2ec9ef268ca69d7d14c7f099d
sreedevk <sreedev@icloud.com>
2026-01-21 13:29:42 +0000

    rpi4b has new ssh keys

--- nixos/opts.nix
+++ nixos/opts.nix
@@ -40,7 +40,7 @@
     apollo        = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINiYLIEBtoti0D2R5/nuGzXTQaYP7OynXMkAuJBeNit6";
     devstation    = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIyTIQBuC8gK9HjVViXha1VVTc8mStsrWU1umEM0puuP";
     olivetin      = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIByeKyDW/vNAU29/sBzbyCH1eDTPMPhYDbhAxnNuv1lu";
-    rpi4b         = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAul9ZMOMARHw6iSIFbQKChc/bkFBx5/mZnrer/YsRvV";
+    rpi4b         = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9aN56ElYeLVtsmZsB0U1t1JlY5xI018brNEHV7X2Am";
     terminus      = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKwPdv2zFlfcDbbRvpIYBPh/oRR7MUYjH397Ma+Tu5iB";
   };
 
