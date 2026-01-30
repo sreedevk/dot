f7211152a4e66cda2a9180b75e46e83764825625
sreedevk <sreedev@icloud.com>
2026-01-30 12:19:04 +0000

    treesitter migration

--- stowed/.config/nvim/lua/packages/treesitter.lua
+++ stowed/.config/nvim/lua/packages/treesitter.lua
@@ -1,5 +1,20 @@
 return {
   {
+    "MeanderingProgrammer/treesitter-modules.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = {
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<CR>",
+          scope_incremental = "<S-CR>",
+          node_incremental = "<TAB>",
+          node_decremental = "<S-TAB>",
+        },
+      },
+    },
+  },
+  {
     "chrisgrieser/nvim-various-textobjs",
     event = "VeryLazy",
     opts = {
@@ -19,178 +34,36 @@ return {
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
     build = ":TSUpdate",
-    branch = "master",
+    branch = "main",
     config = function()
-      vim.o.foldmethod = 'expr'
-      vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      local ts = require 'nvim-treesitter'
+      local parsers = { "bash", "c", "cpp", "css", "csv", "desktop", "diff", "dockerfile", "eex", "elixir", "erlang",
+        "fennel", "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore", "gleam", "haskell",
+        "haskell_persistent", "heex", "html", "http", "hyprlang", "janet_simple", "javascript", "jq", "json", "latex",
+        "ledger", "lua", "make", "markdown", "markdown_inline", "nginx", "nix", "ocaml", "ocaml_interface", "ocamllex",
+        "python", "ruby", "rust", "sql", "ssh_config", "supercollider", "tmux", "todotxt", "toml", "tsv", "typescript",
+        "xml", "yaml", "zathurarc", "zig" }
 
-      vim.treesitter.language.register("bash", "apkbuild")
-      vim.treesitter.language.register("journal", "ledger")
+      local langmaps = {
+        { "journal", "ledger" }
+      }
 
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup({
-        ensure_installed = {
-          "bash",
-          "c",
-          "cpp",
-          "css",
-          "csv",
-          "desktop",
-          "diff",
-          "dockerfile",
-          "eex",
-          "elixir",
-          "erlang",
-          "fennel",
-          "git_config",
-          "git_rebase",
-          "gitattributes",
-          "gitcommit",
-          "gitignore",
-          "gleam",
-          "haskell",
-          "haskell_persistent",
-          "heex",
-          "html",
-          "http",
-          "hyprlang",
-          "janet_simple",
-          "javascript",
-          "jq",
-          "json",
-          "latex",
-          "ledger",
-          "lua",
-          "make",
-          "markdown",
-          "markdown_inline",
-          "nginx",
-          "nix",
-          "ocaml",
-          "ocaml_interface",
-          "ocamllex",
-          "python",
-          "ruby",
-          "rust",
-          "sql",
-          "ssh_config",
-          "supercollider",
-          "tmux",
-          "todotxt",
-          "toml",
-          "tsv",
-          "typescript",
-          "xml",
-          "yaml",
-          "zathurarc",
-          "zig",
-        },
-        sync_install = false,
-        ignore_install = { "comment" },
-        auto_install = false,
-        incremental_selection = {
-          enable = true,
-          disable = vim.g.auxbuffers,
-          keymaps = {
-            init_selection = "<CR>",
-            scope_incremental = "<S-CR>",
-            node_incremental = "<TAB>",
-            node_decremental = "<S-TAB>",
-          },
-        },
-        indent = { enable = true },
-        refactor = {
-          navigation = {
-            enable = true,
-            disable = function(lang, bufnr)
-              return lang == 'csv' and vim.api.nvim_buf_line_count(bufnr) > 10000
-            end,
-            keymaps = {
-              goto_definition_lsp_fallback = "gd",
-            },
-          },
-          smart_rename = {
-            enable = true,
-            disable = function(lang, bufnr)
-              return lang == 'csv' and vim.api.nvim_buf_line_count(bufnr) > 10000
-            end,
-            keymaps = {
-              smart_rename = "trr",
-            },
-          },
-          highlight_definitions = {
-            enable = true,
-            disable = function(lang, bufnr)
-              return lang == 'csv' and vim.api.nvim_buf_line_count(bufnr) > 10000
-            end,
-            clear_on_cursor_move = true,
-            additional_vim_regex_highlighting = false,
-          },
-        },
-        textobjects = {
-          swap = {
-            enable = true,
-            disable = function(lang, bufnr)
-              return lang == 'csv' and vim.api.nvim_buf_line_count(bufnr) > 10000
-            end,
-            swap_next = {
-              ["<M-0>"] = "@parameter.inner",
-            },
-            swap_previous = {
-              ["<M-9>"] = "@parameter.inner",
-            },
-          },
-          lsp_interop = {
-            enable = true,
-            disable = function(lang, bufnr)
-              return lang == 'csv' and vim.api.nvim_buf_line_count(bufnr) > 10000
-            end,
-            border = 'none',
-            floating_preview_opts = {},
-            peek_definition_code = {
-              ["<leader>gd"] = "@function.outer",
-              ["<leader>gD"] = "@class.outer",
-            },
-          },
-          select = {
-            enable = true,
-            disable = function(lang, bufnr)
-              return lang == 'csv' and vim.api.nvim_buf_line_count(bufnr) > 10000
-            end,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-              ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
-            },
-          },
-          move = {
-            enable = true,
-            disable = function(lang, bufnr)
-              return lang == 'csv' and vim.api.nvim_buf_line_count(bufnr) > 10000
-            end,
-            set_jumps = true,
-            goto_next_start = {
-              ["]m"] = "@function.outer",
-              ["]]"] = { query = "@class.outer", desc = "next class start" },
-            },
-          },
-          selection_modes = {
-            ['@parameter.outer'] = 'v', -- charwise
-            ['@function.outer'] = 'V',  -- linewise
-            ['@class.outer'] = '<c-v>', -- blockwise
-          },
-          include_surrounding_whitespace = true,
-        },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-          disable = function(lang, bufnr)
-            return lang == 'csv' and vim.api.nvim_buf_line_count(bufnr) > 10000
-          end,
-        }
+      for _, langmap in ipairs(langmaps) do
+        vim.treesitter.language.register(langmap[1], langmap[2])
+      end
+
+      for _, parser in ipairs(parsers) do
+        ts.install(parser)
+      end
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = parsers,
+        callback = function()
+          vim.wo[0][0].foldmethod = 'expr'
+          vim.wo[0][0].foldexpr   = 'v:lua.vim.treesitter.foldexpr()'
+          vim.bo.indentexpr       = "v:lua.require'nvim-treesitter'.indentexpr()"
+          vim.treesitter.start()
+        end,
       })
     end
   },
@@ -235,15 +108,70 @@ return {
   },
   {
     'nvim-treesitter/nvim-treesitter-textobjects',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    lazy = true,
-    event = "BufReadPost"
-  },
-  {
-    'nvim-treesitter/nvim-treesitter-refactor',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    lazy = true,
-    event = "BufReadPost"
+    dependencies = { "nvim-treesitter/nvim-treesitter", branch = "main" },
+    branch = "main",
+    init = function()
+      vim.g.no_plugin_maps = true
+    end,
+    config = function()
+      require("nvim-treesitter-textobjects").setup {
+        select = {
+          lookahead = true,
+          selection_modes = {
+            ['@parameter.outer'] = 'v', -- charwise
+            ['@function.outer'] = 'V',  -- linewise
+            ['@class.outer'] = '<c-v>', -- blockwise
+          },
+          include_surrounding_whitespace = false,
+        },
+        move = {
+          set_jumps = true,
+        },
+      }
+
+      local select = require "nvim-treesitter-textobjects.select"
+      vim.keymap.set({ "x", "o" }, "af", function() select.select_textobject("@function.outer", "textobjects") end)
+      vim.keymap.set({ "x", "o" }, "if", function() select.select_textobject("@function.inner", "textobjects") end)
+      vim.keymap.set({ "x", "o" }, "ac", function() select.select_textobject("@class.outer", "textobjects") end)
+      vim.keymap.set({ "x", "o" }, "ic", function() select.select_textobject("@class.inner", "textobjects") end)
+      vim.keymap.set({ "x", "o" }, "as", function() select.select_textobject("@local.scope", "locals") end)
+
+      -- Swaps
+      local swap = require("nvim-treesitter-textobjects.swap")
+      vim.keymap.set("n", "<M-0>", function() swap.swap_next "@parameter.inner" end)
+      vim.keymap.set("n", "<M-9>", function() swap.swap_previous "@parameter.outer" end)
+
+
+      local move = require("nvim-treesitter-textobjects.move")
+      vim.keymap.set({ "n", "x", "o" }, "]m", function() move.goto_next_start("@function.outer", "textobjects") end)
+      vim.keymap.set({ "n", "x", "o" }, "]]", function() move.goto_next_start("@class.outer", "textobjects") end)
+      vim.keymap.set({ "n", "x", "o" }, "]o",
+        function() move.goto_next_start({ "@loop.inner", "@loop.outer" }, "textobjects") end)
+      vim.keymap.set({ "n", "x", "o" }, "]s", function() move.goto_next_start("@local.scope", "locals") end)
+      vim.keymap.set({ "n", "x", "o" }, "]z", function() move.goto_next_start("@fold", "folds") end)
+
+      vim.keymap.set({ "n", "x", "o" }, "]M", function() move.goto_next_end("@function.outer", "textobjects") end)
+      vim.keymap.set({ "n", "x", "o" }, "][", function() move.goto_next_end("@class.outer", "textobjects") end)
+
+      vim.keymap.set({ "n", "x", "o" }, "[m", function() move.goto_previous_start("@function.outer", "textobjects") end)
+      vim.keymap.set({ "n", "x", "o" }, "[[", function() move.goto_previous_start("@class.outer", "textobjects") end)
+
+      vim.keymap.set({ "n", "x", "o" }, "[M", function() move.goto_previous_end("@function.outer", "textobjects") end)
+      vim.keymap.set({ "n", "x", "o" }, "[]", function() move.goto_previous_end("@class.outer", "textobjects") end)
+
+      vim.keymap.set({ "n", "x", "o" }, "]q", function() move.goto_next("@conditional.outer", "textobjects") end)
+      vim.keymap.set({ "n", "x", "o" }, "[q", function() move.goto_previous("@conditional.outer", "textobjects") end)
+
+      local ts_repeat_move = require "nvim-treesitter-textobjects.repeatable_move"
+
+      vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+      vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+
+      vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+      vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+      vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+      vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
+    end,
   },
   {
     "aaronik/treewalker.nvim",
