50e1e894a4caa632e1afa2f02e1f4e54da06ce3f
sreedevk <sreedev@icloud.com>
2026-02-12 15:36:35 +0000

    remove n8n

--- secrets.nix
+++ secrets.nix
@@ -46,7 +46,6 @@ with builtins;
   "secrets/nyc0/miniflux_env.age".publicKeys                  = attrValues nyc0;
   "secrets/nyc0/minio_env.age".publicKeys                     = attrValues nyc0;
   "secrets/nyc0/moneroconf.age".publicKeys                    = attrValues nyc0;
-  "secrets/nyc0/n8n_env.age".publicKeys                       = attrValues nyc0;
   "secrets/nyc0/next_explorer_env.age".publicKeys             = attrValues nyc0;
   "secrets/nyc0/nullptrderef1_admin_password.age".publicKeys  = attrValues nyc0;
   "secrets/nyc0/openai_api_key.age".publicKeys                = attrValues nyc0;
