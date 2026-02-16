8c0f332d9808255d17facd93ca01a1b3eebd6307
sreedevk <sreedev@icloud.com>
2026-02-16 11:46:01 +0000

    add neovim autocmd to auto create dir when writing files

--- stowed/.config/nvim/lua/core/commands.lua
+++ stowed/.config/nvim/lua/core/commands.lua
@@ -65,3 +65,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
     }
   end
 })
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = vim.api.nvim_create_augroup("auto_create_dir", {}),
+  callback = function(event)
+    if event.match:match("^%w%w+:[\\/][\\/]") then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
