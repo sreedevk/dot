622aa0b1adf2daa4a3411d62ffe84947282207c3
sreedevk <sreedev@icloud.com>
2026-02-17 01:15:23 +0000

    replace cmp with blink

---
+++ stowed/.config/nvim/lua/packages/blink.lua
@@ -0,0 +1,108 @@
+return {
+  {
+    'saghen/blink.compat',
+    -- use v2.* for blink.cmp v1.*
+    version = '2.*',
+    lazy = true,
+    opts = {},
+  },
+  {
+    'saghen/blink.cmp',
+    dependencies = {
+      {
+        'Gelio/cmp-natdat',
+        config = true,
+      },
+      {
+        "mikavilpas/blink-ripgrep.nvim",
+        version = "*",
+      },
+      'kirasok/cmp-hledger',
+    },
+    version = '1.*',
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      keymap = {
+        preset = 'none',
+        ["<C-k>"] = { 'select_prev', 'fallback' },
+        ["<C-j>"] = { 'select_next', 'fallback' },
+        ["<C-b>"] = {
+          function(cmp)
+            cmp.scroll_documentation_up(4)
+          end,
+        },
+        ["<C-f>"] = {
+          function(cmp)
+            cmp.scroll_documentation_down(4)
+          end,
+        },
+        ["<C-e>"] = { 'cancel' },
+        ["<CR>"] = { 'accept', 'fallback' },
+      },
+
+      appearance = {
+        nerd_font_variant = 'mono'
+      },
+
+      cmdline = {
+        enabled = false,
+      },
+
+      completion = {
+        keyword = { range = 'full' },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 1800
+        },
+        accept = {
+          auto_brackets = {
+            enabled = false,
+          },
+        },
+        auto_show = false,
+        ghost_text = {
+          enabled = true,
+        },
+      },
+      sources = {
+        default = {
+          'lsp',
+          'path',
+          'snippets',
+          'natdat',
+          'buffer',
+          'hledger',
+          'ripgrep'
+        },
+        providers = {
+          hledger = {
+            name = 'hledger',
+            module = 'blink.compat.source',
+            score_offset = -1,
+            opts = {}
+          },
+          ripgrep = {
+            module = "blink-ripgrep",
+            name = "Ripgrep",
+            score_offset = -3,
+            opts = {},
+          },
+          natdat = {
+            name = 'natdat',
+            module = 'blink.compat.source',
+            score_offset = 1,
+            opts = {}
+          },
+          snippets = {
+            opts = {
+              friendly_snippets = true,
+            },
+          },
+        },
+      },
+      fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+    opts_extend = { "sources.default" }
+  }
+}
