49a10fc4626a705beaccce6fb6ab6c2d71437d77
sreedevk <sreedev@icloud.com>
2026-02-18 01:12:40 +0000

    replace aerial nvim with outline nvim

---
+++ stowed/.config/nvim/lua/packages/outline.lua
@@ -0,0 +1,16 @@
+return {
+  "hedyhli/outline.nvim",
+  lazy = true,
+  cmd = { "Outline", "OutlineOpen" },
+  keys = {
+    { "<Leader>ce", "<cmd>Outline<CR>", desc = "Toggle Code Outline" },
+  },
+  opts = {
+    outline_window = {
+      position = 'right',
+      auto_close = false,
+      auto_jump = false,
+      jump_highlight_duration = 300,
+    }
+  },
+}
