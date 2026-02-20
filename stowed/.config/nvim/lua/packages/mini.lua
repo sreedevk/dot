b65c0d6dbe1d92c6827558d1e70159d19e07c1ee
sreedevk <sreedev@icloud.com>
2026-02-20 01:23:36 +0000

    nvim: add mini.cmdline

--- stowed/.config/nvim/lua/packages/mini.lua
+++ stowed/.config/nvim/lua/packages/mini.lua
@@ -48,4 +48,31 @@ return {
     config = true,
     version = false,
   },
+  {
+    'nvim-mini/mini.cmdline',
+    version = '*',
+    opts = {
+      autocomplete = {
+        enable = true,
+        delay = 0,
+        predicate = nil,
+        map_arrows = true,
+      },
+
+      autocorrect = {
+        enable = false,
+        func = nil,
+      },
+
+      autopeek = {
+        enable = true,
+        n_context = 1,
+        predicate = nil,
+        window = {
+          config = {},
+          statuscolumn = nil,
+        },
+      },
+    }
+  },
 }
