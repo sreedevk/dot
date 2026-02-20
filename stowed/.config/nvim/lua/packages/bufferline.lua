dcebab93735ea625a4fc57644270e2155881096d
sreedevk <sreedev@icloud.com>
2026-02-20 14:41:18 +0000

    improve bufferline

--- stowed/.config/nvim/lua/packages/bufferline.lua
+++ stowed/.config/nvim/lua/packages/bufferline.lua
@@ -5,7 +5,21 @@ return {
   opts = {
     options = {
       themable = true,
-      numbers = "ordinal" -- "none" | "ordinal" | "buffer_id" | "both"
+      numbers = "buffer_id", -- "none" | "ordinal" | "buffer_id" | "both"
+      color_icons = true,
+      show_buffer_icons = true,
+      show_buffer_close_icons = false,
+      show_close_icon = false,
+      show_tab_indicators = true,
+      duplicates_across_groups = false,
+      separator_style = "slope", -- "slant" | "slope" | "thick" | "thin"
+      always_show_bufferline = false,
+      auto_toggle_bufferline = true,
+      hover = {
+        enabled = false,
+        delay = 200,
+        reveal = { 'close' }
+      },
     },
   }
 }
