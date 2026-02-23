492604b0c024666cbd8ec58fae22b5f3325d64a0
sreedevk <sreedev@icloud.com>
2026-02-23 13:47:29 +0000

    gitgraph removed

--- stowed/.config/nvim/lua/packages/git.lua
+++ stowed/.config/nvim/lua/packages/git.lua
@@ -123,38 +123,4 @@ return {
       end
     },
   },
-  {
-    'isakbm/gitgraph.nvim',
-    opts = {
-      git_cmd = "git",
-      symbols = {
-        merge_commit = 'M',
-        commit = '*',
-      },
-      format = {
-        timestamp = '%H:%M:%S %d-%m-%Y',
-        fields = { 'hash', 'timestamp', 'author', 'branch_name', 'tag' },
-      },
-      hooks = {
-        on_select_commit = function(commit)
-          vim.notify('DiffviewOpen ' .. commit.hash .. '^!')
-          vim.cmd(':DiffviewOpen ' .. commit.hash .. '^!')
-        end,
-        on_select_range_commit = function(from, to)
-          vim.notify('DiffviewOpen ' .. from.hash .. '~1..' .. to.hash)
-          vim.cmd(':DiffviewOpen ' .. from.hash .. '~1..' .. to.hash)
-        end,
-      },
-    },
-    keys = {
-      {
-        "<leader>gg",
-        function()
-          require('gitgraph').draw({}, { all = true, max_count = 5000 })
-        end,
-        desc = "GitGraph - Draw",
-      },
-    },
-  },
-
 }
