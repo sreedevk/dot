fe7d4f4c0f68051bacb5dd762d3624492a110462
sreedevk <sreedev@icloud.com>
2026-02-25 14:56:49 +0000

    try moving pkgs to unstable

--- nixos/common/overlays/stable.nix
+++ nixos/common/overlays/stable.nix
@@ -2,15 +2,7 @@
 _: prev: {
   inherit (inputs.stablepkgs.legacyPackages.${prev.stdenv.hostPlatform.system})
     adguard
-    ardour
-    awscli2
-    davinci-resolve
-    gurk-rs
     imager
-    jellyfin-media-player
-    obs-studio
-    obs-studio-plugins
-    silicon
     zls
     ;
 }
