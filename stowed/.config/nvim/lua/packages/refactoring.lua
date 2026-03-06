a5f8330675b7124a13b437e0666a27cb6b7054fc
sreedevk <sreedev@icloud.com>
2026-03-06 18:43:35 +0000

    refactoring nvim lazy loading enabled

--- stowed/.config/nvim/lua/packages/refactoring.lua
+++ stowed/.config/nvim/lua/packages/refactoring.lua
@@ -4,6 +4,72 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
-  lazy = false,
+  cmd = { "Refactor" },
+  lazy = true,
   opts = {},
+  keys = {
+    {
+      "<Leader>re",
+      function()
+        return require('refactoring').refactor('Extract Function')
+      end,
+      mode = { "n", "x" },
+      expr = true,
+      desc = "Refactor Extract Function",
+    },
+    {
+      "<Leader>rf",
+      function()
+        return require('refactoring').refactor('Extract Function To File')
+      end,
+      mode = { "n", "x" },
+      expr = true,
+      desc = "Refactor Extract To File",
+    },
+    {
+      "<Leader>rv",
+      function()
+        return require('refactoring').refactor('Extract Variable')
+      end,
+      mode = { "n", "x" },
+      expr = true,
+      desc = "Refactor Variable",
+    },
+    {
+      "<leader>rI",
+      function()
+        return require('refactoring').refactor('Inline Function')
+      end,
+      mode = { "n", "x" },
+      expr = true,
+      desc = "Refactor Inline Function",
+    },
+    {
+      "<leader>ri",
+      function()
+        return require('refactoring').refactor('Inline Variable')
+      end,
+      mode = { "n", "x" },
+      expr = true,
+      desc = "Refactor Inline Variable"
+    },
+    {
+      "<leader>rbb",
+      function()
+        return require('refactoring').refactor('Extract Block')
+      end,
+      mode = { "n", "x" },
+      expr = true,
+      desc = "Refactor Extract Block"
+    },
+    {
+      "<leader>rbf",
+      function()
+        return require('refactoring').refactor('Extract Block To File')
+      end,
+      mode = { "n", "x" },
+      expr = true,
+      desc = "Refactor Extract Block to File"
+    },
+  },
 }
