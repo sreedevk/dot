e5cdc473bbe182c408b6134a06241794d2f42e42
sreedevk <sreedev@icloud.com>
2026-03-06 16:41:36 +0000

    hyprland issues fixed

--- nixos/common/hm/hyprland/opts.nix
+++ nixos/common/hm/hyprland/opts.nix
@@ -136,8 +136,8 @@ in
 
   scrolling = {
     fullscreen_on_one_column = "true";
-    column_width = "0.85";
-    focus_fit_method = "0"; # 0 = center; 1 = fit
+    column_width = "0.90";
+    focus_fit_method = "1"; # 0 = center; 1 = fit
     follow_focus = "true";
     direction = "right";
   };
