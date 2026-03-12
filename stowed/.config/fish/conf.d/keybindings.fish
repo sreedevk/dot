a436a88fcc5406f800cb33050f4f41f4b1c5f61b
sreedevk <sreedev@icloud.com>
2026-03-12 06:58:13 -0400

    add rakefile completions for fish

--- stowed/.config/fish/conf.d/keybindings.fish
+++ stowed/.config/fish/conf.d/keybindings.fish
@@ -1,7 +1,13 @@
+function __ce_accept_or_eol
+    commandline -f accept-autosuggestion
+    commandline -f end-of-line
+end
+
 function fish_user_key_bindings
     fish_vi_key_bindings
 
     bind -M insert ctrl-e accept-autosuggestion
     bind -M insert ctrl-a beginning-of-line
     bind -M insert ctrl-w backward-kill-word
+    bind -M insert ctrl-e __ce_accept_or_eol
 end
