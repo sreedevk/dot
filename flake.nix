b3296301a4ca2c10964cc627bf4d6e982dcd366e
sreedevk <sreedev@icloud.com>
2026-03-14 16:51:53 -0400

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'disko':
        'github:nix-community/disko/7b9f7f88ab3b339f8142dc246445abb3c370d3d3?narHash=sha256-khlHllTsovXgT2GZ0WxT4%2BRvuMjNeR5OW0UYeEHPYQo%3D' (2026-03-09)
      → 'github:nix-community/disko/878ec37d6a8f52c6c801d0e2a2ad554c75b9353c?narHash=sha256-qWKbLUJpavIpvOdX1fhHYm0WGerytFHRoh9lVck6Bh0%3D' (2026-03-14)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/fe416aaedd397cacb33a610b33d60ff2b431b127?narHash=sha256-b/GV2ysM8mKHhinse2wz%2BuP37epUrSE%2BsAKXy/xvBY4%3D' (2026-03-12)
      → 'github:nixos/nixpkgs/c06b4ae3d6599a672a6210b7021d699c351eebda?narHash=sha256-wvfdLLWJ2I9oEpDd9PfMA8osfIZicoQ5MT1jIwNs9Tk%3D' (2026-03-13)
    
    flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'vicinae':
        'github:vicinaehq/vicinae/3fe7f6985bb9951332a11626d390f935516b8e3b?narHash=sha256-k3rmWZlUYLMOT33AvwZ56eZfjNc6KXH2AN5Fe8LDKFM%3D' (2026-03-09)
      → 'github:vicinaehq/vicinae/4d775ae58fe0612d60cba99bea42fc8ba2835128?narHash=sha256-EUevIsThXX8mdHSGJGdKCOY3Zp2YOfqdF4D4LhGIwY8%3D' (2026-03-14)

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     llama-cpp.url  = "github:ggml-org/llama.cpp?ref=b7541";
     nixpkgs.url    = "github:nixos/nixpkgs?ref=nixos-unstable&shallow=1";
     stablepkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11&shallow=1";
-    vicinae.url    = "github:vicinaehq/vicinae?ref=v0.20.5";
+    vicinae.url    = "github:vicinaehq/vicinae?ref=v0.20.6";
 
     librepods = {
       url                    = "github:Chrisbattarbee/librepods";
