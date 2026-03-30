c6c1d7daf0dab9fa3ac74a87e783fd681c0f32be
sreedevk <sreedev@icloud.com>
2026-03-30 11:42:03 +0000

    cleanup secrets.nix

--- secrets.nix
+++ secrets.nix
@@ -8,72 +8,72 @@ let
   };
 in
 with builtins;
-{
-  "secrets/nyc0/apollo_admin_password.age".publicKeys         = attrValues nyc0;
-  "secrets/nyc0/apollo_nginx_env.age".publicKeys              = attrValues nyc0;
-  "secrets/nyc0/arcane_agent_env.age".publicKeys              = attrValues nyc0;
-  "secrets/nyc0/arcane_env.age".publicKeys                    = attrValues nyc0;
-  "secrets/nyc0/aria2_env.age".publicKeys                     = attrValues nyc0;
-  "secrets/nyc0/attic_env.age".publicKeys                     = attrValues nyc0;
-  "secrets/nyc0/attic_server_config_toml.age".publicKeys      = attrValues nyc0;
-  "secrets/nyc0/autobrr_env.age".publicKeys                   = attrValues nyc0;
-  "secrets/nyc0/autokuma_env.age".publicKeys                  = attrValues nyc0;
-  "secrets/nyc0/aws-secrets.age".publicKeys                   = attrValues nyc0;
-  "secrets/nyc0/baikal_user_password.age".publicKeys          = attrValues nyc0;
-  "secrets/nyc0/bitmagnet_env.age".publicKeys                 = attrValues nyc0;
-  "secrets/nyc0/grimmory.age".publicKeys                      = attrValues nyc0;
-  "secrets/nyc0/cargo-token.age".publicKeys                   = attrValues nyc0;
-  "secrets/nyc0/container_registry_env.age".publicKeys        = attrValues nyc0;
-  "secrets/nyc0/dawarich_env.age".publicKeys                  = attrValues nyc0;
-  "secrets/nyc0/ddns_config_json.age".publicKeys              = attrValues nyc0;
-  "secrets/nyc0/digitalocean-token.age".publicKeys            = attrValues nyc0;
-  "secrets/nyc0/dumb_assets_env.age".publicKeys               = attrValues nyc0;
-  "secrets/nyc0/fastmail_server_env.age".publicKeys           = attrValues nyc0;
-  "secrets/nyc0/firefly_env.age".publicKeys                   = attrValues nyc0;
-  "secrets/nyc0/gh-token.age".publicKeys                      = attrValues nyc0;
-  "secrets/nyc0/ghcr_ro_token.age".publicKeys                 = attrValues nyc0;
-  "secrets/nyc0/gitea_env.age".publicKeys                     = attrValues nyc0;
-  "secrets/nyc0/gotify_env.age".publicKeys                    = attrValues nyc0;
-  "secrets/nyc0/hugging_face_token.age".publicKeys            = attrValues nyc0;
-  "secrets/nyc0/invio_env.age".publicKeys                     = attrValues nyc0;
-  "secrets/nyc0/jellystat_env.age".publicKeys                 = attrValues nyc0;
-  "secrets/nyc0/jira-token.age".publicKeys                    = attrValues nyc0;
-  "secrets/nyc0/k3s_token.age".publicKeys                     = attrValues nyc0;
-  "secrets/nyc0/karakeep_env.age".publicKeys                  = attrValues nyc0;
-  "secrets/nyc0/koito_env.age".publicKeys                     = attrValues nyc0;
-  "secrets/nyc0/livebook_env.age".publicKeys                  = attrValues nyc0;
-  "secrets/nyc0/miniflux_app_password.age".publicKeys         = attrValues nyc0;
-  "secrets/nyc0/miniflux_env.age".publicKeys                  = attrValues nyc0;
-  "secrets/nyc0/minio_env.age".publicKeys                     = attrValues nyc0;
-  "secrets/nyc0/moneroconf.age".publicKeys                    = attrValues nyc0;
-  "secrets/nyc0/next_explorer_env.age".publicKeys             = attrValues nyc0;
-  "secrets/nyc0/nullptrderef1_admin_password.age".publicKeys  = attrValues nyc0;
-  "secrets/nyc0/openai_api_key.age".publicKeys                = attrValues nyc0;
-  "secrets/nyc0/openweather-token.age".publicKeys             = attrValues nyc0;
-  "secrets/nyc0/paperless_env.age".publicKeys                 = attrValues nyc0;
-  "secrets/nyc0/paperless_gpt_env.age".publicKeys             = attrValues nyc0;
-  "secrets/nyc0/pastebin-token.age".publicKeys                = attrValues nyc0;
-  "secrets/nyc0/phoenix_user_password.age".publicKeys         = attrValues nyc0;
-  "secrets/nyc0/photoprism_env.age".publicKeys                = attrValues nyc0;
-  "secrets/nyc0/pinepods_env.age".publicKeys                  = attrValues nyc0;
-  "secrets/nyc0/pocket_id_env.age".publicKeys                 = attrValues nyc0;
-  "secrets/nyc0/podgrab_env.age".publicKeys                   = attrValues nyc0;
-  "secrets/nyc0/pushtify_env.age".publicKeys                  = attrValues nyc0;
-  "secrets/nyc0/radarr-api-key.age".publicKeys                = attrValues nyc0;
-  "secrets/nyc0/radicle_passphrase.age".publicKeys            = attrValues nyc0;
-  "secrets/nyc0/restic_backup_password.age".publicKeys        = attrValues nyc0;
-  "secrets/nyc0/romm_env.age".publicKeys                      = attrValues nyc0;
-  "secrets/nyc0/rpi4b_pi_user_password.age".publicKeys        = attrValues nyc0;
-  "secrets/nyc0/searxng_env.age".publicKeys                   = attrValues nyc0;
-  "secrets/nyc0/slskd_env.age".publicKeys                     = attrValues nyc0;
-  "secrets/nyc0/sonarr-api-key.age".publicKeys                = attrValues nyc0;
-  "secrets/nyc0/stash_vr_env.age".publicKeys                  = attrValues nyc0;
-  "secrets/nyc0/tailscale_nginx_env.age".publicKeys           = attrValues nyc0;
-  "secrets/nyc0/taskwarrior_client_id.age".publicKeys         = attrValues nyc0;
-  "secrets/nyc0/taskwarrior_encryption_secret.age".publicKeys = attrValues nyc0;
-  "secrets/nyc0/taskwarrior_env.age".publicKeys               = attrValues nyc0;
-  "secrets/nyc0/tinyauth_env.age".publicKeys                  = attrValues nyc0;
-  "secrets/nyc0/vpn-acc.age".publicKeys                       = attrValues nyc0;
-  "secrets/nyc0/vpn-loc.age".publicKeys                       = attrValues nyc0;
-  "secrets/nyc0/wallhaven-token.age".publicKeys               = attrValues nyc0;
-}
+(foldl' (acc: elem: acc // { "secrets/nyc0/${elem}.age".publicKeys = attrValues nyc0; }) { } [
+  "apollo_admin_password"
+  "apollo_nginx_env"
+  "arcane_agent_env"
+  "arcane_env"
+  "aria2_env"
+  "attic_env"
+  "attic_server_config_toml"
+  "autobrr_env"
+  "autokuma_env"
+  "aws-secrets"
+  "baikal_user_password"
+  "bitmagnet_env"
+  "grimmory"
+  "cargo-token"
+  "container_registry_env"
+  "dawarich_env"
+  "ddns_config_json"
+  "digitalocean-token"
+  "dumb_assets_env"
+  "fastmail_server_env"
+  "firefly_env"
+  "gh-token"
+  "ghcr_ro_token"
+  "gitea_env"
+  "gotify_env"
+  "hugging_face_token"
+  "invio_env"
+  "jellystat_env"
+  "jira-token"
+  "k3s_token"
+  "karakeep_env"
+  "koito_env"
+  "livebook_env"
+  "miniflux_app_password"
+  "miniflux_env"
+  "minio_env"
+  "moneroconf"
+  "next_explorer_env"
+  "nullptrderef1_admin_password"
+  "openai_api_key"
+  "openweather-token"
+  "paperless_env"
+  "paperless_gpt_env"
+  "pastebin-token"
+  "phoenix_user_password"
+  "photoprism_env"
+  "pinepods_env"
+  "pocket_id_env"
+  "podgrab_env"
+  "pushtify_env"
+  "radarr-api-key"
+  "radicle_passphrase"
+  "restic_backup_password"
+  "romm_env"
+  "rpi4b_pi_user_password"
+  "searxng_env"
+  "slskd_env"
+  "sonarr-api-key"
+  "stash_vr_env"
+  "tailscale_nginx_env"
+  "taskwarrior_client_id"
+  "taskwarrior_encryption_secret"
+  "taskwarrior_env"
+  "tinyauth_env"
+  "vpn-acc"
+  "vpn-loc"
+  "wallhaven-token"
+])
