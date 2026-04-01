9fdc849940aa754d7cb188995c68d628720732e0
sreedevk <sreedev@icloud.com>
2026-04-01 17:13:46 +0000

    noctalia-shell updated

--- flake.nix
+++ flake.nix
@@ -58,7 +58,7 @@
     };
 
     noctalia = {
-      url                    = "github:noctalia-dev/noctalia-shell?ref=v4.7.1";
+      url                    = "github:noctalia-dev/noctalia-shell?ref=v4.7.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
