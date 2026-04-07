7ea4fef42462e179f0f79b3052c18ebe5831a3bb
sreedevk <sreedev@icloud.com>
2026-04-07 11:18:28 +0000

    Revert "add glyph lsp for sml"
    This reverts commit 0ca2dde8dab5838495075e5af3560feb8c40de97.

--- stowed/.config/nvim/lsp/glyph.lua
+++
@@ -1,9 +0,0 @@
----@type vim.lsp.Config
-return {
-  cmd = { 'cthulhu', 'lsp' },
-
-  filetypes = { 'sml' },
-  root_markers = {
-    '.git',
-  },
-}
--- stowed/.config/nvim/lua/core/lsp.lua
+++ stowed/.config/nvim/lua/core/lsp.lua
@@ -17,7 +17,6 @@ vim.lsp.enable({
   "jsonls",
   "ltex_plus",
   "lua_ls",
-  "glyph",
   "marksman",
   "nil_ls",
   "ocamllsp",
