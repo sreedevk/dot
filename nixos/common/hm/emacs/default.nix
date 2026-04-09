91ccb840dfec9d7c5a11dec7a26c78d801262136
sreedevk <sreedev@icloud.com>
2026-04-09 10:34:10 +0000

    update doom emacs

--- nixos/common/hm/emacs/default.nix
+++ nixos/common/hm/emacs/default.nix
@@ -36,7 +36,7 @@
       enable = true;
       source = builtins.fetchGit {
         url = "https://github.com/doomemacs/doomemacs.git";
-        rev = "470e653f08cfe85bbc02516af65e44d3b9c735b8";
+        rev = "cfb5aef1e9422dc02ac16da99919294d58e687e5";
         ref = "master";
       };
     };
