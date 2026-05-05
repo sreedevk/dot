a15770e22eba2f9a98038a001615c6537d7295c1
sreedevk <sreedev@icloud.com>
2026-05-05 08:22:17 +0000

    add docker monitor

--- nixos/hosts/odoo/opts.nix
+++ nixos/hosts/odoo/opts.nix
@@ -11,14 +11,34 @@
     {
       name = "eDP-1";
       desc = "Lenovo Group Limited MNE007JA1-3";
-      resolution = { x = 1920; y = 1200; };
-      position = { x = 0; y = 0; };
+      resolution = {
+        x = 1920;
+        y = 1200;
+      };
+      position = {
+        x = 0;
+        y = 0;
+      };
       rate = 60;
       scale = 1;
-      bitdepth   = 10;
+      bitdepth = 10;
+    }
+    {
+      name = "HDMI-A-1";
+      desc = "Iiyama North America PL2492HN 1178622401411";
+      resolution = {
+        x = 1920;
+        y = 1080;
+      };
+      position = {
+        x = 1200;
+        y = 0;
+      };
+      rate = 60;
+      scale = 1;
+      bitdepth = 10;
     }
   ];
 
-  ports = {
-  };
+  ports = { };
 }
