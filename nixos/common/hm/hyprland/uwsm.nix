4f3b157a6ffaca242fc2e484f9323c2988967945
sreedevk <sreedev@icloud.com>
2026-05-09 16:55:22 +0000

    uwsm fixes

--- nixos/common/hm/hyprland/uwsm.nix
+++ nixos/common/hm/hyprland/uwsm.nix
@@ -49,6 +49,8 @@ in
       text = ''
         export EGL_PLATFORM=wayland
         export __EGL_EXTERNAL_PLATFORM_CONFIG_DIRS=/usr/share/egl/egl_external_platform.d
+        export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+        export __EGL_EXTERNAL_PLATFORM_CONFIG_DIRS=/usr/share/egl/egl_external_platform.d
         export ELECTRON_OZONE_PLATFORM_HINT=auto
         export GBM_BACKEND=nvidia-drm
         export GDK_DPI_SCALE=${opts.desktop.scale}
