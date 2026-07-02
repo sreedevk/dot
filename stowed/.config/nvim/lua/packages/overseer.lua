77a382ee6e44fc5b5f1eaedefc53f8e1920bce3f
Sreedev Kodichath (srkod) <srkod@odoo.com>
2026-07-02 08:29:03 +0000

    [IMP] common: neovim overseer keybindings improved

--- stowed/.config/nvim/lua/packages/overseer.lua
+++ stowed/.config/nvim/lua/packages/overseer.lua
@@ -9,6 +9,29 @@ return {
         ":OverseerShell ",
         desc = "Async Run",
         noremap = true,
+      },
+      {
+        '<leader>or',
+        function()
+          local overseer = require("overseer")
+          local task_list = require("overseer.task_list")
+          local tasks = overseer.list_tasks({
+            status = {
+              overseer.STATUS.SUCCESS,
+              overseer.STATUS.FAILURE,
+              overseer.STATUS.CANCELED,
+            },
+            sort = task_list.sort_finished_recently
+          })
+          if vim.tbl_isempty(tasks) then
+            vim.notify("No tasks found", vim.log.levels.WARN)
+          else
+            local most_recent = tasks[1]
+            overseer.run_action(most_recent, "restart")
+          end
+        end,
+        desc = "restart last task",
+        noremap = true
       }
     },
     ---@type overseer.SetupOpts
@@ -24,18 +47,29 @@ return {
         keymaps = {
           ["?"] = "keymap.show_help",
           ["g?"] = "keymap.show_help",
+          ["gd"] = {
+            function()
+              for _, task in ipairs(require("overseer").list_tasks()) do
+                task:dispose()
+              end
+            end,
+            mode = "n",
+            nowait = true,
+            desc = "Dispose all tasks"
+          },
           ["<CR>"] = "keymap.run_action",
-          ["dd"] = { "keymap.run_action", opts = { action = "dispose" }, desc = "Dispose task" },
+          ["dd"] = { "keymap.run_action", opts = { action = "dispose" }, desc = "dispose task" },
+          ["r"] = { "keymap.run_action", opts = { action = "restart" }, desc = "restart task" },
           ["<C-e>"] = { "keymap.run_action", opts = { action = "edit" }, desc = "Edit task" },
           ["o"] = "keymap.open",
-          ["<C-v>"] = { "keymap.open", opts = { dir = "vsplit" }, desc = "Open task output in vsplit" },
-          ["<C-s>"] = { "keymap.open", opts = { dir = "split" }, desc = "Open task output in split" },
-          ["<C-t>"] = { "keymap.open", opts = { dir = "tab" }, desc = "Open task output in tab" },
-          ["<C-f>"] = { "keymap.open", opts = { dir = "float" }, desc = "Open task output in float" },
+          ["<C-v>"] = { "keymap.open", opts = { dir = "vsplit" }, desc = "open task output in vsplit" },
+          ["<C-s>"] = { "keymap.open", opts = { dir = "split" }, desc = "open task output in split" },
+          ["<C-t>"] = { "keymap.open", opts = { dir = "tab" }, desc = "open task output in tab" },
+          ["<C-f>"] = { "keymap.open", opts = { dir = "float" }, desc = "open task output in float" },
           ["<C-q>"] = {
             "keymap.run_action",
             opts = { action = "open output in quickfix" },
-            desc = "Open task output in the quickfix",
+            desc = "open task output in the quickfix",
           },
           ["p"] = "keymap.toggle_preview",
           ["{"] = "keymap.prev_task",
