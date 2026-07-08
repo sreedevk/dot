05c77a06a15bd5b9e3c348ec46cc82e0d8dfdee7
Sreedev Kodichath (srkod) <srkod@odoo.com>
2026-07-08 12:07:00 +0000

    [IMP] common: add custom addons to firefox

--- nixos/common/hm/firefox/addons.nix
+++ nixos/common/hm/firefox/addons.nix
@@ -1,5 +1,16 @@
 { pkgs, ... }:
-with pkgs.nur.repos.rycee.firefox-addons;
+let
+  rycee = pkgs.nur.repos.rycee;
+  vimium-new-tab-page = rycee.firefox-addons.buildFirefoxXpiAddon {
+    pname = "vimium-new-tab-page";
+    version = "1.0";
+    addonId = "@vimium-new-tab";
+    url = "https://addons.mozilla.org/firefox/downloads/file/4612990/vimium_new_tab_page-1.0.xpi";
+    sha256 = "sha256-NuaTGE57twd3LenqGDS8vNwzVRrrELM/bwcvhntKMt0=";
+    meta = { };
+  };
+in
+with rycee.firefox-addons;
 [
   bitwarden
   consent-o-matic
@@ -14,4 +25,5 @@ with pkgs.nur.repos.rycee.firefox-addons;
   ublock-origin
   vimium
   youtube-shorts-block
+  vimium-new-tab-page
 ]
--- nixos/hosts/odoo/users/srkod/modules/firefox/profiles/main/addons.nix
+++ nixos/hosts/odoo/users/srkod/modules/firefox/profiles/main/addons.nix
@@ -1,5 +1,16 @@
 { pkgs, ... }:
-with pkgs.nur.repos.rycee.firefox-addons;
+let
+  rycee = pkgs.nur.repos.rycee;
+  vimium-new-tab-page = rycee.firefox-addons.buildFirefoxXpiAddon {
+    pname = "vimium-new-tab-page";
+    version = "1.0";
+    addonId = "@vimium-new-tab";
+    url = "https://addons.mozilla.org/firefox/downloads/file/4612990/vimium_new_tab_page-1.0.xpi";
+    sha256 = "sha256-NuaTGE57twd3LenqGDS8vNwzVRrrELM/bwcvhntKMt0=";
+    meta = { };
+  };
+in
+with rycee.firefox-addons;
 [
   bitwarden
   consent-o-matic
@@ -8,4 +19,5 @@ with pkgs.nur.repos.rycee.firefox-addons;
   tampermonkey
   ublock-origin
   vimium
+  vimium-new-tab-page
 ]
