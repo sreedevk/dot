06744f1c5cbb4c72549fd5d1aadf3d6e4d55a795
Sreedev Kodichath (srkod) <srkod@odoo.com>
2026-07-16 13:56:47 +0000

    [UPD] common: replace deprecated functions in refactor.nvim plugin

--- stowed/.config/nvim/lua/packages/refactoring.lua
+++ stowed/.config/nvim/lua/packages/refactoring.lua
@@ -1,8 +1,7 @@
 return {
   "ThePrimeagen/refactoring.nvim",
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
+    "lewis6991/async.nvim",
   },
   cmd = { "Refactor" },
   lazy = true,
@@ -11,65 +10,11 @@ return {
     {
       "<Leader>re",
       function()
-        return require('refactoring').refactor('Extract Function')
+        require("refactoring").select_refactor()
       end,
       mode = { "n", "x" },
       expr = true,
-      desc = "Refactor Extract Function",
-    },
-    {
-      "<Leader>rf",
-      function()
-        return require('refactoring').refactor('Extract Function To File')
-      end,
-      mode = { "n", "x" },
-      expr = true,
-      desc = "Refactor Extract To File",
-    },
-    {
-      "<Leader>rv",
-      function()
-        return require('refactoring').refactor('Extract Variable')
-      end,
-      mode = { "n", "x" },
-      expr = true,
-      desc = "Refactor Variable",
-    },
-    {
-      "<leader>rI",
-      function()
-        return require('refactoring').refactor('Inline Function')
-      end,
-      mode = { "n", "x" },
-      expr = true,
-      desc = "Refactor Inline Function",
-    },
-    {
-      "<leader>ri",
-      function()
-        return require('refactoring').refactor('Inline Variable')
-      end,
-      mode = { "n", "x" },
-      expr = true,
-      desc = "Refactor Inline Variable"
-    },
-    {
-      "<leader>rbb",
-      function()
-        return require('refactoring').refactor('Extract Block')
-      end,
-      mode = { "n", "x" },
-      expr = true,
-      desc = "Refactor Extract Block"
-    },
-    {
-      "<leader>rbf",
-      function()
-        return require('refactoring').refactor('Extract Block To File')
-      end,
-      mode = { "n", "x" },
-      expr = true,
-      desc = "Refactor Extract Block to File"
+      desc = "Select refactor"
     },
   },
 }
